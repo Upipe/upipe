a40c4b9fd8a36a90cd171fe9d98bdaca1f4ce11c
Christophe Massiot <massiot@via.ecp.fr>
2012-06-13 22:22:44 +0200

    add udeal for exclusive (non-reentrant) access
     - also add a ucounter object
     - implement udeal unit test
     - update documentation accordingly

---
+++ include/upipe/udeal.h
@@ -0,0 +1,156 @@
+/*****************************************************************************
+ * udeal.h: upipe efficient exclusive access to a non-reentrant resource
+ *****************************************************************************
+ * Copyright (C) 2012 OpenHeadend S.A.R.L.
+ *
+ * Authors: Christophe Massiot <massiot@via.ecp.fr>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+
+#ifndef _UPIPE_UDEAL_H_
+/** @hidden */
+#define _UPIPE_UDEAL_H_
+
+#include <assert.h>
+
+#include <upipe/config.h>
+#include <upipe/ubase.h>
+#include <upipe/ucounter.h>
+#include <upipe/ueventfd.h>
+#include <upipe/upump.h>
+
+/** @This is the implementation of a structure that deals access to a
+ * non-reentrant resource. */
+struct udeal {
+    /** number of waiters */
+    ucounter waiters;
+    /** number of accesses to the resource (0 or 1) */
+    ucounter access;
+    /** ueventfd triggered when a waiter may be unblocked */
+    ueventfd event;
+};
+
+/** @This initializes a udeal.
+ *
+ * @param udeal pointer to a udeal structure
+ * @return false in case of failure
+ */
+static inline bool udeal_init(struct udeal *udeal)
+{
+    if (unlikely(!ueventfd_init(&udeal->event, true)))
+        return false;
+
+    ucounter_init(&udeal->waiters, 0);
+    ucounter_init(&udeal->access, 0);
+    return true;
+}
+
+/** @This allocates a watcher triggering when a waiter may be unblocked.
+ *
+ * @param udeal pointer to a udeal structure
+ * @param upump_mgr management structure for this event loop
+ * @param cb function to call when the watcher triggers
+ * @param opaque pointer to the module's internal structure
+ * @return pointer to allocated watcher, or NULL in case of failure
+ */
+static inline struct upump *udeal_upump_alloc(struct udeal *udeal,
+                                              struct upump_mgr *upump_mgr,
+                                              upump_cb cb, void *opaque)
+{
+    return ueventfd_upump_alloc(&udeal->event, upump_mgr, cb, opaque, false);
+}
+
+/** @This starts the watcher and tries to immediately run the call-back.
+ *
+ * @param udeal pointer to a udeal structure
+ * @param upump watcher allocated by @ref udeal_upump_alloc
+ * @return false in case of upump error
+ */
+static inline bool udeal_start(struct udeal *udeal, struct upump *upump)
+{
+    if (unlikely(!upump_start(upump)))
+        return false;
+    if (likely(ucounter_add(&udeal->waiters, 1) == 0))
+        upump->cb(upump);
+    return true;
+}
+
+/** @This tries to grab the resource.
+ *
+ * @param udeal pointer to a udeal structure
+ * @return true of the resource may be exclusively used
+ */
+static inline bool udeal_grab(struct udeal *udeal)
+{
+    while (unlikely(ucounter_add(&udeal->access, 1) > 0)) {
+        ueventfd_read(&udeal->event);
+
+        /* double-check */
+        if (likely(ucounter_sub(&udeal->access, 1) > 1))
+            return false;
+
+        /* try again */
+        ueventfd_write(&udeal->event);
+    }
+
+    return true;
+}
+
+/** @This yields access to an exclusive resource previously acquired from
+ * @ref udeal_grab, and stops the watcher.
+ *
+ * @param udeal pointer to a udeal structure
+ * @param upump watcher allocated by @ref udeal_upump_alloc
+ * @return false in case of upump error
+ */
+static inline bool udeal_yield(struct udeal *udeal, struct upump *upump)
+{
+    ucounter_sub(&udeal->access, 1);
+    if (ucounter_sub(&udeal->waiters, 1) > 1)
+        ueventfd_write(&udeal->event);
+    return upump_stop(upump);
+}
+
+/** @This aborts the watcher before it has had a chance to run. It must only
+ * be called in case of abort, otherwise @ref udeal_yield does the same job.
+ *
+ * @param udeal pointer to a udeal structure
+ * @param upump watcher allocated by @ref udeal_upump_alloc
+ * @return false in case of upump error
+ */
+static inline bool udeal_abort(struct udeal *udeal, struct upump *upump)
+{
+    ucounter_sub(&udeal->waiters, 1);
+    return upump_stop(upump);
+}
+
+/** @This cleans up the udeal structure.
+ *
+ * @param udeal pointer to a udeal structure
+ */
+static inline void udeal_clean(struct udeal *udeal)
+{
+    ucounter_clean(&udeal->waiters);
+    ucounter_clean(&udeal->access);
+    ueventfd_clean(&udeal->event);
+}
+
+#endif
---
+++ tests/udeal_test.c
@@ -0,0 +1,143 @@
+/*****************************************************************************
+ * udeal_test.c: unit tests for udeals (using libev)
+ *****************************************************************************
+ * Copyright (C) 2012 OpenHeadend S.A.R.L.
+ *
+ * Authors: Christophe Massiot <massiot@via.ecp.fr>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+
+#undef NDEBUG
+#include <assert.h>
+
+#include <ev.h>
+#include <pthread.h>
+
+#include <upipe/ubase.h>
+#include <upipe/udeal.h>
+#include <upipe/upump.h>
+#include <upump-ev/upump_ev.h>
+
+#define NB_LOOPS 1000
+#define NB_TIMEOUTS 10
+
+static const long nsec_timeouts[NB_TIMEOUTS] = {
+    0, 1000000, 5000000, 0, 50000, 0, 0, 10000000, 5000, 0
+};
+
+struct thread {
+    pthread_t id;
+    unsigned int thread;
+    unsigned int loop;
+};
+
+static unsigned int counter; /* intentionally unprotected */
+static struct udeal udeal;
+static unsigned int nb_loops = NB_LOOPS;
+static unsigned int loop[2] = {0, 0};
+
+static void test_grab(struct upump *upump)
+{
+    struct thread *thread = upump_get_opaque(upump, struct thread *);
+    unsigned int index;
+
+    if (unlikely(thread->loop >= nb_loops)) {
+        udeal_abort(&udeal, upump);
+        return;
+    }
+
+    if (unlikely(!udeal_grab(&udeal)))
+        return;
+
+    index = thread->loop % NB_TIMEOUTS;
+    if (likely(nsec_timeouts[index])) {
+        struct timespec ts = { .tv_sec = 0, .tv_nsec = nsec_timeouts[index] };
+        assert(!nanosleep(&ts, NULL));
+    }
+    counter++;
+    thread->loop++;
+
+    udeal_yield(&udeal, upump);
+
+    index = thread->loop % NB_TIMEOUTS;
+    if (likely(nsec_timeouts[index])) {
+        struct timespec ts = { .tv_sec = 0, .tv_nsec = nsec_timeouts[index] };
+        assert(!nanosleep(&ts, NULL));
+    }
+
+    thread->loop++;
+    udeal_start(&udeal, upump);
+}
+
+static void *test_thread(void *_thread)
+{
+    struct thread *thread = (struct thread *)_thread;
+    /* so that they both start at different places */
+    thread->loop = thread->thread;
+
+    struct ev_loop *loop = ev_loop_new(0);
+    struct upump_mgr *upump_mgr = upump_ev_mgr_alloc(loop);
+    assert(upump_mgr != NULL);
+
+    struct upump *upump = udeal_upump_alloc(&udeal, upump_mgr, test_grab,
+                                              thread);
+    assert(upump != NULL);
+
+    assert(udeal_start(&udeal, upump));
+
+    ev_loop(loop, 0);
+
+    upump_free(upump);
+    assert(urefcount_single(&upump_mgr->refcount));
+    upump_mgr_release(upump_mgr);
+    ev_loop_destroy(loop);
+
+    return NULL;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+        nb_loops = atoi(argv[1]);
+
+    assert(udeal_init(&udeal));
+
+    struct thread threads[2];
+    threads[0].thread = 0;
+    threads[1].thread = 1;
+    assert(pthread_create(&threads[0].id, NULL, test_thread, &threads[0]) == 0);
+    assert(pthread_create(&threads[1].id, NULL, test_thread, &threads[1]) == 0);
+
+    assert(!pthread_join(threads[0].id, NULL));
+    assert(!pthread_join(threads[1].id, NULL));
+
+    assert(counter == nb_loops);
+    udeal_clean(&udeal);
+
+    return 0;
+}
