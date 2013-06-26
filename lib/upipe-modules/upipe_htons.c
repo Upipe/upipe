b0c0b248e0e7b02c94b48c4c1016c3066affda3b
Benjamin Cohen <bencoh@notk.org>
2013-06-26 14:29:07 +0200

    Add upipe_htons

---
+++ lib/upipe-modules/upipe_htons.c
@@ -0,0 +1,233 @@
+/*
+ * Copyright (C) 2013 OpenHeadend S.A.R.L.
+ *
+ * Authors: Benjamin Cohen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short Upipe module - htons
+ */
+
+#include <upipe/ubase.h>
+#include <upipe/uprobe.h>
+#include <upipe/uref.h>
+#include <upipe/upipe.h>
+#include <upipe/uref_block.h>
+#include <upipe/uref_block_flow.h>
+#include <upipe/uref_flow.h>
+#include <upipe/upipe.h>
+#include <upipe/upipe_helper_upipe.h>
+#include <upipe/upipe_helper_flow.h>
+#include <upipe/upipe_helper_output.h>
+#include <upipe/upipe_helper_ubuf_mgr.h>
+#include <upipe-modules/upipe_htons.h>
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <arpa/inet.h>
+
+#define EXPECTED_FLOW_DEF "block."
+
+/** upipe_htons structure */ 
+struct upipe_htons {
+    /** ubuf manager */
+    struct ubuf_mgr *ubuf_mgr;
+
+    /** output pipe */
+    struct upipe *output;
+    /** flow_definition packet */
+    struct uref *flow_def;
+    /** true if the flow definition has already been sent */
+    bool flow_def_sent;
+
+    /** public upipe structure */
+    struct upipe upipe;
+};
+
+UPIPE_HELPER_UPIPE(upipe_htons, upipe);
+UPIPE_HELPER_FLOW(upipe_htons, EXPECTED_FLOW_DEF)
+UPIPE_HELPER_UBUF_MGR(upipe_htons, ubuf_mgr)
+UPIPE_HELPER_OUTPUT(upipe_htons, output, flow_def, flow_def_sent);
+
+/** @internal @This handles input.
+ *
+ * @param upipe description structure of the pipe
+ * @param uref uref structure
+ * @param upump pump that generated the buffer
+ */
+static void upipe_htons_input(struct upipe *upipe, struct uref *uref,
+                             struct upump *upump)
+{
+    struct upipe_htons *upipe_htons = upipe_htons_from_upipe(upipe);
+    struct ubuf *ubuf;
+    size_t size = 0;
+    int remain, bufsize = -1, offset = 0;
+    uint8_t *buf = NULL;
+
+    if (unlikely(uref->ubuf == NULL)) {
+        upipe_htons_output(upipe, uref, upump);
+        return;
+    }
+
+    /* block size */
+    if (unlikely(!uref_block_size(uref, &size))) {
+        upipe_warn(upipe, "could not read uref block size");
+        uref_free(uref);
+        return;
+    }
+    /* copy ubuf if shared or 16b-unaligned */
+    bufsize = -1;
+    if (!uref_block_write(uref, 0, &bufsize, &buf) || ((uintptr_t)buf & 1)) {
+        ubuf = ubuf_block_copy(upipe_htons->ubuf_mgr, uref->ubuf, 0, size);
+        if (unlikely(!ubuf)) {
+            upipe_throw_aerror(upipe);
+            uref_free(uref);
+            return;
+        uref_attach_ubuf(uref, ubuf);
+        }
+    } else {
+        uref_block_unmap(uref, 0);
+    }
+
+    /* process ubuf chunks */
+    while(size > 0) {
+        bufsize = -1;
+        uref_block_write(uref, offset, &bufsize, &buf);
+        if (unlikely((uintptr_t)buf & 1)) {
+            upipe_warn_va(upipe, "unaligned buffer: %p", buf);
+        }
+        for (remain = bufsize; remain > 1; remain -= 2) {
+            *(uint16_t *)buf = htons(*(uint16_t *)buf);
+            buf += 2;
+        }
+
+        uref_block_unmap(uref, offset);
+        offset += bufsize;
+        size -= bufsize;
+    }
+
+    upipe_htons_output(upipe, uref, upump);
+}
+
+/** @internal @This processes control commands on a skip pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @param command type of command to process
+ * @param args arguments of the command
+ * @return false in case of error
+ */
+static bool upipe_htons_control(struct upipe *upipe,
+                               enum upipe_command command, va_list args)
+{
+    switch (command) {
+        case UPIPE_GET_FLOW_DEF: {
+            struct uref **p = va_arg(args, struct uref **);
+            return upipe_htons_get_flow_def(upipe, p);
+        }
+        case UPIPE_GET_OUTPUT: {
+            struct upipe **p = va_arg(args, struct upipe **);
+            return upipe_htons_get_output(upipe, p);
+        }
+        case UPIPE_SET_OUTPUT: {
+            struct upipe *output = va_arg(args, struct upipe *);
+            return upipe_htons_set_output(upipe, output);
+        }
+        case UPIPE_GET_UBUF_MGR: {
+            struct ubuf_mgr **p = va_arg(args, struct ubuf_mgr **);
+            return upipe_htons_get_ubuf_mgr(upipe, p);
+        }
+        case UPIPE_SET_UBUF_MGR: {
+            struct ubuf_mgr *ubuf_mgr = va_arg(args, struct ubuf_mgr *);
+            return upipe_htons_set_ubuf_mgr(upipe, ubuf_mgr);
+        }
+
+        default:
+            return false;
+    }
+}
+
+/** @internal @This allocates a skip pipe.
+ *
+ * @param mgr common management structure
+ * @param uprobe structure used to raise events
+ * @param signature signature of the pipe allocator
+ * @param args optional arguments
+ * @return pointer to upipe or NULL in case of allocation error
+ */
+static struct upipe *upipe_htons_alloc(struct upipe_mgr *mgr,
+                                      struct uprobe *uprobe,
+                                      uint32_t signature, va_list args)
+{
+    struct uref *flow_def;
+    struct upipe *upipe = upipe_htons_alloc_flow(mgr, uprobe, signature, args,
+                                                &flow_def);
+    if (unlikely(upipe == NULL))
+        return NULL;
+
+    upipe_htons_init_output(upipe);
+    upipe_htons_init_ubuf_mgr(upipe);
+
+    upipe_htons_store_flow_def(upipe, flow_def);
+    upipe_throw_ready(upipe);
+    return upipe;
+}
+
+/** @internal @This frees all resources allocated.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_htons_free(struct upipe *upipe)
+{
+    struct upipe_htons *upipe_htons = upipe_htons_from_upipe(upipe);
+    upipe_throw_dead(upipe);
+
+    upipe_htons_clean_output(upipe);
+    upipe_htons_clean_ubuf_mgr(upipe);
+
+    upipe_clean(upipe);
+    free(upipe_htons);
+}
+
+static struct upipe_mgr upipe_htons_mgr = {
+    .signature = UPIPE_HTONS_SIGNATURE,
+
+    .upipe_alloc = upipe_htons_alloc,
+    .upipe_input = upipe_htons_input,
+    .upipe_control = upipe_htons_control,
+    .upipe_free = upipe_htons_free,
+
+    .upipe_mgr_free = NULL
+};
+
+/** @This returns the management structure for skip pipes
+ *
+ * @return pointer to manager
+ */
+struct upipe_mgr *upipe_htons_mgr_alloc(void)
+{
+    return &upipe_htons_mgr;
+}
---
+++ tests/upipe_htons_test.c
@@ -0,0 +1,216 @@
+/*
+ * Copyright (C) 2013 OpenHeadend S.A.R.L.
+ *
+ * Authors: Benjamin Cohen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short unit tests for htons module
+ */
+
+#undef NDEBUG
+
+#include <upipe/uprobe.h>
+#include <upipe/uprobe_stdio.h>
+#include <upipe/uprobe_prefix.h>
+#include <upipe/uprobe_log.h>
+#include <upipe/umem.h>
+#include <upipe/umem_alloc.h>
+#include <upipe/udict.h>
+#include <upipe/udict_inline.h>
+#include <upipe/ubuf.h>
+#include <upipe/ubuf_block.h>
+#include <upipe/ubuf_block_mem.h>
+#include <upipe/uref.h>
+#include <upipe/uref_flow.h>
+#include <upipe/uref_block_flow.h>
+#include <upipe/uref_block.h>
+#include <upipe/uref_std.h>
+#include <upipe/upipe.h>
+#include <upipe-modules/upipe_htons.h>
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <assert.h>
+
+#define UDICT_POOL_DEPTH 10
+#define UREF_POOL_DEPTH 10
+#define UBUF_POOL_DEPTH 10
+#define UPROBE_LOG_LEVEL UPROBE_LOG_DEBUG
+
+#define PACKETS_NUM 45
+#define PACKET_SIZE 524
+
+unsigned int nb_packets = 0;
+
+/** definition of our uprobe */
+static bool catch(struct uprobe *uprobe, struct upipe *upipe,
+                  enum uprobe_event event, va_list args)
+{
+    switch (event) {
+        default:
+            assert(0);
+            break;
+        case UPROBE_READY:
+        case UPROBE_DEAD:
+        case UPROBE_NEW_FLOW_DEF:
+            break;
+    }
+    return true;
+}
+
+/** helper phony pipe to test upipe_htons */
+static struct upipe *htons_test_alloc(struct upipe_mgr *mgr,
+                                          struct uprobe *uprobe,
+                                          uint32_t signature, va_list args)
+{
+    struct upipe *upipe = malloc(sizeof(struct upipe));
+    assert(upipe != NULL);
+    upipe_init(upipe, mgr, uprobe);
+    return upipe;
+}
+
+/** helper phony pipe to test upipe_htons */
+static void htons_test_input(struct upipe *upipe, struct uref *uref,
+                          struct upump *upump)
+{
+    assert(uref != NULL);
+    const uint8_t *buffer;
+    size_t size = 0;
+    int pos = 0, len = -1;
+    assert(uref_block_size(uref, &size));
+    upipe_dbg_va(upipe, "received packet of size %zu", size);
+
+    while (size > 0) {
+        assert(uref_block_read(uref, pos, &len, &buffer));
+        pos += len;
+        while (len > 1) {
+            assert(htons(*(uint16_t*)buffer) == nb_packets*PACKET_SIZE+size);
+            len -= 2;
+            size -= 2;
+            buffer += 2;
+        }
+        uref_block_unmap(uref, 0);
+    }
+    nb_packets--;
+    uref_free(uref);
+}
+
+/** helper phony pipe to test upipe_htons */
+static void htons_test_free(struct upipe *upipe)
+{
+    upipe_clean(upipe);
+    free(upipe);
+}
+
+/** helper phony pipe to test upipe_htons */
+static struct upipe_mgr htons_test_mgr = {
+    .upipe_alloc = htons_test_alloc,
+    .upipe_input = htons_test_input,
+    .upipe_control = NULL,
+    .upipe_free = NULL,
+
+    .upipe_mgr_free = NULL
+};
+
+int main(int argc, char *argv[])
+{
+    struct umem_mgr *umem_mgr = umem_alloc_mgr_alloc();
+    assert(umem_mgr != NULL);
+    struct udict_mgr *udict_mgr = udict_inline_mgr_alloc(UDICT_POOL_DEPTH,
+                                                         umem_mgr, -1, -1);
+    assert(udict_mgr != NULL);
+    struct uref_mgr *uref_mgr = uref_std_mgr_alloc(UREF_POOL_DEPTH, udict_mgr,
+                                                   0);
+    assert(uref_mgr != NULL);
+    struct ubuf_mgr *ubuf_mgr = ubuf_block_mem_mgr_alloc(UBUF_POOL_DEPTH,
+                                                         UBUF_POOL_DEPTH,
+                                                         umem_mgr, -1, -1,
+                                                         -1, 0);
+    assert(ubuf_mgr != NULL);
+    struct uprobe uprobe;
+    uprobe_init(&uprobe, catch, NULL);
+    struct uprobe *uprobe_stdio = uprobe_stdio_alloc(&uprobe, stdout,
+                                                     UPROBE_LOG_LEVEL);
+    assert(uprobe_stdio != NULL);
+    struct uprobe *log = uprobe_log_alloc(uprobe_stdio, UPROBE_LOG_LEVEL);
+    assert(log != NULL);
+
+    /* flow def */
+    struct uref *uref;
+    uref = uref_block_flow_alloc_def(uref_mgr, "foo.");
+    assert(uref != NULL);
+
+    struct upipe *upipe_sink = upipe_flow_alloc(&htons_test_mgr, log, uref);
+    assert(upipe_sink != NULL);
+
+    struct upipe_mgr *upipe_htons_mgr = upipe_htons_mgr_alloc();
+    assert(upipe_htons_mgr != NULL);
+    struct upipe *upipe_htons = upipe_flow_alloc(upipe_htons_mgr,
+            uprobe_pfx_adhoc_alloc(log, UPROBE_LOG_LEVEL, "htons"),
+            uref);
+    assert(upipe_htons != NULL);
+    assert(upipe_set_output(upipe_htons, upipe_sink));
+    uref_free(uref);
+
+    uint8_t *buffer;
+    int size, i;
+
+    nb_packets = PACKETS_NUM;
+    for (i=0; i < PACKETS_NUM; i++) {
+        uref = uref_block_alloc(uref_mgr, ubuf_mgr, PACKET_SIZE);
+        size = -1;
+        uref_block_write(uref, 0, &size, &buffer);
+        assert(size == PACKET_SIZE);
+        while (size > 1) {
+            *(uint16_t*)buffer = nb_packets*PACKET_SIZE+size;
+            buffer += 2;
+            size -= 2;
+        }
+        uref_block_unmap(uref, 0);
+        upipe_input(upipe_htons, uref, NULL);
+    }
+    /* TODO test segmented, shared, unaligned ubufs */
+
+    /* flush */
+    upipe_release(upipe_htons);
+
+    printf("nb_packets: %u\n", nb_packets);
+    assert(nb_packets == 0);
+
+    /* release everything */
+    upipe_mgr_release(upipe_htons_mgr); // nop
+
+    htons_test_free(upipe_sink);
+
+    uref_mgr_release(uref_mgr);
+    ubuf_mgr_release(ubuf_mgr);
+    udict_mgr_release(udict_mgr);
+    umem_mgr_release(umem_mgr);
+    uprobe_log_free(log);
+    uprobe_stdio_free(uprobe_stdio);
+
+    return 0;
+}
