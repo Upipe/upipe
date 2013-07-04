Upipe/upipe

commit 9683efc45867e221290ea2308a31775d5b07fd7a

rework the API of upump_mgr and uclock helpers
Also fix PTS handling in avcodec_decode.

lib/upipe-av/upipe_avcodec_decode.c
@@ -28,23 +28,25 @@
  */
 
 #include <upipe/ubase.h>
+#include <upipe/uclock.h>
 #include <upipe/uprobe.h>
 #include <upipe/ubuf.h>
 #include <upipe/uref.h>
 #include <upipe/uref_attr.h>
-#include <upipe/uref_clock.h>
 #include <upipe/uref_pic.h>
 #include <upipe/uref_flow.h>
 #include <upipe/uref_pic_flow.h>
 #include <upipe/uref_sound_flow.h>
 #include <upipe/uref_block_flow.h>
 #include <upipe/uref_block.h>
+#include <upipe/uref_clock.h>
 #include <upipe/upipe.h>
 #include <upipe/upipe_helper_upipe.h>
 #include <upipe/upipe_helper_flow.h>
 #include <upipe/upipe_helper_ubuf_mgr.h>
 #include <upipe/upipe_helper_output.h>
 #include <upipe/upipe_helper_upump_mgr.h>
+#include <upipe/upipe_helper_upump.h>
 #include <upipe/upipe_helper_sink.h>
 #include <upipe-av/upipe_avcodec_decode.h>
 
@@ -65,6 +67,9 @@
 
 #define EXPECTED_FLOW "block."
 
+/** @hidden */
+static void upipe_avcdec_reset_upump_mgr(struct upipe *upipe);
+/** @hidden */
 static bool upipe_avcdec_input_packet(struct upipe *upipe, struct uref *uref,
                                       struct upump *upump);
 
@@ -111,6 +116,8 @@ struct upipe_avcdec {
     uint64_t prev_rap;
     /** latest incoming uref */
     struct uref *uref;
+    /** next PTS */
+    uint64_t next_pts;
 
     /** avcodec context */
     AVCodecContext *context;
@@ -130,7 +137,8 @@ UPIPE_HELPER_UPIPE(upipe_avcdec, upipe);
 UPIPE_HELPER_FLOW(upipe_avcdec, EXPECTED_FLOW)
 UPIPE_HELPER_OUTPUT(upipe_avcdec, output, output_flow, output_flow_sent)
 UPIPE_HELPER_UBUF_MGR(upipe_avcdec, ubuf_mgr);
-UPIPE_HELPER_UPUMP_MGR(upipe_avcdec, upump_mgr, upump_av_deal)
+UPIPE_HELPER_UPUMP_MGR(upipe_avcdec, upump_mgr, upipe_avcdec_reset_upump_mgr)
+UPIPE_HELPER_UPUMP(upipe_avcdec, upump_av_deal, upump_mgr)
 UPIPE_HELPER_SINK(upipe_avcdec, urefs, blockers, upipe_avcdec_input_packet)
 
 /** @internal */
@@ -382,8 +390,7 @@ static bool upipe_avcdec_open_codec(struct upipe *upipe, AVCodec *codec,
             context->release_buffer = upipe_avcdec_release_buffer;
             context->flags |= CODEC_FLAG_EMU_EDGE;
             context->lowres = upipe_avcdec->lowres;
-            context->skip_loop_filter = AVDISCARD_ALL;
-
+            //context->skip_loop_filter = AVDISCARD_ALL;
 
             if (!upipe_avcdec->output_flow) {
                 struct uref *outflow = uref_dup(upipe_avcdec->input_flow);
@@ -615,7 +622,7 @@ static void upipe_avcdec_set_index_rap(struct upipe *upipe, struct uref *uref)
  * @param upump upump structure
  */
 static void upipe_avcdec_output_frame(struct upipe *upipe, AVFrame *frame,
-                                     struct upump *upump)
+                                      struct upump *upump)
 {
     struct ubuf *ubuf;
     struct upipe_avcdec *upipe_avcdec = upipe_avcdec_from_upipe(upipe);
@@ -671,6 +678,22 @@ static void upipe_avcdec_output_frame(struct upipe *upipe, AVFrame *frame,
     /* index rap attribute */
     upipe_avcdec_set_index_rap(upipe, uref);
 
+    /* DTS has no meaning from now on */
+    uref_clock_delete_dts(uref);
+
+    uint64_t pts;
+    if (!uref_clock_get_pts(uref, &pts)) {
+        pts = upipe_avcdec->next_pts;
+        if (pts != UINT64_MAX)
+            uref_clock_set_pts(uref, pts);
+    }
+
+    uint64_t duration;
+    if (pts != UINT64_MAX && uref_clock_get_duration(uref, &duration))
+        upipe_avcdec->next_pts = pts + duration;
+    else
+        upipe_warn(upipe, "couldn't determine next_pts");
+
     upipe_avcdec_output(upipe, uref, upump);
 }
 
@@ -685,7 +708,7 @@ static void upipe_avcdec_output_audio(struct upipe *upipe, AVFrame *frame,
 {
     struct ubuf *ubuf;
     struct upipe_avcdec *upipe_avcdec = upipe_avcdec_from_upipe(upipe);
-    struct uref *uref = frame->opaque;
+    struct uref *uref = uref_dup(frame->opaque);
     int bufsize = -1, avbufsize;
     size_t size = 0;
     uint8_t *buf;
@@ -733,7 +756,7 @@ static void upipe_avcdec_output_audio(struct upipe *upipe, AVFrame *frame,
  * @param upump upump structure
  */
 static bool upipe_avcdec_process_buf(struct upipe *upipe, uint8_t *buf,
-                                    size_t size, struct upump *upump)
+                                     size_t size, struct upump *upump)
 {
     int gotframe = 0, len;
     AVPacket avpkt;
@@ -804,7 +827,7 @@ static bool upipe_avcdec_process_buf(struct upipe *upipe, uint8_t *buf,
  * @return true if the output could be written
  */
 static bool upipe_avcdec_input_packet(struct upipe *upipe, struct uref *uref,
-                              struct upump *upump)
+                                      struct upump *upump)
 {
     uint8_t *inbuf;
     size_t insize = 0;
@@ -874,24 +897,21 @@ static void upipe_avcdec_input(struct upipe *upipe, struct uref *uref,
         return;
     }
 
-    if (unlikely(!upipe_avcdec_check_sink(upipe)))
-        upipe_avcdec_hold_sink(upipe, uref);
-    else if (!upipe_avcdec_input_packet(upipe, uref, upump)) {
+    if (unlikely(!upipe_avcdec_check_sink(upipe) ||
+                 !upipe_avcdec_input_packet(upipe, uref, upump))) {
         upipe_avcdec_block_sink(upipe, upump);
         upipe_avcdec_hold_sink(upipe, uref);
     }
 }
 
-/* @internal @This defines a new upump_mgr after aborting av_deal
+/** @internal @This resets upump_mgr-related fields.
  *
  * @param upipe description structure of the pipe
- * @return false in case of error
  */
-static bool _upipe_avcdec_set_upump_mgr(struct upipe *upipe,
-                                       struct upump_mgr *upump_mgr)
+static void upipe_avcdec_reset_upump_mgr(struct upipe *upipe)
 {
+    upipe_avcdec_set_upump_av_deal(upipe, NULL);
     upipe_avcdec_abort_av_deal(upipe);
-    return upipe_avcdec_set_upump_mgr(upipe, upump_mgr);
 }
 
 /** @internal @This returns the current codec definition string
@@ -931,8 +951,9 @@ static bool _upipe_avcdec_set_lowres(struct upipe *upipe, int lowres)
 
     if (upipe_avcdec->context && upipe_avcdec->context->codec) {
         codec_def = upipe_av_to_flow_def(upipe_avcdec->context->codec->id);
-        ret = _upipe_avcdec_set_codec(upipe, codec_def, upipe_avcdec->context->extradata,
-                                upipe_avcdec->context->extradata_size);
+        ret = _upipe_avcdec_set_codec(upipe, codec_def,
+                                      upipe_avcdec->context->extradata,
+                                      upipe_avcdec->context->extradata_size);
     }
     return ret;
 }
@@ -955,8 +976,8 @@ static bool _upipe_avcdec_get_lowres(struct upipe *upipe, int *lowres_p)
  * @param args arguments of the command
  * @return false in case of error
  */
-static bool upipe_avcdec_control(struct upipe *upipe, enum upipe_command command,
-                               va_list args)
+static bool upipe_avcdec_control(struct upipe *upipe,
+                                 enum upipe_command command, va_list args)
 {
     switch (command) {
         /* generic linear stuff */
@@ -986,7 +1007,7 @@ static bool upipe_avcdec_control(struct upipe *upipe, enum upipe_command command
         }
         case UPIPE_SET_UPUMP_MGR: {
             struct upump_mgr *upump_mgr = va_arg(args, struct upump_mgr *);
-            return _upipe_avcdec_set_upump_mgr(upipe, upump_mgr);
+            return upipe_avcdec_set_upump_mgr(upipe, upump_mgr);
         }
 
 
@@ -1004,17 +1025,17 @@ static bool upipe_avcdec_control(struct upipe *upipe, enum upipe_command command
             int size = va_arg(args, int);
             return _upipe_avcdec_set_codec(upipe, codec, extradata, size);
         }
-        case UPIPE_AVCDEC_SET_LOWRES: {
-            unsigned int signature = va_arg(args, unsigned int);
-            assert(signature == UPIPE_AVCDEC_SIGNATURE);
-            unsigned int lowres = va_arg(args, int);
-            return _upipe_avcdec_set_lowres(upipe, lowres);
-        }
         case UPIPE_AVCDEC_GET_LOWRES: {
             unsigned int signature = va_arg(args, unsigned int);
             assert(signature == UPIPE_AVCDEC_SIGNATURE);
             int *lowres_p = va_arg(args, int *);
             return _upipe_avcdec_get_lowres(upipe, lowres_p);
+        }
+        case UPIPE_AVCDEC_SET_LOWRES: {
+            unsigned int signature = va_arg(args, unsigned int);
+            assert(signature == UPIPE_AVCDEC_SIGNATURE);
+            unsigned int lowres = va_arg(args, int);
+            return _upipe_avcdec_set_lowres(upipe, lowres);
         }
 
         default:
@@ -1046,6 +1067,7 @@ static void upipe_avcdec_free(struct upipe *upipe)
     upipe_avcdec_clean_output(upipe);
     upipe_avcdec_clean_output(upipe);
     upipe_avcdec_clean_ubuf_mgr(upipe);
+    upipe_avcdec_clean_upump_av_deal(upipe);
     upipe_avcdec_clean_upump_mgr(upipe);
     upipe_avcdec_free_flow(upipe);
 }
@@ -1071,17 +1093,18 @@ static struct upipe *upipe_avcdec_alloc(struct upipe_mgr *mgr,
     struct upipe_avcdec *upipe_avcdec = upipe_avcdec_from_upipe(upipe);
     upipe_avcdec_init_ubuf_mgr(upipe);
     upipe_avcdec_init_upump_mgr(upipe);
+    upipe_avcdec_init_upump_av_deal(upipe);
     upipe_avcdec_init_output(upipe);
     upipe_avcdec_init_sink(upipe);
     upipe_avcdec->input_flow = flow_def;
     upipe_avcdec->context = NULL;
-    upipe_avcdec->upump_av_deal = NULL;
     upipe_avcdec->pixfmt = NULL;
     upipe_avcdec->frame = avcodec_alloc_frame();
     upipe_avcdec->lowres = 0;
 
     upipe_avcdec->index_rap = 0;
     upipe_avcdec->prev_rap = 0;
+    upipe_avcdec->next_pts = UINT64_MAX;
 
     upipe_throw_ready(upipe);
 

include/upipe/Makefile.am
@@ -35,6 +35,7 @@ pkginclude_HEADERS = \
 	upipe_helper_ubuf_mgr.h \
 	upipe_helper_uclock.h \
 	upipe_helper_upipe.h \
+	upipe_helper_upump.h \
 	upipe_helper_upump_mgr.h \
 	upipe_helper_uref_mgr.h \
 	upipe_helper_uref_stream.h \

include/upipe/upipe_helper_uclock.h
@@ -1,9 +1,7 @@
-/*****************************************************************************
- * upipe_helper_uclock.h: upipe helper functions for uclock
- *****************************************************************************
- * Copyright (C) 2012 OpenHeadend S.A.R.L.
+/*
+ * Copyright (C) 2012-2013 OpenHeadend S.A.R.L.
  *
- * Authors: Christophe Massiot <massiot@via.ecp.fr>
+ * Authors: Christophe Massiot
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -23,7 +21,11 @@
  * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *****************************************************************************/
+ */
+
+/** @file
+ * @short Upipe helper functions for uclock
+ */
 
 #ifndef _UPIPE_UPIPE_HELPER_UCLOCK_H_
 /** @hidden */
@@ -41,9 +43,6 @@
  * @code
  *  struct uclock *uclock;
  * @end code
- *
- * You must also declare @ref #UPIPE_HELPER_UPUMP_MGR prior to using this
- * macro.
  *
  * Supposing the name of your structure is upipe_foo, it declares:
  * @list
@@ -83,8 +82,10 @@
  * @param STRUCTURE name of your private upipe structure 
  * @param UCLOCK name of the @tt {struct uclock *} field of
  * your private upipe structure
+ * @param RESET function called when all uclock-related objects must be
+ * reset (struct upipe *)
  */
-#define UPIPE_HELPER_UCLOCK(STRUCTURE, UCLOCK)                              \
+#define UPIPE_HELPER_UCLOCK(STRUCTURE, UCLOCK, RESET)                       \
 /** @internal @This initializes the private members for this helper.        \
  *                                                                          \
  * @param upipe description structure of the pipe                           \
@@ -122,7 +123,7 @@ static bool STRUCTURE##_set_uclock(struct upipe *upipe,                     \
     STRUCTURE->UCLOCK = uclock;                                             \
     if (likely(uclock != NULL))                                             \
         uclock_use(STRUCTURE->UCLOCK);                                      \
-    STRUCTURE##_set_upump(upipe, NULL);                                     \
+    RESET(upipe);                                                           \
     return true;                                                            \
 }                                                                           \
 /** @internal @This cleans up the private members for this helper.          \

include/upipe/upipe_helper_upump.h
@@ -1,0 +1,129 @@
+/*
+ * Copyright (C) 2013 OpenHeadend S.A.R.L.
+ *
+ * Authors: Christophe Massiot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short Upipe helper functions for pumps
+ */
+
+#ifndef _UPIPE_UPIPE_HELPER_UPUMP_H_
+/** @hidden */
+#define _UPIPE_UPIPE_HELPER_UPUMP_H_
+
+#include <upipe/ubase.h>
+#include <upipe/upump.h>
+#include <upipe/upipe.h>
+
+#include <stdbool.h>
+
+/** @This declares three functions dealing with a pump which we suppose serves
+ * as a worker task for the pipe.
+ *
+ * You must add one pointer to your private upipe structure, for instance:
+ * @code
+ *  struct upump *upump;
+ * @end code
+ *
+ * You must also declare @ref #UPIPE_HELPER_UPIPE prior to using this macro.
+ * Also we suppose that you have a upump manager available in your structure.
+ *
+ * Supposing the name of your structure is upipe_foo, and the name of the
+ * pointer is upump, it declares:
+ * @list
+ * @item @code
+ *  void upipe_foo_init_upump(struct upipe *upipe)
+ * @end code
+ * Typically called in your upipe_foo_alloc() function.
+ *
+ * @item @code
+ *  void upipe_foo_set_upump(struct upipe *upipe, struct upump *upump)
+ * @end code
+ * Called whenever you allocate or free the worker.
+ *
+ * @item @code
+ *  void upipe_foo_clean_upump(struct upipe *upipe)
+ * @end code
+ * Typically called from your upipe_foo_free() function.
+ * @end list
+ *
+ * @param STRUCTURE name of your private upipe structure
+ * @param UPUMP name of the @tt{struct upump *} field of
+ * your private upipe structure
+ * @param UPUMP_MGR name of the @tt{struct upump_mgr *} field of
+ * your private upipe structure
+ */
+#define UPIPE_HELPER_UPUMP(STRUCTURE, UPUMP, UPUMP_MGR)                     \
+/** @internal @This initializes the private members for this helper.        \
+ *                                                                          \
+ * @param upipe description structure of the pipe                           \
+ */                                                                         \
+static void STRUCTURE##_init_##UPUMP(struct upipe *upipe)                   \
+{                                                                           \
+    struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
+    s->UPUMP = NULL;                                                        \
+}                                                                           \
+/** @internal @This sets the upump to use.                                  \
+ *                                                                          \
+ * @param upipe description structure of the pipe                           \
+ * @param upump upump structure to use                                      \
+ */                                                                         \
+static void STRUCTURE##_set_##UPUMP(struct upipe *upipe,                    \
+                                    struct upump *upump)                    \
+{                                                                           \
+    struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
+    if (unlikely(s->UPUMP != NULL)) {                                       \
+        upump_stop(s->UPUMP);                                               \
+        upump_free(s->UPUMP);                                               \
+    }                                                                       \
+    s->UPUMP = upump;                                                       \
+}                                                                           \
+/** @internal @This sets the upump to use.                                  \
+ *                                                                          \
+ * @param upipe description structure of the pipe                           \
+ * @param timeout time to wait before waking up                             \
+ */                                                                         \
+static void STRUCTURE##_wait_##UPUMP(struct upipe *upipe, uint64_t timeout, \
+                                     upump_cb cb)                           \
+{                                                                           \
+    struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
+    struct upump *watcher = upump_alloc_timer(s->UPUMP_MGR, cb, upipe,      \
+                                              timeout, 0);                  \
+    if (unlikely(watcher == NULL)) {                                        \
+        upipe_err(upipe, "can't create watcher");                           \
+        upipe_throw_upump_error(upipe);                                     \
+    } else {                                                                \
+        STRUCTURE##_set_##UPUMP(upipe, watcher);                            \
+        upump_start(watcher);                                               \
+    }                                                                       \
+}                                                                           \
+/** @internal @This cleans up the private members for this helper.          \
+ *                                                                          \
+ * @param upipe description structure of the pipe                           \
+ */                                                                         \
+static void STRUCTURE##_clean_##UPUMP(struct upipe *upipe)                  \
+{                                                                           \
+    STRUCTURE##_set_##UPUMP(upipe, NULL);                                   \
+}
+
+#endif

include/upipe/upipe_helper_upump_mgr.h
@@ -1,9 +1,7 @@
-/*****************************************************************************
- * upipe_helper_upump_mgr.h: upipe helper functions for upump manager
- *****************************************************************************
- * Copyright (C) 2012 OpenHeadend S.A.R.L.
+/*
+ * Copyright (C) 2012-2013 OpenHeadend S.A.R.L.
  *
- * Authors: Christophe Massiot <massiot@via.ecp.fr>
+ * Authors: Christophe Massiot
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -23,7 +21,11 @@
  * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *****************************************************************************/
+ */
+
+/** @file
+ * @short Upipe helper functions for upump manager
+ */
 
 #ifndef _UPIPE_UPIPE_HELPER_UPUMP_MGR_H_
 /** @hidden */
@@ -35,13 +37,11 @@
 
 #include <stdbool.h>
 
-/** @This declares five functions dealing with the upump manager, and an
- * associated upump which we suppose serves as a worker task for the pipe.
+/** @This declares four functions dealing with the upump manager.
  *
- * You must add two pointers to your private upipe structure, for instance:
+ * You must add one pointer to your private upipe structure, for instance:
  * @code
  *  struct upump_mgr *upump_mgr;
- *  struct upump *upump;
  * @end code
  *
  * You must also declare @ref #UPIPE_HELPER_UPIPE prior to using this macro.
@@ -52,11 +52,6 @@
  *  void upipe_foo_init_upump_mgr(struct upipe *upipe)
  * @end code
  * Typically called in your upipe_foo_alloc() function.
- *
- * @item @code
- *  void upipe_foo_set_upump(struct upipe *upipe, struct upump *upump)
- * @end code
- * Called whenever you allocate or free a worker.
  *
  * @item @code
  *  bool upipe_foo_get_upump_mgr(struct upipe *upipe, struct upump_mgr **p)
@@ -89,10 +84,10 @@
  * @param STRUCTURE name of your private upipe structure 
  * @param UPUMP_MGR name of the @tt {struct upump_mgr *} field of
  * your private upipe structure
- * @param UPUMP name of the @tt{struct upump *} field of
- * your private upipe structure
+ * @param RESET function called when all upump_mgr-related objects must be
+ * reset (struct upipe *)
  */
-#define UPIPE_HELPER_UPUMP_MGR(STRUCTURE, UPUMP_MGR, UPUMP)                 \
+#define UPIPE_HELPER_UPUMP_MGR(STRUCTURE, UPUMP_MGR, RESET)                 \
 /** @internal @This initializes the private members for this helper.        \
  *                                                                          \
  * @param upipe description structure of the pipe                           \
@@ -101,21 +96,6 @@ static void STRUCTURE##_init_upump_mgr(struct upipe *upipe)                 \
 {                                                                           \
     struct STRUCTURE *STRUCTURE = STRUCTURE##_from_upipe(upipe);            \
     STRUCTURE->UPUMP_MGR = NULL;                                            \
-    STRUCTURE->UPUMP = NULL;                                                \
-}                                                                           \
-/** @internal @This sets the upump to use.                                  \
- *                                                                          \
- * @param upipe description structure of the pipe                           \
- * @param upump upump structure to use                                      \
- */                                                                         \
-static void STRUCTURE##_set_upump(struct upipe *upipe, struct upump *upump) \
-{                                                                           \
-    struct STRUCTURE *STRUCTURE = STRUCTURE##_from_upipe(upipe);            \
-    if (unlikely(STRUCTURE->UPUMP != NULL)) {                               \
-        upump_stop(STRUCTURE->UPUMP);                                       \
-        upump_free(STRUCTURE->UPUMP);                                       \
-    }                                                                       \
-    STRUCTURE->UPUMP = upump;                                               \
 }                                                                           \
 /** @internal @This gets the current upump_mgr.                             \
  *                                                                          \
@@ -141,13 +121,13 @@ static bool STRUCTURE##_set_upump_mgr(struct upipe *upipe,                  \
                                       struct upump_mgr *upump_mgr)          \
 {                                                                           \
     struct STRUCTURE *STRUCTURE = STRUCTURE##_from_upipe(upipe);            \
-    STRUCTURE##_set_upump(upipe, NULL);                                     \
     if (unlikely(STRUCTURE->UPUMP_MGR != NULL))                             \
         upump_mgr_release(STRUCTURE->UPUMP_MGR);                            \
                                                                             \
     STRUCTURE->UPUMP_MGR = upump_mgr;                                       \
     if (likely(upump_mgr != NULL))                                          \
         upump_mgr_use(STRUCTURE->UPUMP_MGR);                                \
+    RESET(upipe);                                                           \
     return true;                                                            \
 }                                                                           \
 /** @internal @This cleans up the private members for this helper.          \
@@ -157,7 +137,6 @@ static bool STRUCTURE##_set_upump_mgr(struct upipe *upipe,                  \
 static void STRUCTURE##_clean_upump_mgr(struct upipe *upipe)                \
 {                                                                           \
     struct STRUCTURE *STRUCTURE = STRUCTURE##_from_upipe(upipe);            \
-    STRUCTURE##_set_upump(upipe, NULL);                                     \
     if (likely(STRUCTURE->UPUMP_MGR != NULL))                               \
         upump_mgr_release(STRUCTURE->UPUMP_MGR);                            \
 }

commit 76fd6478aa1f41989be3632486ee23df2886e657

upipe_avcdec: fix audio, add audio pts
Fix audio bug introduced in 9683efc4, add pts code for
audio frames.

lib/upipe-av/upipe_avcodec_decode.c
@@ -28,7 +28,6 @@
  */
 
 #include <upipe/ubase.h>
-#include <upipe/uclock.h>
 #include <upipe/uprobe.h>
 #include <upipe/ubuf.h>
 #include <upipe/uref.h>
@@ -595,16 +594,17 @@ static bool _upipe_avcdec_set_codec(struct upipe *upipe, const char *codec_def,
     }
 }
 
-/** @internal @This sets the index_rap attribute.
+/** @internal @This sets the various time attributes.
  *
  * @param upipe description structure of the pipe
  * @param uref uref structure
  */
-static void upipe_avcdec_set_index_rap(struct upipe *upipe, struct uref *uref)
+static void upipe_avcdec_set_time_attributes(struct upipe *upipe, struct uref *uref)
 {
     struct upipe_avcdec *upipe_avcdec = upipe_avcdec_from_upipe(upipe);
-    uint64_t rap = 0;
+    uint64_t rap = 0, duration, pts;
 
+    /* rap */
     uref_clock_get_systime_rap(uref, &rap);
     if (unlikely(rap != upipe_avcdec->prev_rap)) {
         upipe_avcdec->prev_rap = rap;
@@ -613,6 +613,23 @@ static void upipe_avcdec_set_index_rap(struct upipe *upipe, struct uref *uref)
     uref_clock_set_index_rap(uref, upipe_avcdec->index_rap);
     upipe_avcdec->index_rap++;
 
+    /* DTS has no meaning from now on */
+    uref_clock_delete_dts(uref);
+
+    /* pts */
+    if (!uref_clock_get_pts(uref, &pts)) {
+        pts = upipe_avcdec->next_pts;
+        if (pts != UINT64_MAX) {
+            uref_clock_set_pts(uref, pts);
+        }
+    }
+
+    /* compute next pts based on current frame duration */
+    if (pts != UINT64_MAX && uref_clock_get_duration(uref, &duration)) {
+        upipe_avcdec->next_pts = pts + duration;
+    } else {
+        upipe_warn(upipe, "couldn't determine next_pts");
+    }
 }
 
 /** @internal @This outputs video frames
@@ -675,24 +692,8 @@ static void upipe_avcdec_output_frame(struct upipe *upipe, AVFrame *frame,
         uref_pic_set_aspect(uref, aspect);
     }
 
-    /* index rap attribute */
-    upipe_avcdec_set_index_rap(upipe, uref);
-
-    /* DTS has no meaning from now on */
-    uref_clock_delete_dts(uref);
-
-    uint64_t pts;
-    if (!uref_clock_get_pts(uref, &pts)) {
-        pts = upipe_avcdec->next_pts;
-        if (pts != UINT64_MAX)
-            uref_clock_set_pts(uref, pts);
-    }
-
-    uint64_t duration;
-    if (pts != UINT64_MAX && uref_clock_get_duration(uref, &duration))
-        upipe_avcdec->next_pts = pts + duration;
-    else
-        upipe_warn(upipe, "couldn't determine next_pts");
+    /* various time-related attribute */
+    upipe_avcdec_set_time_attributes(upipe, uref);
 
     upipe_avcdec_output(upipe, uref, upump);
 }
@@ -707,12 +708,16 @@ static void upipe_avcdec_output_audio(struct upipe *upipe, AVFrame *frame,
                                      struct upump *upump)
 {
     struct ubuf *ubuf;
+    struct uref *uref;
     struct upipe_avcdec *upipe_avcdec = upipe_avcdec_from_upipe(upipe);
-    struct uref *uref = uref_dup(frame->opaque);
     int bufsize = -1, avbufsize;
     size_t size = 0;
     uint8_t *buf;
     AVCodecContext *context = upipe_avcdec->context;
+    
+    /* do NOT duplicate uref since release_buffer() is not called
+     * with audio codecs (and uref would not be released) */
+    uref = frame->opaque;
 
     /* fetch audio sample size (in case it has been reduced) */
     avbufsize = av_samples_get_buffer_size(NULL, context->channels,
@@ -742,8 +747,8 @@ static void upipe_avcdec_output_audio(struct upipe *upipe, AVFrame *frame,
     /* samples in uref */
     uref_sound_flow_set_samples(uref, frame->nb_samples);
 
-    /* index rap attribute */
-    upipe_avcdec_set_index_rap(upipe, uref);
+    /* various time-related attribute */
+    upipe_avcdec_set_time_attributes(upipe, uref);
 
     upipe_avcdec_output(upipe, uref, upump);
 }