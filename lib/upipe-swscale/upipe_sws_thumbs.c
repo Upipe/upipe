04dc37d29fd020845cd168c21aaf51fbe9d2e738
Christophe Massiot <massiot@via.ecp.fr>
2013-09-11 15:16:09 +0200

    change sws algorithm to gauss

--- lib/upipe-swscale/upipe_sws_thumbs.c
+++ lib/upipe-swscale/upipe_sws_thumbs.c
@@ -117,7 +117,7 @@ static inline bool upipe_sws_thumbs_set_context(struct upipe *upipe,
     upipe_sws_thumbs->convert_ctx = sws_getCachedContext(upipe_sws_thumbs->convert_ctx,
                 srcsize->hsize, srcsize->vsize, *upipe_sws_thumbs->srcfmt->pixfmt,
                 dstsize->hsize, dstsize->vsize, *upipe_sws_thumbs->dstfmt->pixfmt,
-                SWS_BICUBIC, NULL, NULL, NULL);
+                SWS_GAUSS, NULL, NULL, NULL);
 
     if (unlikely(!upipe_sws_thumbs->convert_ctx)) {
         upipe_err(upipe, "could not get swscale context");
