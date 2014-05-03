660db8c0813cf2260a7d97b26482ce9203feb91d
Christophe Massiot <cmassiot@openheadend.tv>
2014-05-03 18:07:15 +0200

    new RTP decaps module

---
+++ lib/upipe-modules/upipe_rtp_decaps.c
@@ -0,0 +1,285 @@
+/*
+ * Copyright (C) 2014 OpenHeadend S.A.R.L.
+ *
+ * Authors: Christophe Massiot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short Upipe module decapsulating RTP header from blocks
+ */
+
+#include <upipe/ubase.h>
+#include <upipe/uprobe.h>
+#include <upipe/uref.h>
+#include <upipe/upipe.h>
+#include <upipe/uref_block.h>
+#include <upipe/uref_block_flow.h>
+#include <upipe/uref_flow.h>
+#include <upipe/upipe.h>
+#include <upipe/upipe_helper_upipe.h>
+#include <upipe/upipe_helper_urefcount.h>
+#include <upipe/upipe_helper_void.h>
+#include <upipe/upipe_helper_output.h>
+#include <upipe-modules/upipe_rtp_decaps.h>
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+#include <assert.h>
+
+#include <bitstream/ietf/rtp.h>
+
+#define EXPECTED_FLOW_DEF "block."
+
+/** upipe_rtpd structure */ 
+struct upipe_rtpd {
+    /** refcount management structure */
+    struct urefcount urefcount;
+
+    /** expected sequence number */
+    int expected_seqnum;
+    /** current RTP type */
+    uint8_t type;
+
+    /** output pipe */
+    struct upipe *output;
+    /** input flow definition packet */
+    struct uref *flow_def_input;
+    /** flow definition packet */
+    struct uref *flow_def;
+    /** true if the flow definition has already been sent */
+    bool flow_def_sent;
+
+    /** public upipe structure */
+    struct upipe upipe;
+};
+
+UPIPE_HELPER_UPIPE(upipe_rtpd, upipe, UPIPE_RTPD_SIGNATURE);
+UPIPE_HELPER_UREFCOUNT(upipe_rtpd, urefcount, upipe_rtpd_free)
+UPIPE_HELPER_VOID(upipe_rtpd);
+UPIPE_HELPER_OUTPUT(upipe_rtpd, output, flow_def, flow_def_sent);
+
+/** @internal @This allocates a rtpd pipe.
+ *
+ * @param mgr common management structure
+ * @param uprobe structure used to raise events
+ * @param signature signature of the pipe allocator
+ * @param args optional arguments
+ * @return pointer to upipe or NULL in case of allocation error
+ */
+static struct upipe *upipe_rtpd_alloc(struct upipe_mgr *mgr,
+                                      struct uprobe *uprobe,
+                                      uint32_t signature, va_list args)
+{
+    struct upipe *upipe = upipe_rtpd_alloc_void(mgr, uprobe, signature, args);
+    if (unlikely(upipe == NULL))
+        return NULL;
+
+    struct upipe_rtpd *upipe_rtpd = upipe_rtpd_from_upipe(upipe);
+    upipe_rtpd_init_urefcount(upipe);
+    upipe_rtpd_init_output(upipe);
+    upipe_rtpd->expected_seqnum = -1;
+    upipe_rtpd->type = 0;
+    upipe_rtpd->flow_def_input = NULL;
+
+    upipe_throw_ready(upipe);
+    return upipe;
+}
+
+/** @internal @This handles data.
+ *
+ * @param upipe description structure of the pipe
+ * @param uref uref structure
+ * @param upump_p reference to pump that generated the buffer
+ */
+static inline void upipe_rtpd_input(struct upipe *upipe, struct uref *uref,
+                                    struct upump **upump_p)
+{
+    struct upipe_rtpd *upipe_rtpd = upipe_rtpd_from_upipe(upipe);
+    uint8_t rtp_buffer[RTP_HEADER_SIZE];
+    const uint8_t *rtp_header = uref_block_peek(uref, 0, RTP_HEADER_SIZE, 
+                                                rtp_buffer);
+    if (unlikely(rtp_header == NULL)) {
+        upipe_warn(upipe, "invalid buffer received");
+        uref_free(uref);
+        return;
+    }
+
+    bool valid = rtp_check_hdr(rtp_header);
+    bool extension = rtp_check_extension(rtp_header);
+    uint8_t cc = rtp_get_cc(rtp_header);
+    uint8_t type = rtp_get_type(rtp_header);
+    uint16_t seqnum = rtp_get_seqnum(rtp_header);
+    ptrdiff_t extension_offset = rtp_extension((uint8_t *)rtp_header) -
+                                 rtp_header;
+    uref_block_peek_unmap(uref, 0, rtp_buffer, rtp_header);
+
+    if (unlikely(!valid)) {
+        upipe_warn(upipe, "invalid RTP header");
+        uref_free(uref);
+        return;
+    }
+
+    size_t offset = RTP_HEADER_SIZE + 4 * cc;
+
+    if (extension) {
+        rtp_header = uref_block_peek(uref, extension_offset,
+                                     RTP_EXTENSION_SIZE, rtp_buffer);
+        if (unlikely(rtp_header == NULL)) {
+            upipe_warn(upipe, "invalid buffer received");
+            uref_free(uref);
+            return;
+        }
+        offset += 4 * (1 + rtpx_get_length(rtp_header));
+        uref_block_peek_unmap(uref, extension_offset, rtp_buffer, rtp_header);
+    }
+
+    if (unlikely(upipe_rtpd->expected_seqnum != -1 &&
+                 seqnum != upipe_rtpd->expected_seqnum)) {
+        upipe_warn_va(upipe, "potentially lost %d RTP packets",
+                      (seqnum + UINT16_MAX - upipe_rtpd->expected_seqnum) &
+                      UINT16_MAX);
+        uref_flow_set_discontinuity(uref);
+    }
+    upipe_rtpd->expected_seqnum = seqnum + 1;
+    upipe_rtpd->expected_seqnum &= UINT16_MAX;
+
+    if (unlikely(type != upipe_rtpd->type)) {
+        assert(upipe_rtpd->flow_def_input != NULL);
+        struct uref *flow_def = uref_dup(upipe_rtpd->flow_def_input);
+        if (unlikely(flow_def == NULL)) {
+            uref_free(uref);
+            upipe_throw_fatal(upipe, UBASE_ERR_ALLOC);
+            return;
+        }
+
+        switch (type) {
+            case RTP_TYPE_TS:
+                uref_flow_set_def(flow_def, "block.mpegtsaligned.");
+                break;
+            default:
+                break;
+        }
+        upipe_rtpd->type = type;
+        upipe_rtpd_store_flow_def(upipe, flow_def);
+    }
+
+    uref_block_resize(uref, offset, -1);
+    upipe_rtpd_output(upipe, uref, upump_p);
+}
+
+/** @internal @This sets the input flow definition.
+ *
+ * @param upipe description structure of the pipe
+ * @param flow_def flow definition packet
+ * @return an error code
+ */
+static int upipe_rtpd_set_flow_def(struct upipe *upipe, struct uref *flow_def)
+{
+    if (flow_def == NULL)
+        return UBASE_ERR_INVALID;
+    UBASE_RETURN(uref_flow_match_def(flow_def, EXPECTED_FLOW_DEF))
+    struct uref *flow_def_dup;
+    if (unlikely((flow_def_dup = uref_dup(flow_def)) == NULL))
+        return UBASE_ERR_ALLOC;
+    struct upipe_rtpd *upipe_rtpd = upipe_rtpd_from_upipe(upipe);
+    uref_free(upipe_rtpd->flow_def_input);
+    upipe_rtpd->flow_def_input = flow_def_dup;
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This processes control commands on a rtpd pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @param command type of command to process
+ * @param args arguments of the command
+ * @return an error code
+ */
+static int upipe_rtpd_control(struct upipe *upipe, int command, va_list args)
+{
+    switch (command) {
+        case UPIPE_AMEND_FLOW_FORMAT: {
+            struct uref *flow_format = va_arg(args, struct uref *);
+            return upipe_throw_new_flow_format(upipe, flow_format, NULL);
+        }
+        case UPIPE_GET_FLOW_DEF: {
+            struct uref **p = va_arg(args, struct uref **);
+            return upipe_rtpd_get_flow_def(upipe, p);
+        }
+        case UPIPE_SET_FLOW_DEF: {
+            struct uref *flow_def = va_arg(args, struct uref *);
+            return upipe_rtpd_set_flow_def(upipe, flow_def);
+        }
+        case UPIPE_GET_OUTPUT: {
+            struct upipe **p = va_arg(args, struct upipe **);
+            return upipe_rtpd_get_output(upipe, p);
+        }
+        case UPIPE_SET_OUTPUT: {
+            struct upipe *output = va_arg(args, struct upipe *);
+            return upipe_rtpd_set_output(upipe, output);
+        }
+        default:
+            return UBASE_ERR_UNHANDLED;
+    }
+}
+
+/** @internal @This frees all resources allocated.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_rtpd_free(struct upipe *upipe)
+{
+    struct upipe_rtpd *upipe_rtpd = upipe_rtpd_from_upipe(upipe);
+    upipe_dbg_va(upipe, "releasing pipe %p", upipe);
+    upipe_throw_dead(upipe);
+
+    uref_free(upipe_rtpd->flow_def_input);
+    upipe_rtpd_clean_output(upipe);
+    upipe_rtpd_clean_urefcount(upipe);
+    upipe_rtpd_free_void(upipe);
+}
+
+static struct upipe_mgr upipe_rtpd_mgr = {
+    .refcount = NULL,
+    .signature = UPIPE_RTPD_SIGNATURE,
+
+    .upipe_alloc = upipe_rtpd_alloc,
+    .upipe_input = upipe_rtpd_input,
+    .upipe_control = upipe_rtpd_control,
+
+    .upipe_mgr_control = NULL
+};
+
+/** @This returns the management structure for rtpd pipes.
+ *
+ * @return pointer to manager
+ */
+struct upipe_mgr *upipe_rtpd_mgr_alloc(void)
+{
+    return &upipe_rtpd_mgr;
+}
---
+++ tests/upipe_rtp_decaps_test.c
@@ -0,0 +1,223 @@
+/*
+ * Copyright (C) 2014 OpenHeadend S.A.R.L.
+ *
+ * Authors: Christophe Massiot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#undef NDEBUG
+
+#include <upipe/uprobe.h>
+#include <upipe/uprobe_stdio.h>
+#include <upipe/uprobe_prefix.h>
+#include <upipe/umem.h>
+#include <upipe/umem_alloc.h>
+#include <upipe/udict.h>
+#include <upipe/udict_inline.h>
+#include <upipe/uref.h>
+#include <upipe/uref_std.h>
+#include <upipe/uref_block.h>
+#include <upipe/uref_block_flow.h>
+#include <upipe/ubuf_block_mem.h>
+#include <upipe/upipe.h>
+#include <upipe/upipe_helper_upipe.h>
+#include <upipe-modules/upipe_rtp_decaps.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include <bitstream/ietf/rtp.h>
+
+#define UDICT_POOL_DEPTH    0
+#define UREF_POOL_DEPTH     0
+#define UBUF_POOL_DEPTH     0
+#define UBUF_PREPEND        0
+#define UBUF_APPEND         0
+#define UBUF_ALIGN          32
+#define UBUF_ALIGN_OFFSET   0
+#define SIZE                1328
+
+#define UPROBE_LOG_LEVEL UPROBE_LOG_DEBUG
+
+static unsigned int nb_packets = 0;
+static bool expect_discontinuity = false;
+
+/** helper phony pipe to test upipe_rtpd */
+static struct upipe *rtpd_test_alloc(struct upipe_mgr *mgr,
+                                     struct uprobe *uprobe,
+                                     uint32_t signature, va_list args)
+{
+    struct upipe *upipe = malloc(sizeof(struct upipe));
+    assert(upipe != NULL);
+    upipe_init(upipe, mgr, uprobe);
+    upipe_throw_ready(upipe);
+    return upipe;
+}
+
+/** helper phony pipe to test upipe_rtpd */
+static void rtpd_test_input(struct upipe *upipe, struct uref *uref,
+                            struct upump **upump_p)
+{
+    const uint8_t *buf;
+    int size;
+
+    size = -1;
+    ubase_assert(uref_block_read(uref, 0, &size, &buf));
+    assert(size == SIZE - RTP_HEADER_SIZE);
+    uref_block_unmap(uref, 0);
+    assert(ubase_check(uref_flow_get_discontinuity(uref)) ==
+           expect_discontinuity);
+    nb_packets--;
+    uref_free(uref);
+}
+
+/** helper phony pipe to test upipe_rtpd */
+static void rtpd_test_free(struct upipe *upipe)
+{
+    upipe_throw_dead(upipe);
+    upipe_clean(upipe);
+    free(upipe);
+}
+
+/** helper phony pipe to test upipe_rtpd */
+static struct upipe_mgr rtpd_test_mgr = {
+    .refcount = NULL,
+    .signature = 0,
+    .upipe_alloc = rtpd_test_alloc,
+    .upipe_input = rtpd_test_input,
+    .upipe_control = NULL
+};
+
+/** definition of our uprobe */
+static int catch(struct uprobe *uprobe, struct upipe *upipe, int event, va_list args)
+{
+    switch (event) {
+        case UPROBE_READY:
+        case UPROBE_DEAD:
+            break;
+        case UPROBE_NEW_FLOW_DEF: {
+            struct uref *flow_def = va_arg(args, struct uref *);
+            const char *def;
+            ubase_assert(uref_flow_get_def(flow_def, &def));
+            assert(!strcmp(def, "block.mpegtsaligned."));
+            break;
+        }
+        default:
+            assert(0);
+            break;
+    }
+    return UBASE_ERR_NONE;
+}
+
+int main(int argc, char **argv)
+{
+    printf("Compiled %s %s - %s\n", __DATE__, __TIME__, __FILE__);
+    struct uref *uref = NULL;
+    uint8_t *buf;
+    int size;
+
+    /* uref and mem management */
+    struct umem_mgr *umem_mgr = umem_alloc_mgr_alloc();
+    assert(umem_mgr != NULL);
+    struct udict_mgr *udict_mgr = udict_inline_mgr_alloc(UDICT_POOL_DEPTH, umem_mgr, -1, -1);
+    assert(udict_mgr != NULL);
+    struct uref_mgr *uref_mgr = uref_std_mgr_alloc(UREF_POOL_DEPTH, udict_mgr, 0); 
+    assert(uref_mgr != NULL);
+
+    /* block */
+    struct ubuf_mgr *block_mgr = ubuf_block_mem_mgr_alloc(UBUF_POOL_DEPTH,
+            UBUF_POOL_DEPTH, umem_mgr,
+            UBUF_ALIGN,
+            UBUF_ALIGN_OFFSET);
+    assert(block_mgr);
+
+    /* uprobe stuff */
+    struct uprobe uprobe;
+    uprobe_init(&uprobe, catch, NULL);
+    struct uprobe *uprobe_stdio = uprobe_stdio_alloc(&uprobe, stdout,
+                                                     UPROBE_LOG_DEBUG);
+    assert(uprobe_stdio != NULL);
+
+    uref = uref_block_flow_alloc_def(uref_mgr, NULL);
+    assert(uref);
+
+    /* build rtpd pipe */
+    struct upipe_mgr *upipe_rtpd_mgr = upipe_rtpd_mgr_alloc();
+    assert(upipe_rtpd_mgr);
+    struct upipe *rtpd = upipe_void_alloc(upipe_rtpd_mgr,
+                uprobe_pfx_alloc(uprobe_use(uprobe_stdio), UPROBE_LOG_LEVEL,
+                                 "rtpd"));
+    assert(rtpd);
+    ubase_assert(upipe_set_flow_def(rtpd, uref));
+    uref_free(uref);
+
+    /* rtpd_test */
+    struct upipe *rtpd_test = upipe_void_alloc(&rtpd_test_mgr,
+            uprobe_pfx_alloc(uprobe_use(uprobe_stdio), UPROBE_LOG_LEVEL,
+                             "rtpdtest"));
+    assert(rtpd_test != NULL);
+    ubase_assert(upipe_set_output(rtpd, rtpd_test));
+    upipe_release(rtpd_test);
+
+    /* Now send uref */
+    uref = uref_block_alloc(uref_mgr, block_mgr, SIZE);
+    size = -1;
+    uref_block_write(uref, 0, &size, &buf);
+    rtp_set_hdr(buf);
+    rtp_set_type(buf, RTP_TYPE_TS);
+    rtp_set_seqnum(buf, 1);
+    rtp_set_timestamp(buf, 0);
+    uref_block_unmap(uref, 0);
+    nb_packets = 1;
+    upipe_input(rtpd, uref, NULL);
+    assert(!nb_packets);
+
+    uref = uref_block_alloc(uref_mgr, block_mgr, SIZE);
+    size = -1;
+    uref_block_write(uref, 0, &size, &buf);
+    rtp_set_hdr(buf);
+    rtp_set_type(buf, RTP_TYPE_TS);
+    rtp_set_seqnum(buf, 42);
+    rtp_set_timestamp(buf, 0);
+    uref_block_unmap(uref, 0);
+    expect_discontinuity = true;
+    nb_packets = 1;
+    upipe_input(rtpd, uref, NULL);
+    assert(!nb_packets);
+
+    /* release pipe */
+    upipe_release(rtpd);
+    rtpd_test_free(rtpd_test);
+
+    /* release managers */
+    upipe_mgr_release(upipe_rtpd_mgr); // no-op
+    uref_mgr_release(uref_mgr);
+    ubuf_mgr_release(block_mgr);
+    umem_mgr_release(umem_mgr);
+    udict_mgr_release(udict_mgr);
+    uprobe_release(uprobe_stdio);
+    uprobe_clean(&uprobe);
+
+    return 0;
+}
