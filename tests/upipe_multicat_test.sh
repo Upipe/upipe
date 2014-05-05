07ca8830f1aaf1e87058ce8e56f277a3113b1410
Benjamin Cohen <bencoh@notk.org>
2014-05-05 15:54:46 +0000

    valgrind: suppress spurious libev errors

--- tests/upipe_multicat_test.sh
+++ tests/upipe_multicat_test.sh
@@ -13,9 +13,15 @@ if ! which valgrind >/dev/null 2>&1; then
 	exit 1
 fi
 
+# valgrind suppressions
+VALGRIND_SUPPRESSIONS=" --suppressions=$srcdir/valgrind.supp "
+if [ "$UNAME" == "Darwin" ]; then
+    VALGRIND_SUPPRESSIONS+=" --suppressions=$srcdir/valgrind_osx.supp "
+fi
+
 mkdir -p "$TMP"/test2
 # Run in valgrind, with leak checking enabled
-libtool --mode=execute valgrind -q --leak-check=full ./upipe_multicat_test "$TMP"/test2/ .bar > /dev/null 2> "$TMP"/logs
+libtool --mode=execute valgrind -q --leak-check=full $VALGRIND_SUPPRESSIONS ./upipe_multicat_test "$TMP"/test2/ .bar > /dev/null 2> "$TMP"/logs
 RET=$?
 if test -s "$TMP"/logs; then
         cat "$TMP"/logs >&2
