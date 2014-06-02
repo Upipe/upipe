c4a90313bf6e2afd732cbc64503169470026fc86
Christophe Massiot <cmassiot@openheadend.tv>
2014-06-02 11:59:14 +0200

    new idempotent module

---
+++ lib/upipe-modules/upipe_idem.c
@@ -0,0 +1,186 @@
+/*
+ * Copyright (C) 2014 OpenHeadend S.A.R.L.
+ *
+ * Authors: Christophe Massiot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short Upipe linear module sending output identical to input
+ */
+
+#include <upipe/ubase.h>
+#include <upipe/uprobe.h>
+#include <upipe/uref.h>
+#include <upipe/upipe.h>
+#include <upipe/upipe.h>
+#include <upipe/upipe_helper_upipe.h>
+#include <upipe/upipe_helper_urefcount.h>
+#include <upipe/upipe_helper_void.h>
+#include <upipe/upipe_helper_output.h>
+#include <upipe-modules/upipe_idem.h>
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <assert.h>
+
+/** upipe_idem structure */ 
+struct upipe_idem {
+    /** refcount management structure */
+    struct urefcount urefcount;
+
+    /** output pipe */
+    struct upipe *output;
+    /** flow_definition packet */
+    struct uref *flow_def;
+    /** true if the flow definition has already been sent */
+    bool flow_def_sent;
+
+    /** public upipe structure */
+    struct upipe upipe;
+};
+
+UPIPE_HELPER_UPIPE(upipe_idem, upipe, UPIPE_IDEM_SIGNATURE);
+UPIPE_HELPER_UREFCOUNT(upipe_idem, urefcount, upipe_idem_free)
+UPIPE_HELPER_VOID(upipe_idem);
+UPIPE_HELPER_OUTPUT(upipe_idem, output, flow_def, flow_def_sent)
+
+/** @internal @This handles data.
+ *
+ * @param upipe description structure of the pipe
+ * @param uref uref structure
+ * @param upump_p reference to pump that generated the buffer
+ */
+static inline void upipe_idem_input(struct upipe *upipe, struct uref *uref,
+                                    struct upump **upump_p)
+{
+    upipe_idem_output(upipe, uref, upump_p);
+}
+
+/** @internal @This sets the input flow definition.
+ *
+ * @param upipe description structure of the pipe
+ * @param flow_def flow definition packet
+ * @return an error code
+ */
+static int upipe_idem_set_flow_def(struct upipe *upipe, struct uref *flow_def)
+{
+    if (flow_def == NULL)
+        return UBASE_ERR_INVALID;
+    struct uref *flow_def_dup;
+    if (unlikely((flow_def_dup = uref_dup(flow_def)) == NULL))
+        return UBASE_ERR_ALLOC;
+    upipe_idem_store_flow_def(upipe, flow_def_dup);
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This processes control commands on an idem pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @param command type of command to process
+ * @param args arguments of the command
+ * @return an error code
+ */
+static int upipe_idem_control(struct upipe *upipe, int command, va_list args)
+{
+    switch (command) {
+        case UPIPE_AMEND_FLOW_FORMAT: {
+            struct uref *flow_format = va_arg(args, struct uref *);
+            return upipe_throw_new_flow_format(upipe, flow_format, NULL);
+        }
+        case UPIPE_GET_FLOW_DEF: {
+            struct uref **p = va_arg(args, struct uref **);
+            return upipe_idem_get_flow_def(upipe, p);
+        }
+        case UPIPE_SET_FLOW_DEF: {
+            struct uref *flow_def = va_arg(args, struct uref *);
+            return upipe_idem_set_flow_def(upipe, flow_def);
+        }
+        case UPIPE_GET_OUTPUT: {
+            struct upipe **p = va_arg(args, struct upipe **);
+            return upipe_idem_get_output(upipe, p);
+        }
+        case UPIPE_SET_OUTPUT: {
+            struct upipe *output = va_arg(args, struct upipe *);
+            return upipe_idem_set_output(upipe, output);
+        }
+        default:
+            return UBASE_ERR_UNHANDLED;
+    }
+}
+
+/** @internal @This allocates an idem pipe.
+ *
+ * @param mgr common management structure
+ * @param uprobe structure used to raise events
+ * @param signature signature of the pipe allocator
+ * @param args optional arguments
+ * @return pointer to upipe or NULL in case of allocation error
+ */
+static struct upipe *upipe_idem_alloc(struct upipe_mgr *mgr,
+                                      struct uprobe *uprobe,
+                                      uint32_t signature, va_list args)
+{
+    struct upipe *upipe = upipe_idem_alloc_void(mgr, uprobe, signature, args);
+    if (unlikely(upipe == NULL))
+        return NULL;
+
+    upipe_idem_init_urefcount(upipe);
+    upipe_idem_init_output(upipe);
+
+    upipe_throw_ready(upipe);
+    return upipe;
+}
+
+/** @internal @This frees all resources allocated.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_idem_free(struct upipe *upipe)
+{
+    upipe_throw_dead(upipe);
+
+    upipe_idem_clean_output(upipe);
+    upipe_idem_clean_urefcount(upipe);
+    upipe_idem_free_void(upipe);
+}
+
+static struct upipe_mgr upipe_idem_mgr = {
+    .refcount = NULL,
+    .signature = UPIPE_IDEM_SIGNATURE,
+
+    .upipe_alloc = upipe_idem_alloc,
+    .upipe_input = upipe_idem_input,
+    .upipe_control = upipe_idem_control,
+
+    .upipe_mgr_control = NULL
+};
+
+/** @This returns the management structure for idem pipes.
+ *
+ * @return pointer to manager
+ */
+struct upipe_mgr *upipe_idem_mgr_alloc(void)
+{
+    return &upipe_idem_mgr;
+}
