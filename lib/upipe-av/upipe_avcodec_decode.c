8dbb0342c2bcb5ecafa4012c0f0e0ed176f8d20d
Benjamin Cohen <bencoh@notk.org>
2014-08-25 11:16:15 +0200

    upipe-avcdec: force p.align to 16 bytes

--- lib/upipe-av/upipe_avcodec_decode.c
+++ lib/upipe-av/upipe_avcodec_decode.c
@@ -237,6 +237,7 @@ static int upipe_avcdec_get_buffer_pic(struct AVCodecContext *context,
         return -1;
     }
 
+    UBASE_FATAL(upipe, uref_pic_flow_set_align(flow_def_attr, 16))
     UBASE_FATAL(upipe, uref_pic_flow_set_hsize(flow_def_attr, context->width))
     UBASE_FATAL(upipe, uref_pic_flow_set_vsize(flow_def_attr, context->height))
     UBASE_FATAL(upipe, uref_pic_flow_set_hsize_visible(flow_def_attr, context->width))
