Upipe/upipe

commit 37326e0abc7f4e8f434905fc73f4f3f1a6076c1f

inter-pipe communication using urequests
Urequests allow to receive uref_mgr, ubuf_mgr, uclock and amended flow format
asynchronously. See the documentation for more details.

lib/upipe-ts/upipe_ts_psi_inserter.c
@@ -78,8 +78,10 @@ struct upipe_ts_psii {
     struct upipe *output;
     /** output flow definition packet */
     struct uref *flow_def;
-    /** true if the flow definition has already been sent */
-    bool flow_def_sent;
+    /** output state */
+    enum upipe_helper_output_state output_state;
+    /** list of output requests */
+    struct uchain request_list;
 
     /** list of input subpipes */
     struct uchain subs;
@@ -94,7 +96,7 @@ struct upipe_ts_psii {
 UPIPE_HELPER_UPIPE(upipe_ts_psii, upipe, UPIPE_TS_PSII_SIGNATURE)
 UPIPE_HELPER_UREFCOUNT(upipe_ts_psii, urefcount, upipe_ts_psii_free)
 UPIPE_HELPER_VOID(upipe_ts_psii)
-UPIPE_HELPER_OUTPUT(upipe_ts_psii, output, flow_def, flow_def_sent)
+UPIPE_HELPER_OUTPUT(upipe_ts_psii, output, flow_def, output_state, request_list)
 
 /** @internal @This is the private context of a program of a ts_psii pipe. */
 struct upipe_ts_psii_sub {
@@ -357,6 +359,20 @@ static int upipe_ts_psii_sub_control(struct upipe *upipe,
                                      int command, va_list args)
 {
     switch (command) {
+        case UPIPE_REGISTER_REQUEST: {
+            struct upipe_ts_psii *upipe_ts_psii =
+                upipe_ts_psii_from_sub_mgr(upipe->mgr);
+            struct urequest *request = va_arg(args, struct urequest *);
+            return upipe_ts_psii_alloc_output_proxy(
+                    upipe_ts_psii_to_upipe(upipe_ts_psii), request);
+        }
+        case UPIPE_UNREGISTER_REQUEST: {
+            struct upipe_ts_psii *upipe_ts_psii =
+                upipe_ts_psii_from_sub_mgr(upipe->mgr);
+            struct urequest *request = va_arg(args, struct urequest *);
+            return upipe_ts_psii_free_output_proxy(
+                    upipe_ts_psii_to_upipe(upipe_ts_psii), request);
+        }
         case UPIPE_SET_FLOW_DEF: {
             struct uref *flow_def = va_arg(args, struct uref *);
             return upipe_ts_psii_sub_set_flow_def(upipe, flow_def);
@@ -511,9 +527,7 @@ static int upipe_ts_psii_set_flow_def(struct upipe *upipe,
     upipe_ts_psii_store_flow_def(upipe, flow_def_dup);
     /* Force sending it immediately, because subpipes also send to output
      * without passing by our helper. */
-    struct upipe_ts_psii *upipe_ts_psii = upipe_ts_psii_from_upipe(upipe);
-    upipe_ts_psii->flow_def_sent = true;
-    upipe_throw_new_flow_def(upipe, upipe_ts_psii->flow_def);
+    upipe_ts_psii_output(upipe, NULL, NULL);
     return UBASE_ERR_NONE;
 }
 
@@ -548,6 +562,14 @@ static int upipe_ts_psii_control(struct upipe *upipe,
                                  int command, va_list args)
 {
     switch (command) {
+        case UPIPE_REGISTER_REQUEST: {
+            struct urequest *request = va_arg(args, struct urequest *);
+            return upipe_ts_psii_alloc_output_proxy(upipe, request);
+        }
+        case UPIPE_UNREGISTER_REQUEST: {
+            struct urequest *request = va_arg(args, struct urequest *);
+            return upipe_ts_psii_free_output_proxy(upipe, request);
+        }
         case UPIPE_GET_FLOW_DEF: {
             struct uref **p = va_arg(args, struct uref **);
             return upipe_ts_psii_get_flow_def(upipe, p);

tests/upipe_ts_psi_inserter_test.c
@@ -82,10 +82,9 @@ static int catch(struct uprobe *uprobe, struct upipe *upipe,
     return UBASE_ERR_NONE;
 }
 
-/** helper phony pipe to test upipe_ts_psii */
-static struct upipe *ts_test_alloc(struct upipe_mgr *mgr,
-                                   struct uprobe *uprobe, uint32_t signature,
-                                   va_list args)
+/** helper phony pipe */
+static struct upipe *test_alloc(struct upipe_mgr *mgr, struct uprobe *uprobe,
+                                uint32_t signature, va_list args)
 {
     struct upipe *upipe = malloc(sizeof(struct upipe));
     assert(upipe != NULL);
@@ -93,28 +92,46 @@ static struct upipe *ts_test_alloc(struct upipe_mgr *mgr,
     return upipe;
 }
 
-/** helper phony pipe to test upipe_ts_psii */
-static void ts_test_input(struct upipe *upipe, struct uref *uref,
-                          struct upump **upump_p)
+/** helper phony pipe */
+static void test_input(struct upipe *upipe, struct uref *uref,
+                       struct upump **upump_p)
 {
     assert(uref != NULL);
     nb_packets++;
     uref_free(uref);
 }
 
-/** helper phony pipe to test upipe_ts_psii */
-static void ts_test_free(struct upipe *upipe)
+/** helper phony pipe */
+static int test_control(struct upipe *upipe, int command, va_list args)
+{
+    switch (command) {
+        case UPIPE_SET_FLOW_DEF:
+            return UBASE_ERR_NONE;
+        case UPIPE_REGISTER_REQUEST: {
+            struct urequest *urequest = va_arg(args, struct urequest *);
+            return upipe_throw_provide_request(upipe, urequest);
+        }
+        case UPIPE_UNREGISTER_REQUEST:
+            return UBASE_ERR_NONE;
+        default:
+            assert(0);
+            return UBASE_ERR_UNHANDLED;
+    }
+}
+
+/** helper phony pipe */
+static void test_free(struct upipe *upipe)
 {
     upipe_clean(upipe);
     free(upipe);
 }
 
-/** helper phony pipe to test upipe_ts_psii */
+/** helper phony pipe */
 static struct upipe_mgr ts_test_mgr = {
     .refcount = NULL,
-    .upipe_alloc = ts_test_alloc,
-    .upipe_input = ts_test_input,
-    .upipe_control = NULL
+    .upipe_alloc = test_alloc,
+    .upipe_input = test_input,
+    .upipe_control = test_control
 };
 
 int main(int argc, char *argv[])
@@ -156,9 +173,9 @@ int main(int argc, char *argv[])
             uprobe_pfx_alloc(uprobe_use(logger), UPROBE_LOG_LEVEL,
                              "ts psii"));
     assert(upipe_ts_psii != NULL);
+    ubase_assert(upipe_set_output(upipe_ts_psii, upipe_sink));
     ubase_assert(upipe_set_flow_def(upipe_ts_psii, uref));
     uref_free(uref);
-    ubase_assert(upipe_set_output(upipe_ts_psii, upipe_sink));
 
     uref = uref_block_flow_alloc_def(uref_mgr, "mpegtspsi.");
     assert(uref != NULL);
@@ -212,7 +229,7 @@ int main(int argc, char *argv[])
 
     upipe_mgr_release(upipe_ts_psii_mgr); // nop
 
-    ts_test_free(upipe_sink);
+    test_free(upipe_sink);
 
     uref_mgr_release(uref_mgr);
     ubuf_mgr_release(ubuf_mgr);

commit 7dac423534a35a369644244367a7f2ec51f2324c

fix wrong latency in ts mux
Bug introduced in 37326e0abc7f4e8f434905fc73f4f3f1a6076c1f.

include/upipe-ts/upipe_ts_psi_inserter.h
@@ -33,6 +33,7 @@ extern "C" {
 
 #define UPIPE_TS_PSII_SIGNATURE UBASE_FOURCC('t','P','i',' ')
 #define UPIPE_TS_PSII_SUB_SIGNATURE UBASE_FOURCC('t','P','i','s')
+#define UPIPE_TS_PSII_INNER_SINK_SIGNATURE UBASE_FOURCC('t','P','i','S')
 
 /** @This extends upipe_command with specific commands for ts_psii_sub. */
 enum upipe_ts_psii_sub_command {

lib/upipe-ts/upipe_ts_psi_inserter.c
@@ -83,6 +83,11 @@ struct upipe_ts_psii {
     /** list of output requests */
     struct uchain request_list;
 
+    /** manager of the pseudo inner sink */
+    struct upipe_mgr inner_sink_mgr;
+    /** pseudo inner sink to get urefs from ts_encaps */
+    struct upipe inner_sink;
+
     /** list of input subpipes */
     struct uchain subs;
 
@@ -97,6 +102,8 @@ UPIPE_HELPER_UPIPE(upipe_ts_psii, upipe, UPIPE_TS_PSII_SIGNATURE)
 UPIPE_HELPER_UREFCOUNT(upipe_ts_psii, urefcount, upipe_ts_psii_free)
 UPIPE_HELPER_VOID(upipe_ts_psii)
 UPIPE_HELPER_OUTPUT(upipe_ts_psii, output, flow_def, output_state, request_list)
+
+UBASE_FROM_TO(upipe_ts_psii, upipe, inner_sink, inner_sink)
 
 /** @internal @This is the private context of a program of a ts_psii pipe. */
 struct upipe_ts_psii_sub {
@@ -207,7 +214,7 @@ static struct upipe *upipe_ts_psii_sub_alloc(struct upipe_mgr *mgr,
         return upipe;
     }
     if (likely(upipe_ts_psii->output != NULL))
-        upipe_set_output(upipe_ts_psii_sub->encaps, upipe_ts_psii->output);
+        upipe_set_output(upipe_ts_psii_sub->encaps, &upipe_ts_psii->inner_sink);
 
     return upipe;
 }
@@ -448,6 +455,83 @@ static void upipe_ts_psii_init_sub_mgr(struct upipe *upipe)
     sub_mgr->upipe_mgr_control = NULL;
 }
 
+/** @internal @This receives data.
+ *
+ * @param sink description structure of the pipe
+ * @param uref uref structure
+ * @param upump_p reference to pump that generated the buffer
+ */
+static void upipe_ts_psii_inner_sink_input(struct upipe *sink,
+                                           struct uref *uref,
+                                           struct upump **upump_p)
+{
+    struct upipe_ts_psii *upipe_ts_psii = upipe_ts_psii_from_inner_sink(sink);
+    struct upipe *upipe = upipe_ts_psii_to_upipe(upipe_ts_psii);
+    upipe_ts_psii_output(upipe, uref, upump_p);
+}
+
+/** @internal @This processes control commands.
+ *
+ * @param sink description structure of the pipe
+ * @param command type of command to process
+ * @param args arguments of the command
+ * @return an error code
+ */
+static int upipe_ts_psii_inner_sink_control(struct upipe *sink,
+                                            int command, va_list args)
+{
+    struct upipe_ts_psii *upipe_ts_psii = upipe_ts_psii_from_inner_sink(sink);
+    struct upipe *upipe = upipe_ts_psii_to_upipe(upipe_ts_psii);
+    switch (command) {
+        case UPIPE_REGISTER_REQUEST: {
+            struct urequest *request = va_arg(args, struct urequest *);
+            return upipe_ts_psii_alloc_output_proxy(upipe, request);
+        }
+        case UPIPE_UNREGISTER_REQUEST: {
+            struct urequest *request = va_arg(args, struct urequest *);
+            return upipe_ts_psii_free_output_proxy(upipe, request);
+        }
+        case UPIPE_SET_FLOW_DEF:
+            return UBASE_ERR_NONE;
+
+        default:
+            return UBASE_ERR_UNHANDLED;
+    }
+}
+
+/** @internal @This initializes the inner pseudo sink pipe.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_ts_psii_init_inner_sink(struct upipe *upipe)
+{
+    struct upipe_ts_psii *upipe_ts_psii = upipe_ts_psii_from_upipe(upipe);
+    struct upipe_mgr *inner_sink_mgr = &upipe_ts_psii->inner_sink_mgr;
+    inner_sink_mgr->refcount = NULL;
+    inner_sink_mgr->signature = UPIPE_TS_PSII_INNER_SINK_SIGNATURE;
+    inner_sink_mgr->upipe_alloc = NULL;
+    inner_sink_mgr->upipe_input = upipe_ts_psii_inner_sink_input;
+    inner_sink_mgr->upipe_control = upipe_ts_psii_inner_sink_control;
+    inner_sink_mgr->upipe_mgr_control = NULL;
+
+    struct upipe *sink = &upipe_ts_psii->inner_sink;
+    sink->refcount = upipe_ts_psii_to_urefcount(upipe_ts_psii);
+    upipe_init(sink, inner_sink_mgr,
+               uprobe_pfx_alloc(uprobe_use(upipe->uprobe), UPROBE_LOG_VERBOSE,
+                                "inner sink"));
+}
+
+/** @internal @This cleans up the inner pseudo sink pipe.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_ts_psii_clean_inner_sink(struct upipe *upipe)
+{
+    struct upipe_ts_psii *upipe_ts_psii = upipe_ts_psii_from_upipe(upipe);
+    struct upipe *sink = &upipe_ts_psii->inner_sink;
+    upipe_clean(sink);
+}
+
 /** @internal @This allocates a ts_psii pipe.
  *
  * @param mgr common management structure
@@ -467,6 +551,7 @@ static struct upipe *upipe_ts_psii_alloc(struct upipe_mgr *mgr,
 
     upipe_ts_psii_init_urefcount(upipe);
     upipe_ts_psii_init_output(upipe);
+    upipe_ts_psii_init_inner_sink(upipe);
     upipe_ts_psii_init_sub_mgr(upipe);
     upipe_ts_psii_init_sub_subs(upipe);
 
@@ -608,6 +693,7 @@ static void upipe_ts_psii_free(struct upipe *upipe)
 {
     upipe_throw_dead(upipe);
     upipe_ts_psii_clean_sub_subs(upipe);
+    upipe_ts_psii_clean_inner_sink(upipe);
     upipe_ts_psii_clean_output(upipe);
     upipe_ts_psii_clean_urefcount(upipe);
     upipe_ts_psii_free_void(upipe);

tests/upipe_ts_psi_inserter_test.c
@@ -65,6 +65,7 @@
 #define UPROBE_LOG_LEVEL UPROBE_LOG_DEBUG
 
 static unsigned int nb_packets = 0;
+static bool expect_flow_def = true;
 
 /** definition of our uprobe */
 static int catch(struct uprobe *uprobe, struct upipe *upipe,
@@ -106,6 +107,7 @@ static int test_control(struct upipe *upipe, int command, va_list args)
 {
     switch (command) {
         case UPIPE_SET_FLOW_DEF:
+            assert(expect_flow_def);
             return UBASE_ERR_NONE;
         case UPIPE_REGISTER_REQUEST: {
             struct urequest *urequest = va_arg(args, struct urequest *);
@@ -212,6 +214,7 @@ int main(int argc, char *argv[])
     assert(uref != NULL);
     uref_clock_set_cr_sys(uref, (UCLOCK_FREQ / 10) * 4);
     uref_clock_set_cr_dts_delay(uref, 0);
+    expect_flow_def = false;
     upipe_input(upipe_ts_psii, uref, NULL);
     assert(nb_packets == 2);
     nb_packets = 0;