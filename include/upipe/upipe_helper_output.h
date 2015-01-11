Upipe/upipe

commit b4c2593b2fd7c0bed4c3517529c3ba25d67b877c

avoid several infinite loops

include/upipe/upipe_helper_output.h
@@ -334,6 +334,13 @@ static void STRUCTURE##_store_flow_def(struct upipe *upipe,                 \
                                        struct uref *flow_def)               \
 {                                                                           \
     struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
+    if (s->FLOW_DEF != NULL && s->FLOW_DEF->udict != NULL &&                \
+        flow_def->udict != NULL &&                                          \
+        !udict_cmp(s->FLOW_DEF->udict, flow_def->udict)) {                  \
+        uref_free(s->FLOW_DEF);                                             \
+        s->FLOW_DEF = flow_def; /* doesn't change the state */              \
+        return;                                                             \
+    }                                                                       \
     if (unlikely(s->FLOW_DEF != NULL)) {                                    \
         uref_free(s->FLOW_DEF);                                             \
         s->OUTPUT_STATE = UPIPE_HELPER_OUTPUT_NONE;                         \

doc/rules.mkdoc
@@ -29,7 +29,7 @@
 
       @item All parameters to @ref upipe_control belong to the caller. The @ref upipe must either duplicate or use (increment the refcount) structures it wants to keep. However if the control command is supposed to return information (such as control structures) from the @ref upipe, the structure belongs to the callee and should be duplicated or used, if required, by the caller.
 
-      @item A pipe shall not throw a @ref UPROBE_NEED_UPUMP_MGR event before receiving its first buffer, or being sent @ref UPIPE_ATTACH_UPUMP_MGR. An exception is allowed for source pipes, but it is legal for the application to not give a upump_mgr right away, and the source pipe should not throw a fatal error if that happens.
+      @item A pipe shall not throw a @ref UPROBE_NEED_UPUMP_MGR event before receiving its first buffer, being sent @ref UPIPE_ATTACH_UPUMP_MGR or being registered a @ref urequest. An exception is allowed for source pipes, but it is legal for the application to not give a upump_mgr right away, and the source pipe should not throw a fatal error if that happens.
 
     @end list
 

include/upipe/upipe_helper_ubuf_mgr.h
@@ -53,9 +53,10 @@ typedef int (*upipe_helper_ubuf_mgr_register)(struct upipe *, struct urequest *)
 /** @This declares five functions dealing with the ubuf manager used on the
  * output of a pipe.
  *
- * You must add two members to your private upipe structure, for instance:
+ * You must add three members to your private upipe structure, for instance:
  * @code
  *  struct ubuf_mgr *ubuf_mgr;
+ *  struct uref *flow_format;
  *  struct urequest ubuf_mgr_request;
  * @end code
  *
@@ -99,13 +100,15 @@ typedef int (*upipe_helper_ubuf_mgr_register)(struct upipe *, struct urequest *)
  * @param STRUCTURE name of your private upipe structure 
  * @param UBUF_MGR name of the @tt {struct ubuf_mgr *} field of
  * your private upipe structure
+ * @param FLOW_FORMAT name of the @tt {struct uref *} field of
+ * your private upipe structure
  * @param REQUEST name of the @tt {struct urequest} field of
  * your private upipe structure
  * @param CHECK function called after a uref manager has been received
  * @param REGISTER function called to register a request
  * @param UNREGISTER function called to unregister a request
  */
-#define UPIPE_HELPER_UBUF_MGR(STRUCTURE, UBUF_MGR, REQUEST,                 \
+#define UPIPE_HELPER_UBUF_MGR(STRUCTURE, UBUF_MGR, FLOW_FORMAT, REQUEST,    \
                               CHECK, REGISTER, UNREGISTER)                  \
 /** @internal @This initializes the private members for this helper.        \
  *                                                                          \
@@ -115,6 +118,7 @@ static void STRUCTURE##_init_ubuf_mgr(struct upipe *upipe)                  \
 {                                                                           \
     struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
     s->UBUF_MGR = NULL;                                                     \
+    s->FLOW_FORMAT = NULL;                                                  \
     urequest_set_opaque(&s->REQUEST, NULL);                                 \
 }                                                                           \
 /** @internal @This handles the result of a ubuf manager request.           \
@@ -127,10 +131,20 @@ static int STRUCTURE##_provide_ubuf_mgr(struct urequest *urequest,          \
 {                                                                           \
     struct upipe *upipe = urequest_get_opaque(urequest, struct upipe *);    \
     struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
-    ubuf_mgr_release(s->UBUF_MGR);                                          \
-    s->UBUF_MGR = va_arg(args, struct ubuf_mgr *);                          \
+    struct ubuf_mgr *ubuf_mgr = va_arg(args, struct ubuf_mgr *);            \
     struct uref *flow_format = va_arg(args, struct uref *);                 \
+    if (ubuf_mgr == s->UBUF_MGR && s->FLOW_FORMAT != NULL &&                \
+        s->FLOW_FORMAT->udict != NULL && flow_format->udict != NULL &&      \
+        !udict_cmp(s->FLOW_FORMAT->udict, flow_format->udict)) {            \
+        ubuf_mgr_release(ubuf_mgr);                                         \
+        uref_free(flow_format);                                             \
+        return UBASE_ERR_NONE;                                              \
+    }                                                                       \
+    ubuf_mgr_release(s->UBUF_MGR);                                          \
+    s->UBUF_MGR = ubuf_mgr;                                                 \
     upipe_dbg_va(upipe, "provided ubuf_mgr %p", s->UBUF_MGR);               \
+    uref_free(s->FLOW_FORMAT);                                              \
+    s->FLOW_FORMAT = uref_dup(flow_format);                                 \
     upipe_helper_ubuf_mgr_check check = CHECK;                              \
     if (check != NULL)                                                      \
         return check(upipe, flow_format);                                   \
@@ -189,6 +203,7 @@ static void STRUCTURE##_clean_ubuf_mgr(struct upipe *upipe)                 \
 {                                                                           \
     struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
     ubuf_mgr_release(s->UBUF_MGR);                                          \
+    uref_free(s->FLOW_FORMAT);                                              \
     /* If the request was registered, it should be unregistered             \
      * automatically. Otherwise it has not been initialized. */             \
 }

include/upipe/upipe_helper_uclock.h
@@ -116,8 +116,13 @@ static int STRUCTURE##_provide_uclock(struct urequest *urequest,            \
 {                                                                           \
     struct upipe *upipe = urequest_get_opaque(urequest, struct upipe *);    \
     struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
+    struct uclock *uclock = va_arg(args, struct uclock *);                  \
+    if (uclock == s->UCLOCK) {                                              \
+        uclock_release(uclock);                                             \
+        return UBASE_ERR_NONE;                                              \
+    }                                                                       \
     uclock_release(s->UCLOCK);                                              \
-    s->UCLOCK = va_arg(args, struct uclock *);                              \
+    s->UCLOCK = uclock;                                                     \
     upipe_dbg_va(upipe, "provided uclock %p", s->UCLOCK);                   \
     upipe_helper_uclock_check check = CHECK;                                \
     return check != NULL ? check(upipe, NULL) : UBASE_ERR_NONE;             \

include/upipe/upipe_helper_uref_mgr.h
@@ -122,8 +122,13 @@ static int STRUCTURE##_provide_uref_mgr(struct urequest *urequest,          \
 {                                                                           \
     struct upipe *upipe = urequest_get_opaque(urequest, struct upipe *);    \
     struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
+    struct uref_mgr *uref_mgr = va_arg(args, struct uref_mgr *);            \
+    if (uref_mgr == s->UREF_MGR) {                                          \
+        uref_mgr_release(uref_mgr);                                         \
+        return UBASE_ERR_NONE;                                              \
+    }                                                                       \
     uref_mgr_release(s->UREF_MGR);                                          \
-    s->UREF_MGR = va_arg(args, struct uref_mgr *);                          \
+    s->UREF_MGR = uref_mgr;                                                 \
     upipe_dbg_va(upipe, "provided uref_mgr %p", s->UREF_MGR);               \
     upipe_helper_uref_mgr_check check = CHECK;                              \
     return check != NULL ? check(upipe, NULL) : UBASE_ERR_NONE;             \

lib/upipe-amt/upipe_amt_source.c
@@ -100,6 +100,8 @@ struct upipe_amtsrc {
 
     /** ubuf manager */
     struct ubuf_mgr *ubuf_mgr;
+    /** flow format packet */
+    struct uref *flow_format;
     /** ubuf manager request */
     struct urequest ubuf_mgr_request;
 
@@ -142,7 +144,7 @@ UPIPE_HELPER_UREF_MGR(upipe_amtsrc, uref_mgr, uref_mgr_request,
                       upipe_amtsrc_check,
                       upipe_amtsrc_register_output_request,
                       upipe_amtsrc_unregister_output_request)
-UPIPE_HELPER_UBUF_MGR(upipe_amtsrc, ubuf_mgr, ubuf_mgr_request,
+UPIPE_HELPER_UBUF_MGR(upipe_amtsrc, ubuf_mgr, flow_format, ubuf_mgr_request,
                       upipe_amtsrc_check,
                       upipe_amtsrc_register_output_request,
                       upipe_amtsrc_unregister_output_request)

lib/upipe-av/upipe_avcodec_decode.c
@@ -109,6 +109,8 @@ struct upipe_avcdec {
 
     /** ubuf manager */
     struct ubuf_mgr *ubuf_mgr;
+    /** flow format packet */
+    struct uref *flow_format;
     /** ubuf manager request */
     struct urequest ubuf_mgr_request;
 
@@ -167,7 +169,7 @@ UPIPE_HELPER_OUTPUT(upipe_avcdec, output, flow_def, output_state, request_list)
 UPIPE_HELPER_FLOW_DEF(upipe_avcdec, flow_def_input, flow_def_attr)
 UPIPE_HELPER_FLOW_DEF_CHECK(upipe_avcdec, flow_def_check)
 
-UPIPE_HELPER_UBUF_MGR(upipe_avcdec, ubuf_mgr, ubuf_mgr_request,
+UPIPE_HELPER_UBUF_MGR(upipe_avcdec, ubuf_mgr, flow_format, ubuf_mgr_request,
                       upipe_avcdec_check,
                       upipe_avcdec_register_output_request,
                       upipe_avcdec_unregister_output_request)

lib/upipe-av/upipe_avcodec_encode.c
@@ -118,6 +118,8 @@ struct upipe_avcenc {
 
     /** ubuf manager */
     struct ubuf_mgr *ubuf_mgr;
+    /** flow format packet */
+    struct uref *flow_format;
     /** ubuf manager request */
     struct urequest ubuf_mgr_request;
 
@@ -170,7 +172,7 @@ UPIPE_HELPER_OUTPUT(upipe_avcenc, output, flow_def, output_state, request_list)
 UPIPE_HELPER_FLOW_DEF(upipe_avcenc, flow_def_input, flow_def_attr)
 UPIPE_HELPER_FLOW_DEF_CHECK(upipe_avcenc, flow_def_check)
 
-UPIPE_HELPER_UBUF_MGR(upipe_avcenc, ubuf_mgr, ubuf_mgr_request,
+UPIPE_HELPER_UBUF_MGR(upipe_avcenc, ubuf_mgr, flow_format, ubuf_mgr_request,
                       upipe_avcenc_check,
                       upipe_avcenc_register_output_request,
                       upipe_avcenc_unregister_output_request)

commit ce27704f92bad107ccd54294f849d006506f35f0

fix segfault
Introduced in b4c2593b2fd7c0bed4c3517529c3ba25d67b877c.

include/upipe/upipe_helper_output.h
@@ -335,7 +335,7 @@ static void STRUCTURE##_store_flow_def(struct upipe *upipe,                 \
 {                                                                           \
     struct STRUCTURE *s = STRUCTURE##_from_upipe(upipe);                    \
     if (s->FLOW_DEF != NULL && s->FLOW_DEF->udict != NULL &&                \
-        flow_def->udict != NULL &&                                          \
+        flow_def != NULL && flow_def->udict != NULL &&                      \
         !udict_cmp(s->FLOW_DEF->udict, flow_def->udict)) {                  \
         uref_free(s->FLOW_DEF);                                             \
         s->FLOW_DEF = flow_def; /* doesn't change the state */              \