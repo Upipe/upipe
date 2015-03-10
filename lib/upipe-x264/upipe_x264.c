35bcba01fb50a4eb68667e5c3dfcb9900994f943
Christophe Massiot <cmassiot@openheadend.tv>
2015-03-11 00:04:44 +0100

    x264: set max_octetrate & remove latency

--- lib/upipe-x264/upipe_x264.c
+++ lib/upipe-x264/upipe_x264.c
@@ -410,6 +410,70 @@ static bool upipe_x264_open(struct upipe *upipe, int width, int height,
         if (params->rc.i_vbv_buffer_size)
             uref_block_flow_set_buffer_size(flow_def_attr,
                                             params->rc.i_vbv_buffer_size * 125);
+
+        uint64_t max_octetrate, max_bs;
+        switch (params->i_level_idc) {
+            case 10:
+                max_octetrate = 64000 / 8;
+                max_bs = 175000 / 8;
+                break;
+            case 11:
+                max_octetrate = 192000 / 8;
+                max_bs = 500000 / 8;
+                break;
+            case 12:
+                max_octetrate = 384000 / 8;
+                max_bs = 1000000 / 8;
+                break;
+            case 13:
+                max_octetrate = 768000 / 8;
+                max_bs = 2000000 / 8;
+                break;
+            case 20:
+                max_octetrate = 2000000 / 8;
+                max_bs = 2000000 / 8;
+                break;
+            case 21:
+            case 22:
+                max_octetrate = 4000000 / 8;
+                max_bs = 4000000 / 8;
+                break;
+            case 30:
+                max_octetrate = 10000000 / 8;
+                max_bs = 10000000 / 8;
+                break;
+            case 31:
+                max_octetrate = 14000000 / 8;
+                max_bs = 14000000 / 8;
+                break;
+            case 32:
+            case 40:
+                max_octetrate = 20000000 / 8;
+                max_bs = 20000000 / 8;
+                break;
+            case 41:
+            case 42:
+                max_octetrate = 50000000 / 8;
+                max_bs = 62500000 / 8;
+                break;
+            case 50:
+                max_octetrate = 135000000 / 8;
+                max_bs = 135000000 / 8;
+                break;
+            default:
+                upipe_warn_va(upipe, "unknown level %"PRIu8,
+                              params->i_level_idc);
+                /* intended fall-through */
+            case 51:
+            case 52:
+                max_octetrate = 240000000 / 8;
+                max_bs = 240000000 / 8;
+                break;
+        }
+        UBASE_FATAL(upipe, uref_block_flow_set_max_octetrate(flow_def_attr,
+                    max_octetrate))
+        UBASE_FATAL(upipe, uref_block_flow_set_max_buffer_size(flow_def_attr,
+                    max_bs))
     }
 
     /* find latency */
@@ -421,9 +485,6 @@ static bool upipe_x264_open(struct upipe *upipe, int width, int height,
                      / params->i_fps_num;
     upipe_x264->initial_latency = latency;
 
-    if (params->rc.i_bitrate)
-        latency += (uint64_t)params->rc.i_vbv_buffer_size * UCLOCK_FREQ /
-                   params->rc.i_bitrate;
     latency += upipe_x264->sc_latency;
     uref_clock_set_latency(flow_def_attr, latency);
 
