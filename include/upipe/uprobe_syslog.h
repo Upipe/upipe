245a135b2223085759ca3f8f2c157b798d883817
Kieran Kunhya <kieran@kunhya.com>
2015-03-22 21:53:41 +0000

    Squashed commit of the following:
    commit b570e7c895a7f9600d86b3af14f2f96e518c3bba
    Author: Kieran Kunhya <kieran@kunhya.com>
    Date:   Sun Mar 22 21:50:27 2015 +0000
    
        uprobe_syslog_test: fix whitespace
    
    commit 97314680c8c7371b178c2d3134b2ca4c4fafb88e
    Author: Kieran Kunhya <kieran@kunhya.com>
    Date:   Sun Mar 22 21:36:38 2015 +0000
    
        Add uprobe_syslog test
    
    commit cd8195b45674a2e9e740895d5f36a0fb38d3a363
    Author: Kieran Kunhya <kierank@obe.tv>
    Date:   Sat Mar 21 19:16:50 2015 +0000
    
        Add missing header
    
    commit c12019dc11e6cf3ac1b02c3c7527fc7554cb3ded
    Author: Kieran Kunhya <kierank@obe.tv>
    Date:   Sat Mar 21 19:13:13 2015 +0000
    
        Get rid of junk file
    
    commit 776abd911f7a6cd3f3fded9fd98882876824158a
    Author: Kieran Kunhya <kierank@obe.tv>
    Date:   Sat Mar 21 14:45:37 2015 +0000
    
        Install header correctly
    
    commit 470431e781b5a09ffd99b97b34ec9925f941744d
    Author: Kieran Kunhya <kierank@obe.tv>
    Date:   Sat Mar 21 14:29:57 2015 +0000
    
        Add syslog probe
    
    Add missing execution of test

---
+++ include/upipe/uprobe_syslog.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2015 Open Broadcast Systems Ltd
+ *
+ * Authors: Kieran Kunhya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short probe outputting all log events to syslog
+ */
+
+#ifndef _UPIPE_UPROBE_SYSLOG_H_
+/** @hidden */
+#define _UPIPE_UPROBE_SYSLOG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <upipe/uprobe.h>
+#include <upipe/uprobe_helper_uprobe.h>
+
+/** @This is a super-set of the uprobe structure with additional local
+ * members. */
+struct uprobe_syslog {
+    /** syslog ident **/
+    char *ident;
+    
+    /** minimum level of printed messages */
+    enum uprobe_log_level min_level;
+
+    /** structure exported to modules */
+    struct uprobe uprobe;
+};
+
+UPROBE_HELPER_UPROBE(uprobe_syslog, uprobe)
+
+/** @This initializes an already allocated uprobe_syslog structure.
+ *
+ * @param uprobe_syslog pointer to the already allocated structure
+ * @param next next probe to test if this one doesn't catch the event
+ * @param stream syslog stream to which to log the messages
+ * @param level level at which to log the messages
+ * @return pointer to uprobe, or NULL in case of error
+ */
+struct uprobe *uprobe_syslog_init(struct uprobe_syslog *uprobe_syslog,
+                                  struct uprobe *next, char *ident,
+                                  int option, int facility,
+                                  enum uprobe_log_level min_level);
+
+/** @This cleans a uprobe_syslog structure.
+ *
+ * @param uprobe_syslog structure to clean
+ */
+void uprobe_syslog_clean(struct uprobe_syslog *uprobe_syslog);
+
+/** @This allocates a new uprobe syslog structure.
+ *
+ * @param next next probe to test if this one doesn't catch the event
+ * @param stream syslog stream to which to log the messages
+ * @param level level at which to log the messages
+ * @return pointer to uprobe, or NULL in case of error
+ */
+struct uprobe *uprobe_syslog_alloc(struct uprobe *next, char *ident,
+                                  int option, int facility,
+                                  enum uprobe_log_level min_level);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ tests/uprobe_syslog_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+srcdir="$1"
+DIR="`mktemp -d tmp.XXXXXXXXXX`"
+./uprobe_stdio_test > "$DIR"/logs
+RET=$?
+if test $RET -ne 0; then
+	rm -rf "$DIR"
+	exit $RET
+fi
+
+if ! which valgrind >/dev/null 2>&1; then
+	echo "#### Please install valgrind for unit tests"
+	exit 1
+fi
+
+unset DIR
+FILE="`mktemp tmp.XXXXXXXXXX`"
+libtool --mode=execute valgrind -q --leak-check=full ./uprobe_syslog_test > /dev/null 2> "$FILE"
+RET=$?
+if test -s "$FILE"; then
+        cat "$FILE" >&2
+        RET=1
+fi
+rm -f "$FILE"
+exit $RET
