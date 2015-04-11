Upipe/upipe

commit 7ddfe5b5e6041971191f4e93a0051a1beeaa8bb5

correctly handle color spaces

lib/upipe-av/upipe_avcodec_encode.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2014 OpenHeadend S.A.R.L.
+ * Copyright (C) 2012-2015 OpenHeadend S.A.R.L.
  *
  * Authors: Benjamin Cohen
  *          Christophe Massiot
@@ -179,6 +179,54 @@ UPIPE_HELPER_UBUF_MGR(upipe_avcenc, ubuf_mgr, flow_format, ubuf_mgr_request,
 UPIPE_HELPER_UPUMP_MGR(upipe_avcenc, upump_mgr)
 UPIPE_HELPER_UPUMP(upipe_avcenc, upump_av_deal, upump_mgr)
 UPIPE_HELPER_INPUT(upipe_avcenc, urefs, nb_urefs, max_urefs, blockers, upipe_avcenc_encode)
+
+/** @This allows to convert from Upipe color space to avcenc color space. */
+struct upipe_avcenc_color {
+    /** Upipe color */
+    const char *upipe_color;
+    /** avcenc color */
+    const char *avcenc_color;
+};
+
+/** @This converts transfer characteristics from Upipe to avcenc. */
+static const struct upipe_avcenc_color upipe_avcenc_color_trc[] = {
+    { "bt470m", "gamma22" },
+    { "bt470bg", "gamma28" },
+    { "log100", "log" },
+    { "log316", "log_sqrt" },
+    { "iec61966-2-4", "iec61966_2_4" },
+    { "bt1361e", "bt1361" },
+    { "iec61966-2-1", "iec61966_2_1" },
+    { "bt2020-10", "bt2020-10bit" },
+    { "bt2020-12", "bt2020-12bit" },
+    { NULL, NULL }
+};
+
+/** @This converts matrix coefficients from Upipe to avcenc. */
+static const struct upipe_avcenc_color upipe_avcenc_color_space[] = {
+    { "GBR", "rgb" },
+    { "YCgCo", "ycocg" },
+    { "bt2020nc", "bt2020_ncl" },
+    { "bt2020c", "bt2020_cl" },
+    { NULL, NULL }
+};
+
+/** @internal @This converts a Upipe color space to avcenc color space.
+ *
+ * @param list conversion list
+ * @param upipe_color Upipe color
+ * @return avcenc color
+ */
+static const char *
+    upipe_avcenc_convert_color(const struct upipe_avcenc_color *list,
+                               const char *upipe_color)
+{
+    int i = 0;
+    while (list[i].upipe_color != NULL)
+        if (!strcmp(list[i].upipe_color, upipe_color))
+            return list[i].avcenc_color;
+    return upipe_color;
+}
 
 /** @hidden */
 static void upipe_avcenc_free(struct upipe *upipe);
@@ -607,6 +655,38 @@ static void upipe_avcenc_encode_video(struct upipe *upipe,
         context->sample_aspect_ratio.num = sar.num;
         context->sample_aspect_ratio.den = sar.den;
     }
+
+    /* set color options if it was changed in the input */
+    const char *content;
+    int ret;
+    content = ubase_check(uref_pic_flow_get_full_range(
+                    upipe_avcenc->flow_def_input)) ? "jpeg" : "mpeg";
+    if ((ret = av_opt_set(upipe_avcenc->context, "color_range", content,
+                          AV_OPT_SEARCH_CHILDREN)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "color_range", content, ret);
+    if (ubase_check(uref_pic_flow_get_colour_primaries(
+                    upipe_avcenc->flow_def_input, &content)) &&
+        (ret = av_opt_set(upipe_avcenc->context, "color_primaries", content,
+                          AV_OPT_SEARCH_CHILDREN)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "color_primaries", content, ret);
+    if (ubase_check(uref_pic_flow_get_transfer_characteristics(
+                    upipe_avcenc->flow_def_input, &content)) &&
+        (ret = av_opt_set(upipe_avcenc->context, "color_trc",
+                          upipe_avcenc_convert_color(upipe_avcenc_color_trc,
+                                                     content),
+                          AV_OPT_SEARCH_CHILDREN)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "color_trc", content, ret);
+    if (ubase_check(uref_pic_flow_get_matrix_coefficients(
+                    upipe_avcenc->flow_def_input, &content)) &&
+        (ret = av_opt_set(upipe_avcenc->context, "colorspace",
+                          upipe_avcenc_convert_color(upipe_avcenc_color_space,
+                                                     content),
+                          AV_OPT_SEARCH_CHILDREN)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "colorspace", content, ret);
 
     /* store uref in mapping list */
     ulist_add(&upipe_avcenc->urefs_in_use, uref_to_uchain(uref));

include/upipe/udict.h
@@ -103,6 +103,16 @@ enum udict_type {
     UDICT_TYPE_PIC_HSIZE_VISIBLE,
     /** p.vsizevis */
     UDICT_TYPE_PIC_VSIZE_VISIBLE,
+    /** p.format */
+    UDICT_TYPE_PIC_VIDEO_FORMAT,
+    /** p.fullrange */
+    UDICT_TYPE_PIC_FULL_RANGE,
+    /** p.colorprim */
+    UDICT_TYPE_PIC_COLOUR_PRIMARIES,
+    /** p.transfer */
+    UDICT_TYPE_PIC_TRANSFER_CHARACTERISTICS,
+    /** p.colmatrix */
+    UDICT_TYPE_PIC_MATRIX_COEFFICIENTS,
     /** p.hposition */
     UDICT_TYPE_PIC_HPOSITION,
     /** p.vposition */

include/upipe/uref_pic_flow.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2014 OpenHeadend S.A.R.L.
+ * Copyright (C) 2012-2015 OpenHeadend S.A.R.L.
  *
  * Authors: Christophe Massiot
  *
@@ -80,6 +80,16 @@ UREF_ATTR_UNSIGNED_SH(pic_flow, hsize_visible, UDICT_TYPE_PIC_HSIZE_VISIBLE,
         horizontal visible size)
 UREF_ATTR_UNSIGNED_SH(pic_flow, vsize_visible, UDICT_TYPE_PIC_VSIZE_VISIBLE,
         vertical visible size)
+UREF_ATTR_STRING_SH(pic_flow, video_format, UDICT_TYPE_PIC_VIDEO_FORMAT,
+        video format)
+UREF_ATTR_VOID_SH(pic_flow, full_range, UDICT_TYPE_PIC_FULL_RANGE,
+        colour full range)
+UREF_ATTR_STRING_SH(pic_flow, colour_primaries, UDICT_TYPE_PIC_COLOUR_PRIMARIES,
+        colour primaries)
+UREF_ATTR_STRING_SH(pic_flow, transfer_characteristics,
+        UDICT_TYPE_PIC_TRANSFER_CHARACTERISTICS, transfer characteristics)
+UREF_ATTR_STRING_SH(pic_flow, matrix_coefficients,
+        UDICT_TYPE_PIC_MATRIX_COEFFICIENTS, matrix coefficients)
 
 /** @This allocates a control packet to define a new picture flow. For each
  * plane, uref_pic_flow_add_plane() has to be called afterwards.

lib/upipe-framers/upipe_h264_framer.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2014 OpenHeadend S.A.R.L.
+ * Copyright (C) 2013-2015 OpenHeadend S.A.R.L.
  *
  * Authors: Christophe Massiot
  *
@@ -949,6 +949,12 @@ static bool upipe_h264f_activate_sps(struct upipe *upipe, uint32_t sps_id)
     bool vui = !!ubuf_block_stream_show_bits(s, 1);
     ubuf_block_stream_skip_bits(s, 1);
     upipe_h264f->sar.den = 0;
+    uint8_t video_format = 5;
+    bool full_range = false;
+    uint8_t colour_primaries = 2;
+    uint8_t transfer_characteristics = 2;
+    uint8_t matrix_coefficients = 2;
+
     if (vui) {
         upipe_h264f_stream_fill_bits(s, 1);
         bool ar_present = !!ubuf_block_stream_show_bits(s, 1);
@@ -986,13 +992,21 @@ static bool upipe_h264f_activate_sps(struct upipe *upipe, uint32_t sps_id)
         ubuf_block_stream_skip_bits(s, 1);
         if (video_signal_present) {
             upipe_h264f_stream_fill_bits(s, 5);
-            ubuf_block_stream_skip_bits(s, 4);
-            if (ubuf_block_stream_show_bits(s, 1)) {
-                upipe_h264f_stream_fill_bits(s, 24);
-                ubuf_block_stream_skip_bits(s, 24);
-            }
-            upipe_h264f_stream_fill_bits(s, 1);
+            video_format = ubuf_block_stream_show_bits(s, 3);
+            ubuf_block_stream_skip_bits(s, 3);
+            full_range = !!ubuf_block_stream_show_bits(s, 1);
             ubuf_block_stream_skip_bits(s, 1);
+            bool colour_present = !!ubuf_block_stream_show_bits(s, 1);
+            ubuf_block_stream_skip_bits(s, 1);
+            if (colour_present) {
+                upipe_h264f_stream_fill_bits(s, 24);
+                colour_primaries = ubuf_block_stream_show_bits(s, 8);
+                ubuf_block_stream_skip_bits(s, 8);
+                transfer_characteristics = ubuf_block_stream_show_bits(s, 8);
+                ubuf_block_stream_skip_bits(s, 8);
+                matrix_coefficients = ubuf_block_stream_show_bits(s, 8);
+                ubuf_block_stream_skip_bits(s, 8);
+            }
         }
 
         upipe_h264f_stream_fill_bits(s, 1);
@@ -1075,6 +1089,152 @@ static bool upipe_h264f_activate_sps(struct upipe *upipe, uint32_t sps_id)
         upipe_h264f->duration = 0;
         upipe_h264f->pic_struct_present = false;
         upipe_h264f->hrd = false;
+    }
+
+    const char *video_format_str = NULL;
+    switch (video_format) {
+        case 0:
+            video_format_str = "component";
+            break;
+        case 1:
+            video_format_str = "pal";
+            break;
+        case 2:
+            video_format_str = "ntsc";
+            break;
+        case 3:
+            video_format_str = "secam";
+            break;
+        case 4:
+            video_format_str = "mac";
+            break;
+        default:
+            break;
+    }
+    if (video_format_str != NULL) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_video_format(flow_def,
+                    video_format_str))
+    }
+
+    if (full_range) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_full_range(flow_def))
+    }
+
+    const char *colour_primaries_str = NULL;
+    switch (colour_primaries) {
+        case 1:
+            colour_primaries_str = "bt709";
+            break;
+        case 4:
+            colour_primaries_str = "bt470m";
+            break;
+        case 5:
+            colour_primaries_str = "bt470bg";
+            break;
+        case 6:
+            colour_primaries_str = "smpte170m";
+            break;
+        case 7:
+            colour_primaries_str = "smpte240m";
+            break;
+        case 8:
+            colour_primaries_str = "film";
+            break;
+        case 9:
+            colour_primaries_str = "bt2020";
+            break;
+        default:
+            break;
+    }
+    if (colour_primaries_str != NULL) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_colour_primaries(flow_def,
+                    colour_primaries_str))
+    }
+
+    const char *transfer_characteristics_str = NULL;
+    switch (transfer_characteristics) {
+        case 1:
+            transfer_characteristics_str = "bt709";
+            break;
+        case 4:
+            transfer_characteristics_str = "bt470m";
+            break;
+        case 5:
+            transfer_characteristics_str = "bt470bg";
+            break;
+        case 6:
+            transfer_characteristics_str = "smpte170m";
+            break;
+        case 7:
+            transfer_characteristics_str = "smpte240m";
+            break;
+        case 8:
+            transfer_characteristics_str = "linear";
+            break;
+        case 9:
+            transfer_characteristics_str = "log100";
+            break;
+        case 10:
+            transfer_characteristics_str = "log316";
+            break;
+        case 11:
+            transfer_characteristics_str = "iec61966-2-4";
+            break;
+        case 12:
+            transfer_characteristics_str = "bt1361e";
+            break;
+        case 13:
+            transfer_characteristics_str = "iec61966-2-1";
+            break;
+        case 14:
+            transfer_characteristics_str = "bt2020-10";
+            break;
+        case 15:
+            transfer_characteristics_str = "bt2020-12";
+            break;
+        default:
+            break;
+    }
+    if (transfer_characteristics_str != NULL) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_transfer_characteristics(flow_def,
+                    transfer_characteristics_str))
+    }
+
+    const char *matrix_coefficients_str = NULL;
+    switch (matrix_coefficients) {
+        case 0:
+            matrix_coefficients_str = "GBR";
+            break;
+        case 1:
+            matrix_coefficients_str = "bt709";
+            break;
+        case 4:
+            matrix_coefficients_str = "fcc";
+            break;
+        case 5:
+            matrix_coefficients_str = "bt470bg";
+            break;
+        case 6:
+            matrix_coefficients_str = "smpte170m";
+            break;
+        case 7:
+            matrix_coefficients_str = "smpte240m";
+            break;
+        case 8:
+            matrix_coefficients_str = "YCgCo";
+            break;
+        case 9:
+            matrix_coefficients_str = "bt2020nc";
+            break;
+        case 10:
+            matrix_coefficients_str = "bt2020c";
+            break;
+        default:
+            break;
+    }
+    if (matrix_coefficients_str != NULL) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_matrix_coefficients(flow_def,
+                    matrix_coefficients_str))
     }
 
     upipe_h264f->active_sps = sps_id;

lib/upipe-framers/upipe_mpgv_framer.c
@@ -451,6 +451,11 @@ static bool upipe_mpgvf_parse_sequence(struct upipe *upipe)
     UBASE_FATAL(upipe, uref_block_flow_set_buffer_size(flow_def,
                                                 vbvbuffer * 16 * 1024 / 8))
 
+    uint8_t video_format = 5;
+    uint8_t colour_primaries = 1;
+    uint8_t transfer_characteristics = 1;
+    uint8_t matrix_coefficients = 1;
+
     if (upipe_mpgvf->sequence_display != NULL) {
         size_t size;
         uint8_t display_buffer[MP2VSEQDX_HEADER_SIZE + MP2VSEQDX_COLOR_SIZE];
@@ -458,13 +463,19 @@ static bool upipe_mpgvf_parse_sequence(struct upipe *upipe)
         if (unlikely(!ubase_check(ubuf_block_size(upipe_mpgvf->sequence_display,
                                   &size)) ||
                      (display = ubuf_block_peek(upipe_mpgvf->sequence_display,
-                                                0, size,
-                                                display_buffer)) == NULL)) {
+                        0,
+                        size > MP2VSEQDX_HEADER_SIZE + MP2VSEQDX_COLOR_SIZE ?
+                        MP2VSEQDX_HEADER_SIZE + MP2VSEQDX_COLOR_SIZE : size,
+                        display_buffer)) == NULL)) {
             uref_free(flow_def);
             upipe_throw_fatal(upipe, UBASE_ERR_ALLOC);
             return false;
         }
 
+        video_format = mp2vseqdx_get_format(display);
+        colour_primaries = mp2vseqdx_get_primaries(display);
+        transfer_characteristics = mp2vseqdx_get_transfer(display);
+        matrix_coefficients = mp2vseqdx_get_matrixcoeffs(display);
         uint16_t display_horizontal = mp2vseqdx_get_horizontal(display);
         uint16_t display_vertical = mp2vseqdx_get_vertical(display);
 
@@ -474,6 +485,109 @@ static bool upipe_mpgvf_parse_sequence(struct upipe *upipe)
                                                      display_horizontal))
         UBASE_FATAL(upipe, uref_pic_flow_set_vsize_visible(flow_def,
                                                      display_vertical))
+    }
+
+    const char *video_format_str = NULL;
+    switch (video_format) {
+        case 0:
+            video_format_str = "component";
+            break;
+        case 1:
+            video_format_str = "pal";
+            break;
+        case 2:
+            video_format_str = "ntsc";
+            break;
+        case 3:
+            video_format_str = "secam";
+            break;
+        case 4:
+            video_format_str = "mac";
+            break;
+        default:
+            break;
+    }
+    if (video_format_str != NULL) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_video_format(flow_def,
+                    video_format_str))
+    }
+
+    const char *colour_primaries_str = NULL;
+    switch (colour_primaries) {
+        case 1:
+            colour_primaries_str = "bt709";
+            break;
+        case 4:
+            colour_primaries_str = "bt470m";
+            break;
+        case 5:
+            colour_primaries_str = "bt470bg";
+            break;
+        case 6:
+            colour_primaries_str = "smpte170m";
+            break;
+        case 7:
+            colour_primaries_str = "smpte240m";
+            break;
+        default:
+            break;
+    }
+    if (colour_primaries_str != NULL) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_colour_primaries(flow_def,
+                    colour_primaries_str))
+    }
+
+    const char *transfer_characteristics_str = NULL;
+    switch (transfer_characteristics) {
+        case 1:
+            transfer_characteristics_str = "bt709";
+            break;
+        case 4:
+            transfer_characteristics_str = "bt470m";
+            break;
+        case 5:
+            transfer_characteristics_str = "bt470bg";
+            break;
+        case 6:
+            transfer_characteristics_str = "smpte170m";
+            break;
+        case 7:
+            transfer_characteristics_str = "smpte240m";
+            break;
+        case 8:
+            transfer_characteristics_str = "linear";
+            break;
+        default:
+            break;
+    }
+    if (transfer_characteristics_str != NULL) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_transfer_characteristics(flow_def,
+                    transfer_characteristics_str))
+    }
+
+    const char *matrix_coefficients_str = NULL;
+    switch (matrix_coefficients) {
+        case 1:
+            matrix_coefficients_str = "bt709";
+            break;
+        case 4:
+            matrix_coefficients_str = "fcc";
+            break;
+        case 5:
+            matrix_coefficients_str = "bt470bg";
+            break;
+        case 6:
+            matrix_coefficients_str = "smpte170m";
+            break;
+        case 7:
+            matrix_coefficients_str = "smpte240m";
+            break;
+        default:
+            break;
+    }
+    if (matrix_coefficients_str != NULL) {
+        UBASE_FATAL(upipe, uref_pic_flow_set_matrix_coefficients(flow_def,
+                    matrix_coefficients_str))
     }
 
     flow_def = upipe_mpgvf_store_flow_def_attr(upipe, flow_def);

lib/upipe-gl/upipe_glx_sink.c
@@ -518,6 +518,9 @@ static int upipe_glx_sink_provide_flow_format(struct upipe *upipe,
     uref_pic_flow_set_planes(flow_format, 0);
     uref_pic_flow_add_plane(flow_format, 1, 1, 3, "r8g8b8");
     uref_pic_set_progressive(flow_format);
+    uref_pic_flow_delete_colour_primaries(flow_format);
+    uref_pic_flow_delete_transfer_characteristics(flow_format);
+    uref_pic_flow_delete_matrix_coefficients(flow_format);
     return urequest_provide_flow_format(request, flow_format);
 }
 

lib/upipe-swscale/upipe_sws.c
@@ -111,6 +111,14 @@ struct upipe_sws {
     const char *input_chroma_map[UPIPE_AV_MAX_PLANES];
     /** output chroma map */
     const char *output_chroma_map[UPIPE_AV_MAX_PLANES];
+    /** input colorspace */
+    int input_colorspace;
+    /** output colorspace */
+    int output_colorspace;
+    /** input color range */
+    int input_color_range;
+    /** output color range */
+    int output_color_range;
 
     /** public upipe structure */
     struct upipe upipe;
@@ -126,6 +134,32 @@ UPIPE_HELPER_UBUF_MGR(upipe_sws, ubuf_mgr, flow_format, ubuf_mgr_request,
                       upipe_sws_register_output_request,
                       upipe_sws_unregister_output_request)
 UPIPE_HELPER_INPUT(upipe_sws, urefs, nb_urefs, max_urefs, blockers, upipe_sws_handle)
+
+/** @internal @This converts Upipe color space to sws color space.
+ *
+ * @param upipe description structure of the pipe
+ * @param flow_def flow definition packet
+ * @return sws color space
+ */
+static int upipe_sws_convert_color(struct upipe *upipe, struct uref *flow_def)
+{
+    int colorspace = -1;
+    const char *matrix_coefficients;
+    if (ubase_check(uref_pic_flow_get_matrix_coefficients(flow_def,
+                    &matrix_coefficients))) {
+        if (!strcmp(matrix_coefficients, "bt709"))
+            colorspace = SWS_CS_ITU709;
+        else if (!strcmp(matrix_coefficients, "fcc"))
+            colorspace = SWS_CS_FCC;
+        else if (!strcmp(matrix_coefficients, "smpte170m"))
+            colorspace = SWS_CS_SMPTE170M;
+        else if (!strcmp(matrix_coefficients, "smpte240m"))
+            colorspace = SWS_CS_SMPTE240M;
+        else
+            upipe_warn_va(upipe, "unknown color space %s", matrix_coefficients);
+    }
+    return colorspace;
+}
 
 /** @internal @This handles data.
  *
@@ -172,12 +206,34 @@ static bool upipe_sws_handle(struct upipe *upipe, struct uref *uref,
                     input_hsize, input_vsize >> !!i, upipe_sws->input_pix_fmt,
                     output_hsize, output_vsize >> !!i, upipe_sws->output_pix_fmt,
                     upipe_sws->flags, NULL, NULL, NULL);
-    }
 
-    if (unlikely(upipe_sws->convert_ctx == NULL)) {
-        upipe_err(upipe, "sws_getContext failed");
-        uref_free(uref);
-        return true;
+        if (unlikely(upipe_sws->convert_ctx[i] == NULL)) {
+            upipe_err(upipe, "sws_getContext failed");
+            uref_free(uref);
+            return true;
+        }
+
+        int in_full, out_full, brightness, contrast, saturation;
+        const int *inv_table, *table;
+
+        sws_getColorspaceDetails(upipe_sws->convert_ctx[i],
+                                 (int **)&inv_table, &in_full,
+                                 (int **)&table, &out_full,
+                                 &brightness, &contrast, &saturation);
+
+        if (upipe_sws->input_colorspace != -1)
+            inv_table = sws_getCoefficients(upipe_sws->input_colorspace);
+        if (upipe_sws->input_color_range != -1)
+            in_full = upipe_sws->input_color_range;
+        if (upipe_sws->output_colorspace != -1)
+            table = sws_getCoefficients(upipe_sws->output_colorspace);
+        if (upipe_sws->output_color_range != -1)
+            out_full = upipe_sws->output_color_range;
+
+        if (unlikely(sws_setColorspaceDetails(upipe_sws->convert_ctx[i],
+                        inv_table, in_full, table, out_full,
+                        brightness, contrast, saturation)) < 0)
+            upipe_warn(upipe, "unable to set color space data");
     }
 
     /* map input */
@@ -395,6 +451,9 @@ static int upipe_sws_set_flow_def(struct upipe *upipe, struct uref *flow_def)
         uref_dump(flow_def, upipe->uprobe);
         return UBASE_ERR_EXTERNAL;
     }
+    upipe_sws->input_colorspace = upipe_sws_convert_color(upipe, flow_def);
+    upipe_sws->input_color_range =
+        ubase_check(uref_pic_flow_get_full_range(flow_def)) ? 1 : 0;
 
     flow_def = uref_dup(flow_def);
     if (unlikely(flow_def == NULL)) {
@@ -567,6 +626,9 @@ static struct upipe *upipe_sws_alloc(struct upipe_mgr *mgr,
 
     upipe_throw_ready(upipe);
 
+    upipe_sws->output_colorspace = upipe_sws_convert_color(upipe, flow_def);
+    upipe_sws->output_color_range =
+        ubase_check(uref_pic_flow_get_full_range(flow_def)) ? 1 : 0;
     UBASE_FATAL(upipe, uref_pic_flow_set_align(flow_def, 16))
     upipe_sws_store_flow_def_attr(upipe, flow_def);
     return upipe;

lib/upipe-x264/upipe_x264.c
@@ -373,6 +373,37 @@ static bool upipe_x264_open(struct upipe *upipe, int width, int height,
     if (!ubase_check(uref_pic_get_progressive(upipe_x264->flow_def_input)))
         params->b_interlaced = true;
 
+    const char *content;
+    int ret;
+    if (ubase_check(uref_pic_flow_get_video_format(
+                    upipe_x264->flow_def_input, &content)) &&
+        (ret = x264_param_parse(&upipe_x264->params, "videoformat",
+                                content)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "videoformat", content, ret);
+    content =
+        ubase_check(uref_pic_flow_get_full_range(upipe_x264->flow_def_input)) ?
+        "1" : "0";
+    if ((ret = x264_param_parse(&upipe_x264->params, "fullrange", content)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "fullrange", content, ret);
+    if (ubase_check(uref_pic_flow_get_colour_primaries(
+                    upipe_x264->flow_def_input, &content)) &&
+        (ret = x264_param_parse(&upipe_x264->params, "colorprim", content)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "colorprim", content, ret);
+    if (ubase_check(uref_pic_flow_get_transfer_characteristics(
+                    upipe_x264->flow_def_input, &content)) &&
+        (ret = x264_param_parse(&upipe_x264->params, "transfer", content)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "transfer", content, ret);
+    if (ubase_check(uref_pic_flow_get_matrix_coefficients(
+                    upipe_x264->flow_def_input, &content)) &&
+        (ret = x264_param_parse(&upipe_x264->params, "colormatrix",
+                                content)) < 0)
+        upipe_err_va(upipe, "can't set option %s:%s (%d)",
+                     "colormatrix", content, ret);
+
     /* reconfigure encoder with new parameters and return */
     if (unlikely(upipe_x264->encoder)) {
         if (!ubase_check(_upipe_x264_reconfigure(upipe)))

commit eb476a0b78ae97d02a463360f2339194488b12ff

fix infinite loop introduced in 7ddfe5b5e6041971

lib/upipe-av/upipe_avcodec_encode.c
@@ -222,9 +222,11 @@ static const char *
                                const char *upipe_color)
 {
     int i = 0;
-    while (list[i].upipe_color != NULL)
+    while (list[i].upipe_color != NULL) {
         if (!strcmp(list[i].upipe_color, upipe_color))
             return list[i].avcenc_color;
+        i++;
+    }
     return upipe_color;
 }
 