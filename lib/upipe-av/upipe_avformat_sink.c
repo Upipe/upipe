c94b509d35bcf96978beb92d08f3a1b8d0a6a0e7
Benjamin Cohen <bencoh@notk.org>
2015-05-12 14:26:03 +0200

    upipe_avfsink: set avformat stream id

--- lib/upipe-av/upipe_avformat_sink.c
+++ lib/upipe-av/upipe_avformat_sink.c
@@ -330,6 +330,11 @@ static int upipe_avfsink_sub_set_flow_def(struct upipe *upipe,
         return UBASE_ERR_EXTERNAL;
     }
 
+    uint64_t id;
+    if (likely(ubase_check(uref_flow_get_id(flow_def, &id)))) {
+        stream->id = id;
+    }
+
     uint8_t languages;
     const char *lang;
     if (ubase_check(uref_flow_get_languages(flow_def, &languages)) &&
