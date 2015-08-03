618234276bb763287fb435a3bc92afc18d785ddf
Christophe Massiot <cmassiot@openheadend.tv>
2015-08-04 00:02:22 +0200

    avcenc: fix wrong interpretation of delay param

--- lib/upipe-av/upipe_avcodec_encode.c
+++ lib/upipe-av/upipe_avcodec_encode.c
@@ -149,8 +149,6 @@ struct upipe_avcenc {
 
     /** frame counter */
     uint64_t counter;
-    /** latency in the input flow */
-    uint64_t input_latency;
     /** chroma map */
     const char *chroma_map[UPIPE_AV_MAX_PLANES];
 
@@ -484,21 +482,6 @@ static bool upipe_avcenc_encode_frame(struct upipe *upipe,
 
         if (codec->type == AVMEDIA_TYPE_AUDIO && context->frame_size > 0) {
             uref_sound_flow_set_samples(flow_def_attr, context->frame_size);
-        }
-    }
-    if (context->delay) {
-        struct urational fps;
-        uint64_t rate;
-        if (ubase_check(uref_pic_flow_get_fps(upipe_avcenc->flow_def_input,
-                                              &fps))) {
-            UBASE_FATAL(upipe, uref_clock_set_latency(flow_def_attr,
-                    upipe_avcenc->input_latency +
-                    context->delay * UCLOCK_FREQ * fps.den / fps.num));
-        } else if (ubase_check(uref_sound_flow_get_rate(
-                        upipe_avcenc->flow_def_input, &rate))) {
-            UBASE_FATAL(upipe, uref_clock_set_latency(flow_def_attr,
-                    upipe_avcenc->input_latency +
-                    context->delay * UCLOCK_FREQ / rate));
         }
     }
 
@@ -1073,10 +1056,6 @@ static int upipe_avcenc_set_flow_def(struct upipe *upipe, struct uref *flow_def)
         uref_pic_flow_clear_format(flow_def);
         upipe_avcenc_store_flow_def(upipe, flow_def);
     }
-
-    upipe_avcenc->input_latency = 0;
-    uref_clock_get_latency(upipe_avcenc->flow_def_input,
-                           &upipe_avcenc->input_latency);
     return UBASE_ERR_NONE;
 }
 
