50f48c93af062e4f76970efe390c0b400c952d94
Kieran Kunhya <kierank@ob-encoder.com>
2015-08-16 13:34:39 +0100

    upipe-modules: Add opus rtp decaps

---
+++ include/upipe-modules/upipe_rtp_opus.h
@@ -0,0 +1,16 @@
+#ifndef _UPIPE_MODULES_RTP_OPUS_H_
+# define UPIPE_MODULES_RTP_OPUS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define UPIPE_RTP_OPUS_SIGNATURE UBASE_FOURCC('r','t','p','o')
+
+struct upipe_mgr *upipe_rtp_opus_mgr_alloc(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
