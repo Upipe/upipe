5d0b5dd6a7d265225f56946b98f766903a0ea09a
Arnaud de Turckheim <quarium@gmail.com>
2015-08-27 10:39:19 +0200

    upipe_stream_switcher: add events when an input stream is selected

--- include/upipe-modules/upipe_stream_switcher.h
+++ include/upipe-modules/upipe_stream_switcher.h
@@ -35,6 +35,33 @@ extern "C" {
 #define UPIPE_STREAM_SWITCHER_SIGNATURE UBASE_FOURCC('s','t','s','w')
 #define UPIPE_STREAM_SWITCHER_SUB_SIGNATURE UBASE_FOURCC('s','t','s','s')
 
+/** @This extends uprobe_event with specific events for stream switcher
+ * input. */
+enum upipe_stream_switcher_sub_event {
+    UPROBE_STREAM_SWITCHER_SUB_SENTINEL = UPROBE_LOCAL,
+
+    /** stream is sync */
+    UPROBE_STREAM_SWITCHER_SUB_SYNC,
+    /** stream is switched on */
+    UPROBE_STREAM_SWITCHER_SUB_ENTERING,
+    /** stream is switched off */
+    UPROBE_STREAM_SWITCHER_SUB_LEAVING,
+    /** stream is now destroyed */
+    UPROBE_STREAM_SWITCHER_SUB_DESTROY,
+};
+
+static inline const char *upipe_stream_switcher_sub_event_str(int event)
+{
+    switch ((enum upipe_stream_switcher_sub_event)event) {
+    UBASE_CASE_TO_STR(UPROBE_STREAM_SWITCHER_SUB_SYNC);
+    UBASE_CASE_TO_STR(UPROBE_STREAM_SWITCHER_SUB_ENTERING);
+    UBASE_CASE_TO_STR(UPROBE_STREAM_SWITCHER_SUB_LEAVING);
+    UBASE_CASE_TO_STR(UPROBE_STREAM_SWITCHER_SUB_DESTROY);
+    case UPROBE_STREAM_SWITCHER_SUB_SENTINEL: break;
+    }
+    return NULL;
+}
+
 /** @This returns the management structure for all stream switchers.
  *
  * @return pointer to manager
