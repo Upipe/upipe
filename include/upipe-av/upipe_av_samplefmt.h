Upipe/upipe

commit 6f3174828a7dc42db22265ba522cd9de04d58d68

avcenc: fix encoding mono audio

include/upipe-av/upipe_av_samplefmt.h
@@ -138,6 +138,36 @@ static inline enum AVSampleFormat
     return fmt;
 }
 
+/** @This allows to match an av sample format with a flow definition.
+ *
+ * @param flow_def flow definition
+ * @param fmt av sample format
+ * @return an error code
+ */
+static inline int
+    upipe_av_samplefmt_match_flow_def(struct uref *flow_def,
+                                      enum AVSampleFormat fmt)
+{
+    const char *def;
+    uint8_t planes;
+    if (unlikely(!ubase_check(uref_flow_get_def(flow_def, &def)) ||
+                 !ubase_check(uref_sound_flow_get_planes(flow_def, &planes))))
+        return UBASE_ERR_INVALID;
+    enum AVSampleFormat tmp = AV_SAMPLE_FMT_NONE;
+    for (unsigned int i = 0; upipe_av_sample_fmts[i].fmt != AV_SAMPLE_FMT_NONE;
+         i++) {
+        if (!ubase_ncmp(def, upipe_av_sample_fmts[i].flow_def)) {
+            tmp = upipe_av_sample_fmts[i].fmt;
+            break;
+        }
+    }
+    if (tmp == fmt)
+        return UBASE_ERR_NONE;
+    if (planes == 1 && av_get_planar_sample_fmt(tmp) == fmt)
+        return UBASE_ERR_NONE;
+    return UBASE_ERR_INVALID;
+}
+
 #ifdef __cplusplus
 }
 #endif

lib/upipe-av/upipe_avcodec_encode.c
@@ -1124,17 +1124,15 @@ static int upipe_avcenc_set_flow_def(struct upipe *upipe, struct uref *flow_def)
         upipe_avcenc_store_flow_def_check(upipe, flow_def_check);
 
     } else {
-        uint8_t channels = 0;
-        enum AVSampleFormat sample_fmt =
-            upipe_av_samplefmt_from_flow_def(flow_def, &channels);
         const enum AVSampleFormat *sample_fmts = codec->sample_fmts;
-        if (sample_fmt == AV_SAMPLE_FMT_NONE || sample_fmts == NULL) {
+        if (sample_fmts == NULL) {
             upipe_err_va(upipe, "unknown sample format %s", def);
             uref_free(flow_def_check);
             return UBASE_ERR_INVALID;
         }
         while (*sample_fmts != -1) {
-            if (*sample_fmts == sample_fmt)
+            if (ubase_check(upipe_av_samplefmt_match_flow_def(flow_def,
+                                                              *sample_fmts)))
                 break;
             sample_fmts++;
         }
@@ -1168,8 +1166,14 @@ static int upipe_avcenc_set_flow_def(struct upipe *upipe, struct uref *flow_def)
         context->time_base.num = 1;
         context->time_base.den = 1;//rate; FIXME
 
+        uint8_t channels;
         const uint64_t *channel_layouts =
             upipe_avcenc->context->codec->channel_layouts;
+        if (!ubase_check(uref_sound_flow_get_channels(flow_def, &channels))) {
+            upipe_err_va(upipe, "unsupported channels");
+            uref_free(flow_def_check);
+            return UBASE_ERR_INVALID;
+        }
         while (*channel_layouts != 0) {
             if (av_get_channel_layout_nb_channels(*channel_layouts) == channels)
                 break;

commit a3a361de007147ac38b2fc7a9d290ef7fce775b4

av: fix upipe_av_samplefmt_match_flow_def
This fixes a bug in upipe_avcenc introduced in
6f3174828a7dc42db22265ba522cd9de04d58d68

include/upipe-av/upipe_av_samplefmt.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2014 OpenHeadend S.A.R.L.
+ * Copyright (C) 2013-2016 OpenHeadend S.A.R.L.
  *
  * Authors: Christophe Massiot
  *
@@ -149,8 +149,11 @@ static inline int
                                       enum AVSampleFormat fmt)
 {
     const char *def;
+    uint8_t channels;
     uint8_t planes;
     if (unlikely(!ubase_check(uref_flow_get_def(flow_def, &def)) ||
+                 !ubase_check(uref_sound_flow_get_channels(flow_def,
+                                                           &channels)) ||
                  !ubase_check(uref_sound_flow_get_planes(flow_def, &planes))))
         return UBASE_ERR_INVALID;
     enum AVSampleFormat tmp = AV_SAMPLE_FMT_NONE;
@@ -161,9 +164,13 @@ static inline int
             break;
         }
     }
-    if (tmp == fmt)
+    if (tmp == AV_SAMPLE_FMT_NONE)
+        return UBASE_ERR_INVALID;
+
+    :f (planes == 1 && tmp == fmt)
         return UBASE_ERR_NONE;
-    if (planes == 1 && av_get_planar_sample_fmt(tmp) == fmt)
+    tmp = av_get_planar_sample_fmt(tmp);
+    if ((channels == 1 || planes > 1) && tmp == fmt)
         return UBASE_ERR_NONE;
     return UBASE_ERR_INVALID;
 }