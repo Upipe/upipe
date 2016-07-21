9a22fcc608f5152c8cc9d0fd3338be23a17fb587
Rafaël Carré <funman@videolan.org>
2016-07-21 17:33:42 +0200

    upipe-speexdsp: resample audio to cancel drift rate

---
+++ tests/upipe_speexdsp_test.c
@@ -0,0 +1,232 @@
+/*
+ * Copyright (C) 2016 Open Broadcast Systems Ltd
+ *
+ * Authors: Rafaël Carré
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#undef NDEBUG
+
+#include <upipe/uprobe.h>
+#include <upipe/uprobe_stdio.h>
+#include <upipe/uprobe_prefix.h>
+#include <upipe/uprobe_ubuf_mem.h>
+#include <upipe/uclock.h>
+#include <upipe/umem.h>
+#include <upipe/umem_alloc.h>
+#include <upipe/udict.h>
+#include <upipe/udict_inline.h>
+#include <upipe/uref.h>
+#include <upipe/uref_std.h>
+#include <upipe/uref_sound.h>
+#include <upipe/uref_sound_flow.h>
+#include <upipe/uref_clock.h>
+#include <upipe/uref_dump.h>
+#include <upipe/upipe.h>
+#include <upipe/ubuf_sound_mem.h>
+
+#include <upipe-speexdsp/upipe_speexdsp.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+
+#define UDICT_POOL_DEPTH    5
+#define UREF_POOL_DEPTH     5
+#define UBUF_POOL_DEPTH     0
+#define SAMPLES             1024
+#define UPROBE_LOG_LEVEL    UPROBE_LOG_VERBOSE
+#define ALIGN               0
+
+static bool got_input = false;
+
+/** definition of our uprobe */
+static int catch(struct uprobe *uprobe, struct upipe *upipe,
+                 int event, va_list args)
+{
+    switch (event) {
+        case UPROBE_READY:
+        case UPROBE_DEAD:
+        case UPROBE_NEW_FLOW_DEF:
+            break;
+        default:
+            assert(0);
+            break;
+    }
+    return UBASE_ERR_NONE;
+}
+
+/** helper phony pipe */
+static struct upipe *test_alloc(struct upipe_mgr *mgr, struct uprobe *uprobe,
+                                uint32_t signature, va_list args)
+{
+    struct upipe *upipe = malloc(sizeof(struct upipe));
+	upipe_init(upipe, mgr, uprobe);
+    upipe_throw_ready(upipe);
+    return upipe;
+}
+
+/** helper phony pipe */
+static void test_input(struct upipe *upipe, struct uref *uref,
+                       struct upump **upump_p)
+{
+    assert(uref != NULL);
+    upipe_dbg(upipe, "===> received input uref");
+    size_t size;
+    ubase_assert(uref_sound_size(uref, &size, NULL));
+    assert(size == SAMPLES+1);
+    uref_dump(uref, upipe->uprobe);
+    uref_free(uref);
+    got_input = true;
+}
+
+/** helper phony pipe */
+static int test_control(struct upipe *upipe, int command, va_list args)
+{
+    switch (command) {
+        case UPIPE_SET_FLOW_DEF:
+            return UBASE_ERR_NONE;
+        default:
+            assert(0);
+            return UBASE_ERR_UNHANDLED;
+    }
+}
+
+/** helper phony pipe */
+static void test_free(struct upipe *upipe)
+{
+    upipe_dbg(upipe, "releasing pipe");
+    upipe_throw_dead(upipe);
+    upipe_clean(upipe);
+    free(upipe);
+}
+
+/** helper phony pipe */
+static struct upipe_mgr test_mgr = {
+    .refcount = NULL,
+    .signature = 0,
+    .upipe_alloc = test_alloc,
+    .upipe_input = test_input,
+    .upipe_control = test_control
+};
+
+static void fill_in(struct ubuf *ubuf)
+{
+    size_t size;
+    uint8_t sample_size;
+    ubase_assert(ubuf_sound_size(ubuf, &size, &sample_size));
+
+    const char *channel = NULL;
+    int16_t v = 0;
+    while (ubase_check(ubuf_sound_plane_iterate(ubuf, &channel)) &&
+           channel != NULL) {
+        int16_t *buffer;
+        ubase_assert(ubuf_sound_plane_write_int16_t(ubuf, channel, 0, -1,
+                                                    &buffer));
+
+        for (int x = 0; x < size; x++) {
+            buffer[2*x+0] = v++;
+            buffer[2*x+1] = v++;
+        }
+        ubase_assert(ubuf_sound_plane_unmap(ubuf, channel, 0, -1));
+    }
+}
+
+int main(int argc, char **argv)
+{
+    printf("Compiled %s %s - %s\n", __DATE__, __TIME__, __FILE__);
+    int i, j, k;
+
+    /* uref and mem management */
+    struct umem_mgr *umem_mgr = umem_alloc_mgr_alloc();
+    assert(umem_mgr != NULL);
+    struct udict_mgr *udict_mgr = udict_inline_mgr_alloc(UDICT_POOL_DEPTH,
+                                                         umem_mgr, -1, -1);
+    assert(udict_mgr != NULL);
+    struct uref_mgr *uref_mgr = uref_std_mgr_alloc(UREF_POOL_DEPTH,
+                                                   udict_mgr, 0);
+    assert(uref_mgr != NULL);
+
+    /* sound */
+    struct ubuf_mgr *sound_mgr = ubuf_sound_mem_mgr_alloc(UBUF_POOL_DEPTH,
+                                             UBUF_POOL_DEPTH, umem_mgr, 2*2, ALIGN);
+    assert(sound_mgr);
+    ubase_assert(ubuf_sound_mem_mgr_add_plane(sound_mgr, "lr"));
+
+    /* uprobe stuff */
+    struct uprobe uprobe;
+    uprobe_init(&uprobe, catch, NULL);
+    struct uprobe *logger = uprobe_stdio_alloc(&uprobe, stdout,
+                                               UPROBE_LOG_LEVEL);
+    assert(logger != NULL);
+    logger = uprobe_ubuf_mem_alloc(logger, umem_mgr, UBUF_POOL_DEPTH,
+                                                     UBUF_POOL_DEPTH);
+    assert(logger != NULL);
+
+    /* build speexdsp pipe */
+    struct upipe_mgr *upipe_speexdsp_mgr = upipe_speexdsp_mgr_alloc();
+    assert(upipe_speexdsp_mgr);
+    struct upipe *speexdsp = upipe_void_alloc(upipe_speexdsp_mgr,
+        uprobe_pfx_alloc(uprobe_use(logger), UPROBE_LOG_LEVEL, "speexdsp"));
+    assert(speexdsp);
+
+    /* build phony pipe */
+    struct upipe *test = upipe_void_alloc(&test_mgr,
+            uprobe_pfx_alloc(uprobe_use(logger), UPROBE_LOG_LEVEL,
+                             "test"));
+    assert(test != NULL);
+    ubase_assert(upipe_set_output(speexdsp, test));
+
+    struct uref *flow_def =
+        uref_sound_flow_alloc_def(uref_mgr, "s16.", 2, 2 * 2);
+    ubase_assert(uref_sound_flow_add_plane(flow_def, "lr"));
+    ubase_assert(uref_sound_flow_set_rate(flow_def, 48000));
+    ubase_assert(upipe_set_flow_def(speexdsp, flow_def));
+    uref_free(flow_def);
+
+    struct uref *uref = uref_sound_alloc(uref_mgr, sound_mgr, SAMPLES);
+    fill_in(uref->ubuf);
+    struct urational drift = {
+        .num = SAMPLES+1,
+        .den = SAMPLES,
+    };
+    uref_clock_set_rate(uref, drift);
+    upipe_input(speexdsp, uref, NULL);
+    assert(got_input);
+
+    /* release pipe */
+    upipe_release(speexdsp);
+    test_free(test);
+
+    /* release managers */
+    upipe_mgr_release(upipe_speexdsp_mgr); // no-op
+    ubuf_mgr_release(sound_mgr);
+    uref_mgr_release(uref_mgr);
+    umem_mgr_release(umem_mgr);
+    udict_mgr_release(udict_mgr);
+    uprobe_release(logger);
+    uprobe_clean(&uprobe);
+
+    return 0;
+}
