6bdb05625f158ac6b1f89be8aa00904ad335a63f
Rafaël Carré <funman@videolan.org>
2016-08-11 12:32:30 +0200

    upipe-freetype: text rendering

---
+++ include/upipe-freetype/upipe_freetype.h
@@ -0,0 +1,8 @@
+#ifndef _UPIPE_MODULES_UPIPE_FREETYPE_H_
+# define _UPIPE_MODULES_UPIPE_FREETYPE_H_
+
+#define UPIPE_FREETYPE_SIGNATURE UBASE_FOURCC('f','r','t','2')
+
+struct upipe_mgr *upipe_freetype_mgr_alloc(void);
+
+#endif /* !_UPIPE_MODULES_UPIPE_FREETYPE_H_ */
