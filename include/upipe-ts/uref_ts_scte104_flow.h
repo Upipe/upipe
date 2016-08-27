ccb03f16d32409eaed60f53c69b926ccf6f7185d
Christophe Massiot <cmassiot@openheadend.tv>
2016-08-27 21:27:19 +0200

    SCTE-104 decoder

---
+++ include/upipe-ts/uref_ts_scte104_flow.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 OpenHeadend S.A.R.L.
+ *
+ * Authors: Christophe Massiot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short Upipe flow definition attributes for TS SCTE 104
+ */
+
+#ifndef _UPIPE_TS_UREF_TS_SCTE104_FLOW_H_
+/** @hidden */
+#define _UPIPE_TS_UREF_TS_SCTE104_FLOW_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <upipe/uref.h>
+#include <upipe/uref_attr.h>
+
+#include <string.h>
+#include <stdint.h>
+
+UREF_ATTR_UNSIGNED(ts_scte104_flow, dpi_pid_index, "scte104.dpi", DPI PID index)
+
+#ifdef __cplusplus
+}
+#endif
+#endif
