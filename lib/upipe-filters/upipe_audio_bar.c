bfcf3def12cfcb7b2e09069928ad3d378de6549b
Rafaël Carré <funman@videolan.org>
2016-09-08 21:37:40 +0200

    upipe_audio_bar: iec_scale takes double, so do not use f

--- lib/upipe-filters/upipe_audio_bar.c
+++ lib/upipe-filters/upipe_audio_bar.c
@@ -177,21 +177,21 @@ static struct upipe *upipe_audiobar_alloc(struct upipe_mgr *mgr,
  */
 static double iec_scale(double dB)
 {
-    if (dB < -70.0f)
-        return 0.0f;
-    if (dB < -60.0f)
-        return (dB + 70.0f) * 0.0025f;
-    if (dB < -50.0f)
-        return (dB + 60.0f) * 0.005f + 0.025f;
-    if (dB < -40.0f)
-        return (dB + 50.0f) * 0.0075f + 0.075f;
-    if (dB < -30.0f)
-        return (dB + 40.0f) * 0.015f + 0.15f;
-    if (dB < -20.0f)
-        return (dB + 30.0f) * 0.02f + 0.3f;
-    if (dB < -0.001f || dB > 0.001f)  /* if (dB < 0.0f) */
-        return (dB + 20.0f) * 0.025f + 0.5f;
-    return 1.0f;
+    if (dB < -70.0)
+        return 0.0;
+    if (dB < -60.0)
+        return (dB + 70.0) * 0.0025;
+    if (dB < -50.0)
+        return (dB + 60.0) * 0.005f + 0.025;
+    if (dB < -40.0)
+        return (dB + 50.0) * 0.0075f + 0.075;
+    if (dB < -30.0)
+        return (dB + 40.0) * 0.015f + 0.15;
+    if (dB < -20.0)
+        return (dB + 30.0) * 0.02f + 0.3;
+    if (dB < -0.001f || dB > 0.001)  /* if (dB < 0.0) */
+        return (dB + 20.0) * 0.025f + 0.5;
+    return 1.0;
 }
 
 /** @internal @This copies pixels to the destination picture.
