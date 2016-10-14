Upipe/upipe

commit 053310c65295b0745fc716f376fbc79215a02f47

introduce preroll for file processing

lib/upipe-ts/upipe_ts_mux.c
@@ -341,6 +341,8 @@ struct upipe_ts_mux {
     struct uref *uref;
     /** size of current aggregation */
     size_t uref_size;
+    /** true during the preroll period */
+    bool preroll;
 
     /** manager of the pseudo inner sink */
     struct upipe_mgr inner_sink_mgr;
@@ -1333,7 +1335,7 @@ static int upipe_ts_mux_input_set_flow_def(struct upipe *upipe,
     if (latency + input->buffer_duration > upipe_ts_mux->latency) {
         upipe_ts_mux->latency = latency + input->buffer_duration;
         upipe_ts_mux_build_flow_def(upipe_ts_mux_to_upipe(upipe_ts_mux));
-    } else if (au_per_sec.den) {
+    } else if (upipe_ts_mux->uclock != NULL && au_per_sec.den) { /* live mode */
         upipe_set_max_length(input->encaps,
                 (MIN_BUFFERING + upipe_ts_mux->latency) * au_per_sec.num /
                 au_per_sec.den / UCLOCK_FREQ);
@@ -1739,7 +1741,8 @@ static void upipe_ts_mux_program_change(struct upipe *upipe)
             old_pcr_input = input;
         }
         if (input->input_type == UPIPE_TS_MUX_INPUT_OTHER ||
-            input->input_type == UPIPE_TS_MUX_INPUT_SCTE35)
+            input->input_type == UPIPE_TS_MUX_INPUT_SCTE35 ||
+            input->input_type == UPIPE_TS_MUX_INPUT_UNKNOWN)
             continue;
         if (pcr_input == NULL ||
             (pcr_input->input_type == UPIPE_TS_MUX_INPUT_AUDIO &&
@@ -2378,6 +2381,7 @@ static struct upipe *upipe_ts_mux_alloc(struct upipe_mgr *mgr,
     upipe_ts_mux->cr_sys_remainder = 0;
     upipe_ts_mux->uref = NULL;
     upipe_ts_mux->uref_size = 0;
+    upipe_ts_mux->preroll = true;
 
     uprobe_init(&upipe_ts_mux->probe, upipe_ts_mux_probe, NULL);
     upipe_ts_mux->probe.refcount = upipe_ts_mux_to_urefcount_real(upipe_ts_mux);
@@ -2716,7 +2720,9 @@ static uint64_t upipe_ts_mux_check_available(struct upipe *upipe)
                     upipe_release(input->encaps);
                     continue;
                 } else if (input->input_type != UPIPE_TS_MUX_INPUT_OTHER &&
-                           input->input_type != UPIPE_TS_MUX_INPUT_SCTE35) {
+                           input->input_type != UPIPE_TS_MUX_INPUT_SCTE35 &&
+                           (input->input_type != UPIPE_TS_MUX_INPUT_UNKNOWN ||
+                            mux->preroll)) {
                     upipe_release(upipe_ts_mux_program_to_upipe(program));
                     return UINT64_MAX;
                 }
@@ -3126,6 +3132,8 @@ static void upipe_ts_mux_build_flow_def(struct upipe *upipe)
         upipe_throw_fatal(upipe, UBASE_ERR_ALLOC);
     upipe_ts_mux_store_flow_def(upipe, flow_def);
 
+    if (mux->uclock == NULL)
+        return; /* file mode */
 
     struct uchain *uchain_program;
     ulist_foreach (&mux->programs, uchain_program) {
@@ -3447,6 +3455,18 @@ static int upipe_ts_mux_set_output_size(struct upipe *upipe, unsigned int mtu)
         }
     }
     upipe_ts_mux_build_flow_def(upipe);
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This ends the preroll period.
+ *
+ * @param upipe description structure of the pipe
+ * @return an error code
+ */
+static int upipe_ts_mux_end_preroll(struct upipe *upipe)
+{
+    struct upipe_ts_mux *upipe_ts_mux = upipe_ts_mux_from_upipe(upipe);
+    upipe_ts_mux->preroll = false;
     return UBASE_ERR_NONE;
 }
 
@@ -3981,6 +4001,8 @@ static int _upipe_ts_mux_control(struct upipe *upipe, int command, va_list args)
             struct upipe **p = va_arg(args, struct upipe **);
             return upipe_ts_mux_iterate_sub(upipe, p);
         }
+        case UPIPE_END_PREROLL:
+            return upipe_ts_mux_end_preroll(upipe);
 
         case UPIPE_TS_MUX_GET_CONFORMANCE: {
             UBASE_SIGNATURE_CHECK(args, UPIPE_TS_MUX_SIGNATURE)

include/upipe/upipe.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2015 OpenHeadend S.A.R.L.
+ * Copyright (C) 2012-2016 OpenHeadend S.A.R.L.
  *
  * Authors: Christophe Massiot
  *
@@ -97,6 +97,8 @@ enum upipe_command {
     UPIPE_SET_MAX_LENGTH,
     /** flushes all currently held buffers and unblock the sources (void) */
     UPIPE_FLUSH,
+    /** end the preroll period (void) */
+    UPIPE_END_PREROLL,
 
     /*
      * Output-related commands
@@ -1250,6 +1252,16 @@ static inline int upipe_unregister_request(struct upipe *upipe,
 static inline int upipe_flush(struct upipe *upipe)
 {
     return upipe_control(upipe, UPIPE_FLUSH);
+}
+
+/** @This ends the preroll period in a pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @return an error code
+ */
+static inline int upipe_end_preroll(struct upipe *upipe)
+{
+    return upipe_control(upipe, UPIPE_END_PREROLL);
 }
 
 /** @deprecated @see upipe_flush */

lib/upipe-modules/upipe_even.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 OpenHeadend S.A.R.L.
+ * Copyright (C) 2015-2016 OpenHeadend S.A.R.L.
  *
  * Authors: Christophe Massiot
  *
@@ -62,8 +62,14 @@ struct upipe_even {
     uint64_t first_date;
     /** last date */
     uint64_t last_date;
+    /** true during the preroll period */
+    bool preroll;
     /** true if an input is dead */
     bool dead;
+    /** true if we are in the subpipe loop */
+    bool in_loop;
+    /** true if we need to restart the subpipe loop */
+    bool restart;
 
     /** list of subs */
     struct uchain subs;
@@ -216,14 +222,16 @@ static bool upipe_even_sub_handle(struct upipe *upipe, struct uref *uref,
     uref_clock_get_duration(uref, &duration);
 
     if (unlikely(date + duration < upipe_even->first_date)) {
-        upipe_dbg(upipe, "removing orphan uref");
+        upipe_dbg_va(upipe, "removing early uref (%"PRId64")",
+                     upipe_even->first_date - date - duration);
         uref_free(uref);
         return true;
     }
 
     if (date > upipe_even->last_date) {
         if (upipe_even->dead) {
-            upipe_dbg(upipe, "removing orphan uref");
+            upipe_dbg_va(upipe, "removing late uref (%"PRId64")",
+                         date - upipe_even->last_date);
             uref_free(uref);
             return true;
         }
@@ -253,10 +261,12 @@ static void upipe_even_sub_process(struct upipe *upipe, struct upump **upump_p)
         if (!upipe_even_sub_handle(upipe, uref, upump_p)) {
             upipe_even_sub_unshift_input(upipe, uref);
             break;
-        } else if (upipe_even_sub_check_input(upipe))
+        } else if (upipe_even_sub_check_input(upipe)) {
             /* All packets have been output, release again the pipe that has
              * been used in @ref upipe_even_sub_input. */
             upipe_release(upipe);
+            break;
+        }
     }
     upipe_release(upipe);
 }
@@ -272,8 +282,7 @@ static int upipe_even_sub_set_flow_def(struct upipe *upipe,
 {
     if (flow_def == NULL)
         return UBASE_ERR_INVALID;
-    struct upipe_even_sub *upipe_even_sub =
-        upipe_even_sub_from_upipe(upipe);
+    struct upipe_even_sub *upipe_even_sub = upipe_even_sub_from_upipe(upipe);
     const char *def;
     if (likely(ubase_check(uref_flow_get_def(flow_def, &def)))) {
         if (!ubase_ncmp(def, "pic.sub.") || strstr(def, ".pic.sub."))
@@ -355,15 +364,23 @@ static int upipe_even_sub_control(struct upipe *upipe,
  */
 static void upipe_even_sub_free(struct upipe *upipe)
 {
+    struct upipe_even_sub *upipe_even_sub = upipe_even_sub_from_upipe(upipe);
     struct upipe_even *upipe_even = upipe_even_from_sub_mgr(upipe->mgr);
-    upipe_even->dead = true;
-    upipe_even_process(upipe_even_to_upipe(upipe_even), NULL);
 
     upipe_throw_dead(upipe);
     upipe_even_sub_clean_output(upipe);
     upipe_even_sub_clean_input(upipe);
     upipe_even_sub_clean_sub(upipe);
     upipe_even_sub_clean_urefcount(upipe);
+
+    if (upipe_even_sub->type != UPIPE_EVEN_SUBPIC) {
+        upipe_even->dead = true;
+        if (!upipe_even->in_loop)
+            upipe_even_process(upipe_even_to_upipe(upipe_even), NULL);
+        else
+            upipe_even->restart = true;
+    }
+
     upipe_even_sub_free_void(upipe);
 }
 
@@ -404,9 +421,65 @@ static struct upipe *upipe_even_alloc(struct upipe_mgr *mgr,
     upipe_even_init_sub_subs(upipe);
     struct upipe_even *upipe_even = upipe_even_from_upipe(upipe);
     upipe_even->first_date = UINT64_MAX;
+    upipe_even->preroll = true;
     upipe_even->dead = false;
+    upipe_even->in_loop = false;
+    upipe_even->restart = false;
     upipe_throw_ready(upipe);
     return upipe;
+}
+
+/** @internal @This checks if we have got packets on video and audio inputs.
+ *
+ * @param upipe description structure of the pipe
+ * @return true if we are ready
+ */
+static bool upipe_even_check(struct upipe *upipe)
+{
+    struct upipe_even *upipe_even = upipe_even_from_upipe(upipe);
+    struct uchain *uchain;
+
+    if (upipe_even->dead)
+        return true;
+
+    if (unlikely(upipe_even->first_date == UINT64_MAX)) {
+        uint64_t first_date = 0;
+        ulist_foreach (&upipe_even->subs, uchain) {
+            struct upipe_even_sub *upipe_even_sub =
+                upipe_even_sub_from_uchain(uchain);
+            if (upipe_even_sub->type == UPIPE_EVEN_SUBPIC ||
+                (upipe_even_sub->type == UPIPE_EVEN_UNKNOWN &&
+                 !upipe_even->preroll))
+                continue;
+
+            if (upipe_even_sub->first_date == UINT64_MAX)
+                return false;
+            if (upipe_even_sub->first_date > first_date)
+                first_date = upipe_even_sub->first_date;
+        }
+
+        upipe_even->first_date = first_date;
+        upipe_dbg_va(upipe, "first date %"PRIu64, first_date);
+    }
+
+    uint64_t last_date = UINT64_MAX;
+    ulist_foreach (&upipe_even->subs, uchain) {
+        struct upipe_even_sub *upipe_even_sub =
+            upipe_even_sub_from_uchain(uchain);
+        if (upipe_even_sub->type == UPIPE_EVEN_SUBPIC ||
+            (upipe_even_sub->type == UPIPE_EVEN_UNKNOWN &&
+             !upipe_even->preroll))
+            continue;
+
+        if (upipe_even_sub->last_date == UINT64_MAX)
+            return false;
+        if (upipe_even_sub->last_date < last_date)
+            last_date = upipe_even_sub->last_date;
+    }
+
+    upipe_even->last_date = last_date;
+    upipe_verbose_va(upipe, "last date %"PRIu64, last_date);
+    return true;
 }
 
 /** @internal @This checks if we have got packets on video and audio inputs, so
@@ -418,51 +491,39 @@ static struct upipe *upipe_even_alloc(struct upipe_mgr *mgr,
 static void upipe_even_process(struct upipe *upipe, struct upump **upump_p)
 {
     struct upipe_even *upipe_even = upipe_even_from_upipe(upipe);
-    struct uchain *uchain;
+    upipe_use(upipe);
 
-    if (!upipe_even->dead) {
-        if (unlikely(upipe_even->first_date == UINT64_MAX)) {
-            uint64_t first_date = 0;
-            ulist_foreach (&upipe_even->subs, uchain) {
-                struct upipe_even_sub *upipe_even_sub =
-                    upipe_even_sub_from_uchain(uchain);
-                if (upipe_even_sub->type == UPIPE_EVEN_SUBPIC)
-                    continue;
+    do {
+        upipe_even->restart = false;
 
-                if (upipe_even_sub->first_date == UINT64_MAX)
-                    return;
-                if (upipe_even_sub->first_date > first_date)
-                    first_date = upipe_even_sub->first_date;
-            }
+        if (!upipe_even_check(upipe))
+            break;
 
-            upipe_even->first_date = first_date;
-        }
-
-        uint64_t last_date = UINT64_MAX;
-        ulist_foreach (&upipe_even->subs, uchain) {
+        upipe_even->in_loop = true;
+        struct uchain *uchain, *uchain_tmp;
+        ulist_delete_foreach (&upipe_even->subs, uchain, uchain_tmp) {
             struct upipe_even_sub *upipe_even_sub =
                 upipe_even_sub_from_uchain(uchain);
-            if (upipe_even_sub->type == UPIPE_EVEN_SUBPIC)
-                continue;
-
-            if (upipe_even_sub->last_date == UINT64_MAX)
-                return;
-            if (upipe_even_sub->last_date < last_date)
-                last_date = upipe_even_sub->last_date;
+            upipe_even_sub_process(upipe_even_sub_to_upipe(upipe_even_sub),
+                                   upump_p);
         }
+        upipe_even->in_loop = false;
+    } while (upipe_even->restart);
 
-        upipe_even->last_date = last_date;
-    }
-
-    upipe_use(upipe);
-    struct uchain *uchain_tmp;
-    ulist_delete_foreach (&upipe_even->subs, uchain, uchain_tmp) {
-        struct upipe_even_sub *upipe_even_sub =
-            upipe_even_sub_from_uchain(uchain);
-        upipe_even_sub_process(upipe_even_sub_to_upipe(upipe_even_sub),
-                               upump_p);
-    }
     upipe_release(upipe);
+}
+
+/** @internal @This ends the preroll period.
+ *
+ * @param upipe description structure of the pipe
+ * @return an error code
+ */
+static int upipe_even_end_preroll(struct upipe *upipe)
+{
+    struct upipe_even *upipe_even = upipe_even_from_upipe(upipe);
+    upipe_even->preroll = false;
+    upipe_even_process(upipe, NULL);
+    return UBASE_ERR_NONE;
 }
 
 /** @internal @This processes control commands on a even pipe.
@@ -483,6 +544,8 @@ static int upipe_even_control(struct upipe *upipe, int command, va_list args)
             struct upipe **p = va_arg(args, struct upipe **);
             return upipe_even_iterate_sub(upipe, p);
         }
+        case UPIPE_END_PREROLL:
+            return upipe_even_end_preroll(upipe);
 
         default:
             return UBASE_ERR_UNHANDLED;

commit 2e5f4f9ae9719d60a0b20924ae4f7ada28e760fb

ts_mux: fix file mode
Bug introduced in 053310c65295b0745fc716f376fbc79215a02f47.

lib/upipe-ts/upipe_ts_mux.c
@@ -80,6 +80,7 @@
 #include <stdbool.h>
 #include <stdarg.h>
 #include <string.h>
+#include <limits.h>
 #include <assert.h>
 
 #include <bitstream/mpeg/ts.h>
@@ -1339,7 +1340,8 @@ static int upipe_ts_mux_input_set_flow_def(struct upipe *upipe,
         upipe_set_max_length(input->encaps,
                 (MIN_BUFFERING + upipe_ts_mux->latency) * au_per_sec.num /
                 au_per_sec.den / UCLOCK_FREQ);
-    }
+    } else /* file mode */
+        upipe_set_max_length(input->encaps, UINT_MAX);
 
     upipe_notice_va(upipe,
             "adding %s on PID %"PRIu64" (%"PRIu64" bits/s), latency %"PRIu64" ms, buffer %"PRIu64" ms",
@@ -3132,9 +3134,6 @@ static void upipe_ts_mux_build_flow_def(struct upipe *upipe)
         upipe_throw_fatal(upipe, UBASE_ERR_ALLOC);
     upipe_ts_mux_store_flow_def(upipe, flow_def);
 
-    if (mux->uclock == NULL)
-        return; /* file mode */
-
     struct uchain *uchain_program;
     ulist_foreach (&mux->programs, uchain_program) {
         struct upipe_ts_mux_program *program =
@@ -3143,11 +3142,16 @@ static void upipe_ts_mux_build_flow_def(struct upipe *upipe)
         ulist_foreach (&program->inputs, uchain_input) {
             struct upipe_ts_mux_input *input =
                 upipe_ts_mux_input_from_uchain(uchain_input);
-            if (input->encaps != NULL && input->original_au_per_sec.den)
-                upipe_set_max_length(input->encaps,
-                        (MIN_BUFFERING + mux->latency) *
-                        input->original_au_per_sec.num /
-                        input->original_au_per_sec.den / UCLOCK_FREQ);
+            if (input->encaps != NULL) {
+                if (mux->uclock != NULL && input->original_au_per_sec.den)
+                    /* live mode */
+                    upipe_set_max_length(input->encaps,
+                            (MIN_BUFFERING + mux->latency) *
+                            input->original_au_per_sec.num /
+                            input->original_au_per_sec.den / UCLOCK_FREQ);
+                else /* file mode */
+                    upipe_set_max_length(input->encaps, UINT_MAX);
+            }
         }
     }
 }