be7a8be727328f434584b64e478933e359b885ef
James Darnley <james.darnley@gmail.com>
2016-11-11 03:17:46 +0000

    add comments to sdienc header noting how many pixels the functions process

--- lib/upipe-hbrmt/sdienc.h
+++ lib/upipe-hbrmt/sdienc.h
@@ -1,14 +1,18 @@
+/* process mmsize/2 samples per iteration */
 void upipe_sdi_pack_10_ssse3(uint8_t *dst, const uint8_t *y, int64_t size);
 void upipe_sdi_pack_10_avx  (uint8_t *dst, const uint8_t *y, int64_t size);
 void upipe_sdi_pack_10_avx2 (uint8_t *dst, const uint8_t *y, int64_t size);
 
+/* process 2*mmsize pixels per iteration */
 void upipe_planar_to_uyvy_8_avx(uint16_t *dst, const uint8_t *y, const uint8_t *u, const uint8_t *v, const int64_t width);
 void upipe_planar_to_uyvy_8_avx2(uint16_t *dst, const uint8_t *y, const uint8_t *u, const uint8_t *v, const int64_t width);
 
+/* process mmsize pixels per iteration */
 void upipe_planar_to_uyvy_10_sse2(uint16_t *dst, const uint16_t *y, const uint16_t *u, const uint16_t *v, const int64_t width);
 void upipe_planar_to_uyvy_10_avx (uint16_t *dst, const uint16_t *y, const uint16_t *u, const uint16_t *v, const int64_t width);
 void upipe_planar_to_uyvy_10_avx2(uint16_t *dst, const uint16_t *y, const uint16_t *u, const uint16_t *v, const int64_t width);
 
+/* process (3*mmsize)/4 pixels per iteration */
 void upipe_v210_uyvy_unpack_aligned_ssse3(const uint32_t *src, uint16_t *uyvy, int64_t width);
 void upipe_v210_uyvy_unpack_aligned_avx  (const uint32_t *src, uint16_t *uyvy, int64_t width);
 void upipe_v210_uyvy_unpack_aligned_avx2 (const uint32_t *src, uint16_t *uyvy, int64_t width);
