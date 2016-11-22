45bb1323090c4e3f328c59d13af8bf5a456f82de
Kieran Kunhya <kierank@obe.tv>
2016-11-22 14:05:06 +0100

    netmap source pipe

---
+++ include/upipe-netmap/upipe_netmap_source.h
@@ -0,0 +1,24 @@
+/** @file
+ * @short Upipe netmap source module
+ */
+
+#ifndef _UPIPE_MODULES_UPIPE_NETMAP_SOURCE_H_
+/** @hidden */
+#define _UPIPE_MODULES_UPIPE_NETMAP_SOURCE_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <upipe/upipe.h>
+
+#define UPIPE_NETMAP_SOURCE_SIGNATURE UBASE_FOURCC('n','t','m','s')
+/** @This returns the management structure for netmap_source pipes.
+ *
+ * @return pointer to manager
+ */
+struct upipe_mgr *upipe_netmap_source_mgr_alloc(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
