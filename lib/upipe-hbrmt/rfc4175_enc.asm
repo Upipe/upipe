0a263b3f68bacacf9570028db8508b62443d72fd
James Darnley <james.darnley@gmail.com>
2016-11-26 11:22:07 +0000

    copy and add v210 to planar8 ssse3, avx, and avx2 assembly functions
    ssse3: 3308
    avx:   3117 ~1.06x
    avx2:  2974 ~1.05x ~1.11x

--- lib/upipe-hbrmt/rfc4175_enc.asm
+++ lib/upipe-hbrmt/rfc4175_enc.asm
@@ -28,6 +28,15 @@ v210_mult:        times 2 dw 64, 4, 64, 4, 64, 4, 64, 4
 v210_luma_shuf:   times 2 db 8, 9, 0, 1, 2, 3,12,13, 4, 5, 6, 7,-1,-1,-1,-1
 v210_chroma_shuf: times 2 db 0, 1, 8, 9, 6, 7,-1,-1, 2, 3, 4, 5,12,13,-1,-1
 
+v210_to_planar8_mask1:  times 16 db 0, 255
+v210_to_planar8_mask2:  times  8 db 0, 0, 255, 0
+v210_to_planar8_shuf_y: times  2 db 2, 5, 7,10,13,15,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+v210_to_planar8_shuf_u: times  2 db 1, 6,11,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+v210_to_planar8_shuf_v: times  2 db 3, 9,14,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+
+; Only 16 bytes
+planar_8_avx2_shuf1:   db 0, 1, 2, 8, 9,10,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+
 SECTION .text
 
 %macro v210_to_planar_10 1
@@ -85,3 +94,60 @@ INIT_XMM avx
 v210_to_planar_10 aligned
 INIT_YMM avx2
 v210_to_planar_10 aligned
+
+%macro v210_to_planar_8 1
+
+cglobal v210_to_planar_8_%1, 5, 5, 7, src, y, u, v, width
+    shr widthq, 1
+    lea    yq, [yq+2*widthq]
+    add    uq, widthq
+    add    vq, widthq
+    neg    widthq
+
+    mova   m3, [v210_mult]
+
+    .loop:
+%ifidn %1, unaligned
+        movu   m0, [srcq]
+%else
+        mova   m0, [srcq]
+%endif
+
+        pmullw m1, m0, m3
+        pslld  m0, 4
+        pand   m1, [v210_to_planar8_mask1] ; __ u0 __ v0  __ y1 __ y2  __ v1 __ u2  __ y4 __ y5
+        pand   m0, [v210_to_planar8_mask2] ; __ __ y0 __  __ __ u1 __  __ __ y3 __  __ __ v2 __
+
+        por m0, m1                         ; __ u0 y0 v0  __ y1 u1 y2  __ v1 y3 u2  __ y4 v2 y5
+
+        pshufb m2, m0, [v210_to_planar8_shuf_y]
+        pshufb m1, m0, [v210_to_planar8_shuf_u]
+        pshufb m0, m0, [v210_to_planar8_shuf_v]
+        movq [yq + 2*widthq], xm2
+
+%if notcpuflag(avx2)
+        movd [uq + widthq],   xm1
+        movd [vq + widthq],   xm0
+%else
+        vextracti128 [yq + 2*widthq + 6], m2, 1
+        vpermq m1, m1, q3120
+        vpermq m0, m0, q3120
+        pshufb xm1, xm1, [planar_8_avx2_shuf1]
+        pshufb xm0, xm0, [planar_8_avx2_shuf1]
+        movq [uq + widthq],   xm1
+        movq [vq + widthq],   xm0
+%endif
+
+        add srcq, mmsize
+        add widthq, (3*mmsize)/16
+    jl  .loop
+RET
+
+%endmacro
+
+INIT_XMM ssse3
+v210_to_planar_8 aligned
+INIT_XMM avx
+v210_to_planar_8 aligned
+INIT_YMM avx2
+v210_to_planar_8 aligned
