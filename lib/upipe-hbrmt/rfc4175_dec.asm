6e5e121eae2d6600b7d4c10d7bb73cb7a0e3abd2
James Darnley <james.darnley@gmail.com>
2016-11-29 15:09:32 +0000

    move sdi to planar8 to rfc4175_dec

--- lib/upipe-hbrmt/rfc4175_dec.asm
+++ lib/upipe-hbrmt/rfc4175_dec.asm
@@ -37,6 +37,16 @@ sdi_v210_rshift_hard_2:   times 2 dw 0x2000, 0x800, 0x7fff, 0x7fff, 0x7fff, 0x7f
 sdi_v210_mask_hard_2:     times 2 db 0xfc, 0x0f, 0xfc, 0x0f, 0xfc, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 sdi_v210_shuf_hard_2_end: times 2 db -1, 0, 1, -1, -1, 2, 3, -1, -1, -1, -1, -1, -1, 4, 5, -1
 
+planar_8_c_shuf:       times 2 db 0, 5,10,-1,-1,-1,-1,-1, 3, 2, 8, 7,13,12,-1,-1
+planar_8_v_shuf_after: times 2 db 9,11,13,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+
+planar_8_y_shuf: times 2 db 2, 1, 4, 3, 7, 6, 9, 8,12,11,14,13,-1,-1,-1,-1
+planar_8_y_mult: times 2 dw 0x4, 0x40, 0x4, 0x40, 0x4, 0x40, 0x0, 0x0
+planar_8_y_shuf_after: times 2 db 1, 3, 5, 7, 9,11,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+
+planar_8_avx2_shuf1:   db 0, 1, 2, 8, 9,10,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+planar_8_avx2_shuf2:   db 1, 3, 5, 9,11,13,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+
 SECTION .text
 
 %macro sdi_v210_unpack 0
@@ -111,3 +121,57 @@ INIT_XMM avx
 sdi_v210_unpack
 INIT_YMM avx2
 sdi_v210_unpack
+
+%macro sdi_to_planar_8 0
+
+; sdi_to_planar_8(uint8_t *src, uint8_t *y, uint8_t *u, uint8_t *v, int64_t size)
+cglobal sdi_to_planar_8, 5, 6, 3, src, y, u, v, size, offset
+    xor      offsetq, offsetq
+    add      srcq, sizeq
+    neg      sizeq
+
+.loop:
+    movu     xm0, [srcq+sizeq]
+%if cpuflag(avx2)
+    vinserti128 m0, m0, [srcq+sizeq+15], 1
+%endif
+
+    pshufb   m1, m0, [planar_8_c_shuf]
+
+    pshufb   m0, [planar_8_y_shuf]
+    pmullw   m0, [planar_8_y_mult]
+    pshufb   m0, [planar_8_y_shuf_after]
+    movq     [yq + 2*offsetq], xm0
+%if cpuflag(avx2)
+    vextracti128 [yq + 2*offsetq + 6], m0, 1
+%endif
+
+%if notcpuflag(avx2)
+    movd     [uq + offsetq], m1
+    psllw    m2, m1, 4
+    pshufb   m2, [planar_8_v_shuf_after]
+    movd     [vq + offsetq], m2
+%else
+    vpermq m1, m1, q3120
+    vextracti128 xm2, m1, 1
+    pshufb xm1, [planar_8_avx2_shuf1]
+    movq [uq + offsetq], xm1
+
+    psllw xm2, 4
+    pshufb xm2, [planar_8_avx2_shuf2]
+    movq [vq + offsetq], xm2
+%endif
+
+    add      offsetq, (3*mmsize)/16
+    add      sizeq, (15*mmsize)/16
+    jl .loop
+
+    RET
+%endmacro
+
+INIT_XMM ssse3
+sdi_to_planar_8
+INIT_XMM avx
+sdi_to_planar_8
+INIT_YMM avx2
+sdi_to_planar_8
