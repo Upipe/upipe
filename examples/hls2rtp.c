Upipe/upipe

commit 573d9efb10a8f007d7be3cd1a882a781f05c0484

probe_uref: add upump_p argument to probe
Please note that this shifts the drop_p argument, and consequently applications
may need to be adapted.

examples/hls2rtp.c
@@ -399,9 +399,7 @@ static int catch_rewrite_date(struct uprobe *uprobe, struct upipe *upipe,
 
     UBASE_SIGNATURE_CHECK(args, UPIPE_PROBE_UREF_SIGNATURE);
     struct uref *uref = va_arg(args, struct uref *);
-    bool *drop = va_arg(args, bool *);
 
-    *drop = false;
     int type;
     uint64_t date;
     uref_clock_get_date_orig(uref, &date, &type);

include/upipe-modules/upipe_probe_uref.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 OpenHeadend S.A.R.L.
+ * Copyright (C) 2012-2017 OpenHeadend S.A.R.L.
  *
  * Authors: Benjamin Cohen
  *
@@ -45,7 +45,7 @@ extern "C" {
 enum uprobe_probe_uref_event {
     UPROBE_PROBE_SENTINEL = UPROBE_LOCAL,
 
-    /** received uref event (struct uref *uref, bool *drop) */
+    /** received uref event (struct uref *, struct upump **, bool *drop) */
     UPROBE_PROBE_UREF
 };
 

lib/upipe-hls/upipe_hls_playlist.c
@@ -214,6 +214,7 @@ static int probe_key(struct uprobe *uprobe, struct upipe *inner,
     case UPROBE_PROBE_UREF: {
         UBASE_SIGNATURE_CHECK(args, UPIPE_PROBE_UREF_SIGNATURE);
         struct uref *uref = va_arg(args, struct uref *);
+        struct upump **upump_p = va_arg(args, struct upump **);
         bool *drop = va_arg(args, bool *);
         *drop = true;
 

lib/upipe-modules/upipe_probe_uref.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2014 OpenHeadend S.A.R.L.
+ * Copyright (C) 2012-2017 OpenHeadend S.A.R.L.
  *
  * Authors: Benjamin Cohen
  *
@@ -84,7 +84,8 @@ static void upipe_probe_uref_input(struct upipe *upipe, struct uref *uref,
                                    struct upump **upump_p)
 {
     bool drop = false;
-    upipe_throw(upipe, UPROBE_PROBE_UREF, UPIPE_PROBE_UREF_SIGNATURE, uref, &drop);
+    upipe_throw(upipe, UPROBE_PROBE_UREF, UPIPE_PROBE_UREF_SIGNATURE, uref,
+                upump_p, &drop);
     if (drop) {
         uref_free(uref);
     } else {

commit ad84c2103a1fc82a1271ded0156ad67c1b0a7884

hls2rtp: fix segfault
Introduced in 573d9efb10a8f007d7be3cd1a882a781f05c0484

examples/hls2rtp.c
@@ -472,6 +472,7 @@ static int catch_seek(struct uprobe *uprobe, struct upipe *upipe,
 
     UBASE_SIGNATURE_CHECK(args, UPIPE_PROBE_UREF_SIGNATURE);
     struct uref *uref = va_arg(args, struct uref *);
+    va_arg(args, struct upump **);
     bool *drop = va_arg(args, bool *);
     *drop = false;
 