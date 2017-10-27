10f01f726b4c7640345f4ec888136a2609508bd9
Rafaël Carré <funman@videolan.org>
2017-10-27 12:27:29 +0200

    Apple clang now knows about builtin_sse("ssse3")

--- lib/upipe-hbrmt/upipe_unpack10bit.c
+++ lib/upipe-hbrmt/upipe_unpack10bit.c
@@ -351,22 +351,11 @@ static struct upipe *upipe_unpack10bit_alloc(struct upipe_mgr *mgr,
     upipe_unpack10bit->unpack = upipe_sdi_unpack_c;
 #if defined(HAVE_X86_ASM)
 #if defined(__i686__) || defined(__x86_64__)
-#if !defined(__APPLE__) /* macOS clang doesn't support that builtin yet */
-#if defined(__clang__) && /* clang 3.8 doesn't know ssse3 */ \
-     (__clang_major__ < 3 || (__clang_major__ == 3 && __clang_minor__ <= 8))
-# ifdef __SSSE3__
-        if (1)
-# else
-        if (0)
-# endif
-#else
-        if (__builtin_cpu_supports("ssse3"))
-#endif
-			upipe_unpack10bit->unpack = upipe_sdi_unpack_10_ssse3;
+    if (__builtin_cpu_supports("ssse3"))
+        upipe_unpack10bit->unpack = upipe_sdi_unpack_10_ssse3;
 
     if (__builtin_cpu_supports("avx2"))
         upipe_unpack10bit->unpack = upipe_sdi_unpack_10_avx2;
-#endif
 #endif
 #endif
 
