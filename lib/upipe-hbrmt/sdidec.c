4ce4f25ca9ad44f850a6bc83352c36a11bdaf88a
James Darnley <jdarnley@obe.tv>
2017-11-28 12:15:16 +0100

    upipe-hbrmt: fix sdi_to_v210 C function

--- lib/upipe-hbrmt/sdidec.c
+++ lib/upipe-hbrmt/sdidec.c
@@ -19,7 +19,7 @@ void upipe_sdi_unpack_c(const uint8_t *src, uint16_t *y, uintptr_t pixels)
 
 void upipe_sdi_v210_unpack_c(const uint8_t *src, uint32_t *dst, uintptr_t pixels)
 {
-    for (int i = 0; i < pixels; i += 3 * 4) {
+    for (int i = 0; i < pixels - 5; i += 6) {
         uint16_t a, b, c;
 
         a = ((src[0]  & 0xff) << 2) | (src[1] >> 6);
@@ -39,8 +39,9 @@ void upipe_sdi_v210_unpack_c(const uint8_t *src, uint32_t *dst, uintptr_t pixels
 
         a = ((src[11] & 0x3f) << 4) | (src[12] >> 4);
         b = ((src[12] & 0x0f) << 6) | (src[13] >> 2);
-        c = ((src[14] & 0x03) << 8) | (src[15]);
+        c = ((src[13] & 0x03) << 8) | (src[14]);
         *dst++ = (((c << 20) | (b << 10)) | a);
+        src += 15;
     }
 }
 
