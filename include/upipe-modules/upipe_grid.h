133777536f0f3a922635225d0509b3ccbfe4bd0b
Arnaud de Turckheim <quarium@gmail.com>
2018-02-27 15:55:57 +0100

    upipe_grid: simplify sound extraction
    Audio grid output duplicates the entire buffer so the input sound buffer
    size must be set accordingly.

--- include/upipe-modules/upipe_grid.h
+++ include/upipe-modules/upipe_grid.h
@@ -33,7 +33,11 @@ extern "C" {
 #endif
 
 /** @This is the grid pipe signature. */
-#define UPIPE_GRID_SIGNATURE   UBASE_FOURCC('g','r','i','d')
+#define UPIPE_GRID_SIGNATURE        UBASE_FOURCC('g','r','i','d')
+/** @This is the grid input pipe signature. */
+#define UPIPE_GRID_IN_SIGNATURE     UBASE_FOURCC('g','r','d','i')
+/** @This is the grid output pipe signature. */
+#define UPIPE_GRID_OUT_SIGNATURE    UBASE_FOURCC('g','r','d','o')
 
 /** @This returns grid pipe manager.
  *
@@ -50,8 +54,18 @@ struct upipe_mgr *upipe_grid_mgr_alloc(void);
 struct upipe *upipe_grid_alloc_input(struct upipe *upipe,
                                      struct uprobe *uprobe);
 
-int upipe_grid_out_get_input(struct upipe *upipe, struct upipe **input_p);
-int upipe_grid_out_set_input(struct upipe *upipe, struct upipe *input);
+/** @This enumerates the grid output control commands. */
+enum upipe_grid_out_command {
+    /** sentinel */
+    UPIPE_GRID_OUT_SENTINEL = UPIPE_CONTROL_LOCAL,
+
+    /** get the grid output input pipe (struct upipe **) */
+    UPIPE_GRID_OUT_GET_INPUT,
+    /** set the grid output input pipe (struct upipe *) */
+    UPIPE_GRID_OUT_SET_INPUT,
+    /** iterate the grid input of a grid output (struct upipe **) */
+    UPIPE_GRID_OUT_ITERATE_INPUT,
+};
 
 /** @This allocates a new grid output.
  *
@@ -61,6 +75,45 @@ int upipe_grid_out_set_input(struct upipe *upipe, struct upipe *input);
  */
 struct upipe *upipe_grid_alloc_output(struct upipe *upipe,
                                       struct uprobe *uprobe);
+
+/** @This sets the input of a grid output pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @param input description of the input pipe to set
+ * @return an error code
+ */
+static inline int upipe_grid_out_set_input(struct upipe *upipe,
+                                           struct upipe *input)
+{
+    return upipe_control(upipe, UPIPE_GRID_OUT_SET_INPUT,
+                         UPIPE_GRID_OUT_SIGNATURE, input);
+}
+
+/** @This gets the current input of a grid output pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @param input_p filled with the input pipe
+ * @return an error code
+ */
+static inline int upipe_grid_out_get_input(struct upipe *upipe,
+                                           struct upipe **input_p)
+{
+    return upipe_control(upipe, UPIPE_GRID_OUT_GET_INPUT,
+                         UPIPE_GRID_OUT_SIGNATURE, input_p);
+}
+
+/** @This iterates the inputs of a grid output pipe.
+ *
+ * @param upipe description structure of the output pipe
+ * @param input_p filled with the next input pipe
+ * @return an error code
+ */
+static inline int upipe_grid_out_iterate_input(struct upipe *upipe,
+                                               struct upipe **input_p)
+{
+    return upipe_control(upipe, UPIPE_GRID_OUT_ITERATE_INPUT,
+                         UPIPE_GRID_OUT_SIGNATURE, input_p);
+}
 
 #ifdef __cplusplus
 }
