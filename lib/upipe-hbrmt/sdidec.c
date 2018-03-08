0f2c441273c03801db4571af39e16213c188c497
James Darnley <jdarnley@obe.tv>
2018-03-08 12:34:34 +0100

    upipe_sdi_to_planar_10_c

--- lib/upipe-hbrmt/sdidec.c
+++ lib/upipe-hbrmt/sdidec.c
@@ -82,3 +82,18 @@ void upipe_sdi_to_planar_8_c(const uint8_t *src, uint8_t *y, uint8_t *u, uint8_t
         *y++ = (d << 6) | (e >> 2);
     }
 }
+
+void upipe_sdi_to_planar_10_c(const uint8_t *src, uint16_t *y, uint16_t *u, uint16_t *v, uintptr_t pixels)
+{
+    for (int i = 0; i < 2*pixels; i += 2) {
+        uint8_t a = *src++;
+        uint8_t b = *src++;
+        uint8_t c = *src++;
+        uint8_t d = *src++;
+        uint8_t e = *src++;
+        *u++ = (a << 2)          | ((b >> 6) & 0x03); //1111111122
+        *y++ = ((b & 0x3f) << 4) | ((c >> 4) & 0x0f); //2222223333
+        *v++ = ((c & 0x0f) << 6) | ((d >> 2) & 0x3f); //3333444444
+        *y++ = ((d & 0x03) << 8) | e;                 //4455555555
+    }
+}
