da22df627c055dd816214099cbea86bedc5fb008
Arnaud de Turckheim <quarium@gmail.com>
2018-04-18 17:09:58 +0200

    upipe_v210: fix compilation without asm

--- tests/checkasm/v210dec.c
+++ tests/checkasm/v210dec.c
@@ -81,7 +81,7 @@ void checkasm_check_v210dec(void)
 
     int cpu_flags = av_get_cpu_flags();
 
-#if HAVE_X86ASM
+#ifdef HAVE_X86ASM
     if (cpu_flags & AV_CPU_FLAG_SSSE3) {
         s.planar_10 = upipe_v210_to_planar_10_aligned_ssse3;
         s.planar_8  = upipe_v210_to_planar_8_aligned_ssse3;
--- tests/checkasm/v210enc.c
+++ tests/checkasm/v210enc.c
@@ -89,7 +89,7 @@ void checkasm_check_v210enc(void)
 
     int cpu_flags = av_get_cpu_flags();
 
-#if HAVE_X86ASM
+#ifdef HAVE_X86ASM
     if (cpu_flags & AV_CPU_FLAG_SSSE3) {
         s.planar_10 = upipe_planar_to_v210_10_ssse3;
         s.planar_8  = upipe_planar_to_v210_8_ssse3;
