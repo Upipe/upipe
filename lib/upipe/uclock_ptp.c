c92f1c597eb5178d60f77836c2d68a592edab5d5
Rafaël Carré <funman@videolan.org>
2018-06-07 12:30:25 +0200

    uclock_ptp: get clock from NIC device

---
+++ lib/upipe/uclock_ptp.c
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2018 Open Broadcast Systems Ltd
+ *
+ * Author: Rafaël Carré
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** @file
+ * @short Upipe NIC PTP implementation of uclock
+ */
+
+#include <upipe/ubase.h>
+#include <upipe/urefcount.h>
+#include <upipe/uclock.h>
+#include <upipe/uclock_ptp.h>
+
+#include <sys/ioctl.h>
+#include <linux/ethtool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <net/if.h>
+#include <linux/sockios.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/** super-set of the uclock structure with additional local members */
+struct uclock_ptp {
+    /** refcount management structure */
+    struct urefcount urefcount;
+
+    /** clock device fd */
+    int fd;
+
+    /** structure exported to modules */
+    struct uclock uclock;
+};
+
+UBASE_FROM_TO(uclock_ptp, uclock, uclock, uclock)
+UBASE_FROM_TO(uclock_ptp, urefcount, urefcount, urefcount)
+
+/** @This returns the current time in the given clock.
+ *
+ * @param uclock utility structure passed to the module
+ * @return current system time in 27 MHz ticks
+ */
+static uint64_t uclock_ptp_now_inner(struct uclock *uclock)
+{
+    struct uclock_ptp *ptp = uclock_ptp_from_uclock(uclock);
+
+#define CLOCKFD 3
+#define FD_TO_CLOCKID(fd) ((~(clockid_t) (fd) << 3) | CLOCKFD)
+
+    struct timespec ts;
+    if (unlikely(clock_gettime(FD_TO_CLOCKID(ptp->fd), &ts) == -1))
+        return UINT64_MAX;
+
+    uint64_t now = ts.tv_sec * UCLOCK_FREQ +
+                   ts.tv_nsec * UCLOCK_FREQ / UINT64_C(1000000000);
+    return now;
+}
+
+/** @This returns the current system time.
+ *
+ * @param uclock utility structure passed to the module
+ * @return current system time in 27 MHz ticks
+ */
+static uint64_t uclock_ptp_now(struct uclock *uclock)
+{
+    struct uclock_ptp *ptp = uclock_ptp_from_uclock(uclock);
+    return uclock_ptp_now_inner(uclock);
+}
+
+/** @This frees a uclock.
+ *
+ * @param urefcount pointer to urefcount
+ */
+static void uclock_ptp_free(struct urefcount *urefcount)
+{
+    struct uclock_ptp *ptp = uclock_ptp_from_urefcount(urefcount);
+    urefcount_clean(urefcount);
+    close(ptp->fd);
+    free(ptp);
+}
+
+/** @internal */
+static int uclock_ptp_nic_clock_idx(struct uprobe *uprobe, const char *interface)
+{
+    struct ethtool_ts_info info;
+    struct ifreq ifr;
+
+    memset(&ifr, 0, sizeof(ifr));
+    memset(&info, 0, sizeof(info));
+    info.cmd = ETHTOOL_GET_TS_INFO;
+    strncpy(ifr.ifr_name, interface, IFNAMSIZ - 1);
+    ifr.ifr_data = (char *) &info;
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (fd < 0) {
+        uprobe_err_va(uprobe, NULL, "Couldn't open socket for %s: %m", interface);
+        return -1;
+    }
+
+    if (ioctl(fd, SIOCETHTOOL, &ifr) < 0) {
+        uprobe_err_va(uprobe, NULL, "Couldn't get ethtool ts information for %s: %m",
+            interface);
+        info.phc_index = -1;
+    }
+
+    close(fd);
+
+    return info.phc_index;
+}
+
+/** @internal */
+static int uclock_ptp_open_nic(struct uprobe *uprobe, const char *interface)
+{
+    int idx = uclock_ptp_nic_clock_idx(uprobe, interface);
+
+    if (idx < 0) {
+        uprobe_err_va(uprobe, NULL, "No PTP device found for %s", interface);
+        return -1;
+    }
+
+    char clkdev[32];
+    snprintf(clkdev, sizeof(clkdev), "/dev/ptp%u", idx);
+
+    int fd = open(clkdev, O_RDWR);
+    if (fd < 0)
+        uprobe_err_va(uprobe, NULL, "Could not open PTP device %s: %m", clkdev);
+
+    return fd;
+}
+
+/** @This allocates a new uclock structure.
+ *
+ * @param uprobe probe catching log events for error reporting
+ * @param interface NIC name
+ * @return pointer to uclock, or NULL in case of error
+ */
+struct uclock *uclock_ptp_alloc(struct uprobe *uprobe, const char *interface)
+{
+    struct uclock_ptp *ptp = malloc(sizeof(struct uclock_ptp));
+    if (unlikely(ptp == NULL))
+        return NULL;
+
+    ptp->fd = uclock_ptp_open_nic(uprobe, interface);
+    if (ptp->fd < 0) {
+        free(ptp);
+        return NULL;
+    }
+
+    urefcount_init(uclock_ptp_to_urefcount(ptp), uclock_ptp_free);
+    ptp->uclock.refcount = uclock_ptp_to_urefcount(ptp);
+    ptp->uclock.uclock_now = uclock_ptp_now;
+    ptp->uclock.uclock_to_real = NULL;
+    ptp->uclock.uclock_from_real = NULL;
+    return uclock_ptp_to_uclock(ptp);
+}
