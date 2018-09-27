5fc38ebdfe37906e6ab4748abe3c20f49cf79a33
James Darnley <jdarnley@obe.tv>
2018-09-27 13:52:09 +0200

    upipe-netmap: remove duplicated and unused assembly functions

--- lib/upipe-netmap/v210.asm
+++ lib/upipe-netmap/v210.asm
@@ -24,25 +24,6 @@
 
 SECTION_RODATA
 
-v210_mask: times 4 dd 0x3ff
-v210_mult: dw 64,4,64,4,64,4,64,4
-v210_luma_shuf: db 8,9,0,1,2,3,12,13,4,5,6,7,-1,-1,-1,-1
-v210_chroma_shuf: db 0,1,8,9,6,7,-1,-1,2,3,4,5,12,13,-1,-1
-
-v210_uyvy_mask1: times 2 db 0xff, 0x03, 0xf0, 0x3f, 0x00, 0xfc, 0x0f, 0xc0
-v210_uyvy_mask2: times 2 db 0xf0, 0x3f, 0x00, 0xfc, 0x0f, 0xc0, 0xff, 0x03
-
-v210_uyvy_chroma_shuf1: db  0, 1,-1,-1, 2, 3,-1,-1, 5, 6,-1,-1, 8, 9,-1,-1
-v210_uyvy_luma_shuf1:   db -1,-1, 1, 2,-1,-1, 4, 5,-1,-1, 6, 7,-1,-1, 9,10
-v210_uyvy_chroma_shuf2: db  0, 1,-1,-1, 3, 4,-1,-1, 6, 7,-1,-1, 8, 9,-1,-1
-v210_uyvy_luma_shuf2:   db -1,-1, 2, 3,-1,-1, 4, 5,-1,-1, 7, 8,-1,-1,10,11
-v210_uyvy_chroma_shuf3: db  5, 6,-1,-1, 8, 9,-1,-1,10,11,-1,-1,13,14,-1,-1
-v210_uyvy_luma_shuf3:   db -1,-1, 6, 7,-1,-1, 9,10,-1,-1,12,13,-1,-1,14,15
-
-v210_uyvy_mult1: dw 0x7fff, 0x2000, 0x0800, 0x7fff, 0x2000, 0x0800, 0x7fff, 0x2000
-v210_uyvy_mult2: dw 0x0800, 0x7fff, 0x2000, 0x0800, 0x7fff, 0x2000, 0x0800, 0x7fff
-v210_uyvy_mult3: dw 0x2000, 0x0800, 0x7fff, 0x2000, 0x0800, 0x7fff, 0x2000, 0x0800
-
 v210_sdi_shuf_easy:       db -1, -1, -1, 5, 4, -1, 7, 6, -1, -1, 11, 10, 14, 13, -1, -1
 v210_sdi_mult_easy:       dw 1, 1, 1, 1, 1, 4, 1, 1
 v210_sdi_mask_easy:       db 0x00, 0x00, 0x00, 0x03, 0xff, 0x00, 0x3f, 0xf0, 0x00, 0x00, 0xff, 0xc0, 0x0f, 0xfc, 0x00, 0x00
@@ -58,60 +39,6 @@ v210_sdi_mask_hard_2:     db 0xff, 0xc0, 0xfc, 0x0f, 0xff, 0x03, 0xff, 0x03, 0x0
 v210_sdi_shuf_hard_end_2: db 0, 1, 3, 2, -1, -1, -1, -1, 5, 4, -1, -1, -1, 7, 6, -1
 
 SECTION .text
-
-%macro v210_planar_unpack 1
-
-; v210_planar_unpack(const uint32_t *src, uint16_t *y, uint16_t *u, uint16_t *v, int width)
-cglobal v210_planar_unpack_%1, 5, 5, 7
-    movsxdifnidn r4, r4d
-    lea    r1, [r1+2*r4]
-    add    r2, r4
-    add    r3, r4
-    neg    r4
-
-    mova   m3, [v210_mult]
-    mova   m4, [v210_mask]
-    mova   m5, [v210_luma_shuf]
-    mova   m6, [v210_chroma_shuf]
-.loop:
-%ifidn %1, unaligned
-    movu   m0, [r0]
-%else
-    mova   m0, [r0]
-%endif
-
-    pmullw m1, m0, m3
-    psrld  m0, 10
-    psrlw  m1, 6  ; u0 v0 y1 y2 v1 u2 y4 y5
-    pand   m0, m4 ; y0 __ u1 __ y3 __ v2 __
-
-    shufps m2, m1, m0, 0x8d ; y1 y2 y4 y5 y0 __ y3 __
-    pshufb m2, m5 ; y0 y1 y2 y3 y4 y5 __ __
-    movu   [r1+2*r4], m2
-
-    shufps m1, m0, 0xd8 ; u0 v0 v1 u2 u1 __ v2 __
-    pshufb m1, m6 ; u0 u1 u2 __ v0 v1 v2 __
-    movq   [r2+r4], m1
-    movhps [r3+r4], m1
-
-    add r0, mmsize
-    add r4, 6
-    jl  .loop
-
-    REP_RET
-%endmacro
-
-INIT_XMM ssse3
-v210_planar_unpack unaligned
-
-INIT_XMM avx
-v210_planar_unpack unaligned
-
-INIT_XMM ssse3
-v210_planar_unpack aligned
-
-INIT_XMM avx
-v210_planar_unpack aligned
 
 %macro v210_sdi_unpack_aligned 0
 
