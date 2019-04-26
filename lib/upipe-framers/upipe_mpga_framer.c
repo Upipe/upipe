Upipe/upipe

commit 026ef0879b537c1f98daeb3a7c6d9509ce9cb1c5

mpgaf: update octetrate when increasing

lib/upipe-framers/upipe_mpga_framer.c
@@ -493,19 +493,41 @@ static bool upipe_mpgaf_parse_adts(struct upipe *upipe)
                      ADTS_HEADER_SIZE, header)))
         return true; /* not enough data */
 
-    if (likely(adts_sync_compare_formats(header, upipe_mpgaf->sync_header))) {
-        /* identical sync */
-        goto upipe_mpgaf_parse_adts_shortcut;
-    }
+    uint8_t sampling_freq = adts_get_sampling_freq(header);
+    uint64_t samplerate = aac_samplerate_table[sampling_freq];
+    size_t samples = ADTS_SAMPLES_PER_BLOCK * (1 + adts_get_num_blocks(header));
+    uint16_t adts_length = adts_get_length(header);
+    uint8_t adts_profile = adts_get_profile(header);
+    uint8_t asc_aot = adts_profile +1;
 
-    if (!aac_samplerate_table[adts_get_sampling_freq(header)]) {
+    if (!samplerate) {
         upipe_warn(upipe, "invalid samplerate");
         return false;
     }
 
-    if (adts_get_length(header) < ADTS_HEADER_SIZE) {
+    if (adts_length < ADTS_HEADER_SIZE) {
         upipe_warn(upipe, "invalid header");
         return false;
+    }
+
+    if (samplerate <= 24000) {
+        /* assume SBR on low frequency streams */
+        samplerate *= 2;
+        samples *= 2;
+        asc_aot = ASC_TYPE_SBR;
+    }
+
+    /* Calculate octetrate assuming the stream is CBR. */
+    uint64_t octetrate = adts_length * samplerate / samples;
+    /* Round up to a multiple of 8 kbits/s. */
+    octetrate += 999;
+    octetrate -= octetrate % 1000;
+
+    if (likely(adts_sync_compare_formats(header, upipe_mpgaf->sync_header) &&
+        samples == upipe_mpgaf->samples &&
+        octetrate <= upipe_mpgaf->octetrate)) {
+        /* identical sync */
+        goto upipe_mpgaf_parse_adts_shortcut;
     }
 
     memcpy(upipe_mpgaf->sync_header, header, ADTS_HEADER_SIZE);
@@ -518,21 +540,12 @@ static bool upipe_mpgaf_parse_adts(struct upipe *upipe)
     UBASE_FATAL(upipe, uref_flow_set_complete(flow_def))
 
     upipe_mpgaf->has_crc = !adts_get_protection_absent(header);
-    uint8_t adts_profile = adts_get_profile(header);
-    upipe_mpgaf->asc_aot = upipe_mpgaf->asc_base_aot = adts_profile + 1;
+    upipe_mpgaf->asc_aot = upipe_mpgaf->asc_base_aot = asc_aot;
     upipe_mpgaf->samplerate_idx = upipe_mpgaf->base_samplerate_idx =
-        adts_get_sampling_freq(header);
-    upipe_mpgaf->samplerate = upipe_mpgaf->base_samplerate =
-        aac_samplerate_table[upipe_mpgaf->samplerate_idx];
-    upipe_mpgaf->samples = ADTS_SAMPLES_PER_BLOCK *
-                           (1 + adts_get_num_blocks(header));
+        sampling_freq;
+    upipe_mpgaf->samplerate = upipe_mpgaf->base_samplerate = samplerate;
+    upipe_mpgaf->samples = samples;
     upipe_mpgaf->channels = adts_get_channels(header);
-    if (upipe_mpgaf->samplerate <= 24000) {
-        /* assume SBR on low frequency streams */
-        upipe_mpgaf->samplerate *= 2;
-        upipe_mpgaf->samples *= 2;
-        upipe_mpgaf->asc_aot = ASC_TYPE_SBR;
-    }
     if (upipe_mpgaf->channels == 7)
         upipe_mpgaf->channels = 8;
     upipe_mpgaf->asc_frame_length = false;
@@ -558,12 +571,6 @@ static bool upipe_mpgaf_parse_adts(struct upipe *upipe)
     if (adts_get_home(header))
         UBASE_FATAL(upipe, uref_flow_set_original(flow_def))
 
-    /* Calculate octetrate assuming the stream is CBR. */
-    uint64_t octetrate = adts_get_length(header) *
-                         upipe_mpgaf->samplerate / upipe_mpgaf->samples;
-    /* Round up to a multiple of 8 kbits/s. */
-    octetrate += 999;
-    octetrate -= octetrate % 1000;
     upipe_mpgaf->octetrate = octetrate;
     UBASE_FATAL(upipe, uref_block_flow_set_octetrate(flow_def, octetrate))
 
@@ -575,7 +582,7 @@ static bool upipe_mpgaf_parse_adts(struct upipe *upipe)
         upipe_mpgaf_require_flow_format(upipe, flow_def);
 
 upipe_mpgaf_parse_adts_shortcut:
-    upipe_mpgaf->next_frame_size = adts_get_length(header);
+    upipe_mpgaf->next_frame_size = adts_length;
     return true;
 }
 
@@ -1674,7 +1681,11 @@ static bool upipe_mpgaf_work_raw(struct upipe *upipe, struct uref *uref,
         !upipe_mpgaf_handle_latm(upipe, uref->ubuf, 0, size))
         return true;
 
-    if (unlikely(!upipe_mpgaf->acquired)) {
+    /* Calculate octetrate assuming the stream is CBR. */
+    uint64_t octetrate = size * upipe_mpgaf->samplerate /
+        upipe_mpgaf->samples;
+
+    if (unlikely(!upipe_mpgaf->acquired || octetrate > upipe_mpgaf->octetrate)) {
         upipe_mpgaf_sync_acquired(upipe);
         struct uref *flow_def = upipe_mpgaf_alloc_flow_def_attr(upipe);
         if (unlikely(flow_def == NULL)) {
@@ -1693,9 +1704,6 @@ static bool upipe_mpgaf_work_raw(struct upipe *upipe, struct uref *uref,
         UBASE_FATAL(upipe,
                 uref_sound_flow_set_samples(flow_def, upipe_mpgaf->samples))
 
-        /* Calculate octetrate assuming the stream is CBR. */
-        uint64_t octetrate = size * upipe_mpgaf->samplerate /
-                             upipe_mpgaf->samples;
         /* Round up to a multiple of 8 kbits/s. */
         octetrate += 999;
         octetrate -= octetrate % 1000;

commit f16a577aa09eb2303c3b367f33d9552ff33de97a

upipe_mpga_framer: fix ADTS asc_base_aot value
Broken by 026ef0879b537c1f98daeb3a7c6d9509ce9cb1c5

lib/upipe-framers/upipe_mpga_framer.c
@@ -499,6 +499,7 @@ static bool upipe_mpgaf_parse_adts(struct upipe *upipe)
     uint16_t adts_length = adts_get_length(header);
     uint8_t adts_profile = adts_get_profile(header);
     uint8_t asc_aot = adts_profile +1;
+    uint8_t asc_base_aot = asc_aot;
 
     if (!samplerate) {
         upipe_warn(upipe, "invalid samplerate");
@@ -540,7 +541,8 @@ static bool upipe_mpgaf_parse_adts(struct upipe *upipe)
     UBASE_FATAL(upipe, uref_flow_set_complete(flow_def))
 
     upipe_mpgaf->has_crc = !adts_get_protection_absent(header);
-    upipe_mpgaf->asc_aot = upipe_mpgaf->asc_base_aot = asc_aot;
+    upipe_mpgaf->asc_aot = asc_aot;
+    upipe_mpgaf->asc_base_aot = asc_base_aot;
     upipe_mpgaf->samplerate_idx = upipe_mpgaf->base_samplerate_idx =
         sampling_freq;
     upipe_mpgaf->samplerate = upipe_mpgaf->base_samplerate = samplerate;