e2ea83a9860143caca57b67832c57dba642c8873
Kieran Kunhya <kierank@obe.tv>
2019-07-25 17:35:05 +0100

    Add base of scte104 generator

---
+++ include/upipe-ts/upipe_ts_scte104_generator.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2019 Open Broadcast Systems Ltd
+ *
+ * Authors: Kieran Kunhya
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ */
+
+/** @file
+ * @short Upipe module performing SCTE-35 to SCTE-104 conversion
+ */
+
+#ifndef _UPIPE_MODULES_UPIPE_TS_SCTE104_GENERATOR_H_
+/** @hidden */
+#define _UPIPE_MODULES_UPIPE_TS_SCTE104_GENERATOR_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <upipe/upipe.h>
+
+#define UPIPE_TS_SCTE104_GENERATOR_SIGNATURE UBASE_FOURCC('t','s','c','1')
+
+/** @This returns the management structure for s337e pipes.
+ *
+ * @return pointer to manager
+ */
+struct upipe_mgr *upipe_ts_scte104_generator_mgr_alloc(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ lib/upipe-ts/upipe_ts_scte104_generator.c
@@ -0,0 +1,282 @@
+/*
+ * Copyright (C) 2019 Open Broadcast Systems Ltd
+ *
+ * Authors: Kieran Kunhya
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ */
+
+/** @file
+ * @short Upipe module performing SCTE-35 to SCTE-104 conversion
+ */
+
+#include <upipe/ubase.h>
+#include <upipe/uprobe.h>
+#include <upipe/uref.h>
+#include <upipe/ubuf.h>
+#include <upipe/ubuf_block.h>
+#include <upipe/upipe.h>
+#include <upipe/uref_block.h>
+#include <upipe/uref_block_flow.h>
+#include <upipe/uref_sound_flow.h>
+#include <upipe/uref_flow.h>
+#include <upipe/upipe.h>
+#include <upipe/upipe_helper_upipe.h>
+#include <upipe/upipe_helper_urefcount.h>
+#include <upipe/upipe_helper_void.h>
+#include <upipe/upipe_helper_output.h>
+#include <upipe/upipe_helper_ubuf_mgr.h>
+#include <upipe/upipe_helper_input.h>
+#include <upipe/ubuf.h>
+#include <upipe/ubuf_sound.h>
+#include <upipe/ubuf_sound_mem.h>
+#include <upipe-ts/upipe_ts_scte104_generator.h>
+
+
+#define EXPECTED_FLOW_DEF "void.scte35."
+
+/** upipe_ts_scte104_generator structure */
+struct upipe_ts_scte104_generator {
+    /** refcount management structure */
+    struct urefcount urefcount;
+
+    /** output pipe */
+    struct upipe *output;
+    /** flow_definition packet */
+    struct uref *flow_def;
+    /** output state */
+    enum upipe_helper_output_state output_state;
+    /** list of output requests */
+    struct uchain request_list;
+
+    /** ubuf manager */
+    struct ubuf_mgr *ubuf_mgr;
+    /** flow format packet */
+    struct uref *flow_format;
+    /** ubuf manager request */
+    struct urequest ubuf_mgr_request;
+
+    /** temporary uref storage (used during urequest) */
+    struct uchain urefs;
+    /** nb urefs in storage */
+    unsigned int nb_urefs;
+    /** max urefs in storage */
+    unsigned int max_urefs;
+    /** list of blockers (used during urequest) */
+    struct uchain blockers;
+
+    /** public upipe structure */
+    struct upipe upipe;
+};
+
+/** @hidden */
+static int upipe_ts_scte104_generator_check(struct upipe *upipe, struct uref *flow_format);
+
+/** @hidden */
+static bool upipe_ts_scte104_generator_handle(struct upipe *upipe, struct uref *uref,
+                                struct upump **upump_p);
+
+UPIPE_HELPER_UPIPE(upipe_ts_scte104_generator, upipe, UPIPE_TS_SCTE104_GENERATOR_SIGNATURE);
+UPIPE_HELPER_UREFCOUNT(upipe_ts_scte104_generator, urefcount, upipe_ts_scte104_generator_free)
+UPIPE_HELPER_VOID(upipe_ts_scte104_generator);
+UPIPE_HELPER_OUTPUT(upipe_ts_scte104_generator, output, flow_def, output_state, request_list);
+UPIPE_HELPER_INPUT(upipe_ts_scte104_generator, urefs, nb_urefs, max_urefs, blockers, upipe_ts_scte104_generator_handle)
+UPIPE_HELPER_UBUF_MGR(upipe_ts_scte104_generator, ubuf_mgr, flow_format, ubuf_mgr_request,
+                      upipe_ts_scte104_generator_check,
+                      upipe_ts_scte104_generator_register_output_request,
+                      upipe_ts_scte104_generator_unregister_output_request)
+
+static int upipe_ts_scte104_generator_check(struct upipe *upipe, struct uref *flow_format)
+{
+    if (flow_format != NULL)
+        upipe_ts_scte104_generator_store_flow_def(upipe, flow_format);
+
+    bool was_buffered = !upipe_ts_scte104_generator_check_input(upipe);
+    upipe_ts_scte104_generator_output_input(upipe);
+    upipe_ts_scte104_generator_unblock_input(upipe);
+    if (was_buffered && upipe_ts_scte104_generator_check_input(upipe)) {
+        /* All packets have been output, release again the pipe that has been
+         * used in @ref upipe_ts_scte104_generator_input. */
+        upipe_release(upipe);
+    }
+
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This inputs data.
+ *
+ * @param upipe description structure of the pipe
+ * @param uref uref structure
+ * @param upump_p reference to pump that generated the buffer
+ */
+static void upipe_ts_scte104_generator_input(struct upipe *upipe, struct uref *uref,
+                             struct upump **upump_p)
+{
+    if (!upipe_ts_scte104_generator_check_input(upipe)) {
+        upipe_ts_scte104_generator_hold_input(upipe, uref);
+        upipe_ts_scte104_generator_block_input(upipe, upump_p);
+    } else if (!upipe_ts_scte104_generator_handle(upipe, uref, upump_p)) {
+        upipe_ts_scte104_generator_hold_input(upipe, uref);
+        upipe_ts_scte104_generator_block_input(upipe, upump_p);
+        /* Increment upipe refcount to avoid disappearing before all packets
+         * have been sent. */
+        upipe_use(upipe);
+    }
+}
+
+/** @internal @This handles data.
+ *
+ * @param upipe description structure of the pipe
+ * @param uref uref structure
+ * @param upump_p reference to pump that generated the buffer
+ */
+static bool upipe_ts_scte104_generator_handle(struct upipe *upipe, struct uref *uref,
+                                    struct upump **upump_p)
+{
+    struct upipe_ts_scte104_generator *upipe_ts_scte104_generator = upipe_ts_scte104_generator_from_upipe(upipe);
+
+    if (!upipe_ts_scte104_generator->ubuf_mgr)
+        return false;
+
+    uref_free(uref);
+    //upipe_ts_scte104_generator_output(upipe, uref, upump_p);
+    return true;
+}
+
+/** @internal @This sets the input flow definition.
+ *
+ * @param upipe description structure of the pipe
+ * @param flow_def flow definition packet
+ * @return an error code
+ */
+static int upipe_ts_scte104_generator_set_flow_def(struct upipe *upipe,
+                                          struct uref *flow_def)
+{
+    if (flow_def == NULL)
+        return UBASE_ERR_INVALID;
+
+    const char *def;
+    UBASE_RETURN(uref_flow_get_def(flow_def, &def))
+
+    if (ubase_ncmp(def, EXPECTED_FLOW_DEF))
+        return UBASE_ERR_INVALID;
+
+    struct uref *flow_def_dup = uref_dup(flow_def);
+    if (flow_def_dup == NULL)
+        return UBASE_ERR_ALLOC;
+
+    uref_flow_set_def(flow_def_dup, "block.scte104.");
+
+
+    upipe_ts_scte104_generator_require_ubuf_mgr(upipe, flow_def_dup);
+    return UBASE_ERR_NONE;
+}
+
+/** @internal @This processes control commands on a ts_scte_104_generator pipe.
+ *
+ * @param upipe description structure of the pipe
+ * @param command type of command to process
+ * @param args arguments of the command
+ * @return an error code
+ */
+static int upipe_ts_scte104_generator_control(struct upipe *upipe,
+                                     int command, va_list args)
+{
+    switch (command) {
+        case UPIPE_REGISTER_REQUEST: {
+            struct urequest *request = va_arg(args, struct urequest *);
+            if (request->type == UREQUEST_UBUF_MGR ||
+                request->type == UREQUEST_FLOW_FORMAT)
+                return upipe_throw_provide_request(upipe, request);
+            return upipe_ts_scte104_generator_alloc_output_proxy(upipe, request);
+        }
+        case UPIPE_UNREGISTER_REQUEST: {
+            struct urequest *request = va_arg(args, struct urequest *);
+            if (request->type == UREQUEST_UBUF_MGR ||
+                request->type == UREQUEST_FLOW_FORMAT)
+                return UBASE_ERR_NONE;
+            return upipe_ts_scte104_generator_free_output_proxy(upipe, request);
+        }
+        case UPIPE_SET_FLOW_DEF: {
+            struct uref *flow_def = va_arg(args, struct uref *);
+            return upipe_ts_scte104_generator_set_flow_def(upipe, flow_def);
+        }
+        case UPIPE_GET_FLOW_DEF:
+        case UPIPE_GET_OUTPUT:
+        case UPIPE_SET_OUTPUT:
+            return upipe_ts_scte104_generator_control_output(upipe, command, args);
+        default:
+            return UBASE_ERR_UNHANDLED;
+    }
+}
+
+/** @internal @This allocates a s337_encaps pipe.
+ *
+ * @param mgr common management structure
+ * @param uprobe structure used to raise events
+ * @param signature signature of the pipe allocator
+ * @param args optional arguments
+ * @return pointer to upipe or NULL in case of allocation error
+ */
+static struct upipe *upipe_ts_scte104_generator_alloc(struct upipe_mgr *mgr,
+                                             struct uprobe *uprobe,
+                                             uint32_t signature, va_list args)
+{
+    struct upipe *upipe = upipe_ts_scte104_generator_alloc_void(mgr, uprobe, signature,
+                                                       args);
+    if (unlikely(upipe == NULL))
+        return NULL;
+
+    upipe_ts_scte104_generator_init_urefcount(upipe);
+    upipe_ts_scte104_generator_init_output(upipe);
+    upipe_ts_scte104_generator_init_input(upipe);
+    upipe_ts_scte104_generator_init_ubuf_mgr(upipe);
+
+    upipe_throw_ready(upipe);
+    return upipe;
+}
+
+/** @internal @This frees all resources allocated.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_ts_scte104_generator_free(struct upipe *upipe)
+{
+    upipe_throw_dead(upipe);
+
+    upipe_ts_scte104_generator_clean_output(upipe);
+    upipe_ts_scte104_generator_clean_input(upipe);
+    upipe_ts_scte104_generator_clean_urefcount(upipe);
+    upipe_ts_scte104_generator_clean_ubuf_mgr(upipe);
+    upipe_ts_scte104_generator_free_void(upipe);
+}
+
+static struct upipe_mgr upipe_ts_scte104_generator_mgr = {
+    .refcount = NULL,
+    .signature = UPIPE_TS_SCTE104_GENERATOR_SIGNATURE,
+
+    .upipe_alloc = upipe_ts_scte104_generator_alloc,
+    .upipe_input = upipe_ts_scte104_generator_input,
+    .upipe_control = upipe_ts_scte104_generator_control
+};
+
+/** @This returns the management structure for scte104_generator pipes.
+ *
+ * @return pointer to manager
+ */
+struct upipe_mgr *upipe_ts_scte104_generator_mgr_alloc(void)
+{
+    return &upipe_ts_scte104_generator_mgr;
+}
