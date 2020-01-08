c04864d1d99682f0d0de081e887fa0325780e74d
Kieran Kunhya <kierank@obe.tv>
2020-01-08 20:09:51 +0000

    udp_sink_fast: Set a threshold for logging

--- lib/upipe-modules/upipe_udp_sink_fast.c
+++ lib/upipe-modules/upipe_udp_sink_fast.c
@@ -250,7 +250,7 @@ static bool upipe_udpsink_output(struct upipe *upipe, struct uref *uref,
             //upipe_dbg_va(upipe, "waited %ld ns", wait.tv_nsec);
         }
 #endif
-    } else if (now > systime + 0)
+    } else if (now > systime + (27000/8))
         upipe_warn_va(upipe,
                       "outputting late packet %"PRIu64" us, latency %"PRIu64" us slept %u us",
                       (now - systime) / 27,
