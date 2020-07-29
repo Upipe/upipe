f0230fdfc62317a733538da50be08bd1b5d01fe1
Clément Vasseur <clement.vasseur@gmail.com>
2020-07-29 19:43:29 +0200

    upipe_avfilter: add upipe_avfilt_mgr_get_pixfmt_name()

--- include/upipe-av/upipe_avfilter.h
+++ include/upipe-av/upipe_avfilter.h
@@ -110,6 +110,29 @@ static inline int upipe_avfilt_set_hw_config(struct upipe *upipe,
  */
 struct upipe_mgr *upipe_avfilt_mgr_alloc(void);
 
+/** @This extends upipe_mgr_command with specific commands for avfilt. */
+enum upipe_avfilt_mgr_command {
+    UPIPE_AVFILT_MGR_SENTINEL = UPIPE_MGR_CONTROL_LOCAL,
+
+    /** gets the pixel format name from flow def (struct uref *, const char **) */
+    UPIPE_AVFILT_MGR_GET_PIXFMT_NAME
+};
+
+/** @This returns the pixel format name for the given flow definition.
+ *
+ * @param mgr pointer to manager
+ * @param flow_def flow definition packet
+ * @param name pixel format name
+ * @return an error code
+ */
+static inline int upipe_avfilt_mgr_get_pixfmt_name(struct upipe_mgr *mgr,
+                                                   struct uref *flow_def,
+                                                   const char **name)
+{
+    return upipe_mgr_control(mgr, UPIPE_AVFILT_MGR_GET_PIXFMT_NAME,
+                             UPIPE_AVFILT_SIGNATURE, flow_def, name);
+}
+
 #ifdef __cplusplus
 }
 #endif
