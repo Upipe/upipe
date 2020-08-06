5658f99e0758f98f523b28f196637d137bc13fa2
Arnaud de Turckheim <quarium@gmail.com>
2020-08-06 15:33:59 +0200

    uref_dump: add function to dump uref clocks

--- include/upipe/uref_dump.h
+++ include/upipe/uref_dump.h
@@ -37,6 +37,7 @@ extern "C" {
 #endif
 
 #include <upipe/ubase.h>
+#include <upipe/uclock.h>
 #include <upipe/uref.h>
 #include <upipe/uref_clock.h>
 #include <upipe/udict_dump.h>
@@ -128,6 +129,80 @@ UREF_DUMP(_notice, NOTICE);
 UREF_DUMP(_warn, WARNING);
 UREF_DUMP(_err, ERROR);
 #undef UREF_DUMP
+
+/** @internal @This dumps the clock content of a uref for debug purposes.
+ *
+ * @param uref pointer to the uref
+ * @param uprobe pipe module printing the messages
+ * @param level uprobe log level
+ */
+static inline void uref_dump_clock_lvl(struct uref *uref,
+                                       struct uprobe *uprobe,
+                                       enum uprobe_log_level level)
+{
+    const char *pfx;
+
+#define UREF_DUMP_CLOCK_BD_FMT "%02u:%02u:%02u.%03u+%05u"
+
+#define UREF_DUMP_CLOCK_BD_ARGS(x) \
+    x.hours, x.minutes, x.seconds, x.milliseconds, x.ticks
+
+#define UREF_DUMP_CLOCK_DUMP(Type, TYPE, Clock)                             \
+        uint64_t Type##_##Clock = UINT64_MAX;                               \
+        uref_clock_get_##Type##_##Clock(uref, &Type##_##Clock);             \
+        if (type_##Clock == UREF_DATE_##TYPE)                               \
+            pfx = " *";                                                     \
+        else                                                                \
+            pfx = "  ";                                                     \
+        struct uclock_brokendown Type##_##Clock##_bd =                      \
+            uclock_breakdown(Type##_##Clock);                               \
+        if (Type##_##Clock != UINT64_MAX)                                   \
+            uprobe_log_va(uprobe, NULL, level,                              \
+                          "%s%4s %3s: " UREF_DUMP_CLOCK_BD_FMT              \
+                          " - %" PRIu64 "\n",                               \
+                          pfx, #Clock, #Type,                               \
+                          UREF_DUMP_CLOCK_BD_ARGS(Type##_##Clock##_bd),     \
+                          Type##_##Clock);                                  \
+
+#define UREF_DUMP_CLOCK_DUMP_CLOCK(Clock)                                   \
+    uint64_t date_##Clock = UINT64_MAX;                                     \
+    int type_##Clock = UREF_DATE_NONE;                                      \
+    uref_clock_get_date_##Clock(uref, &date_##Clock, &type_##Clock);        \
+    UREF_DUMP_CLOCK_DUMP(pts, PTS, Clock);                                  \
+    UREF_DUMP_CLOCK_DUMP(dts, DTS, Clock);                                  \
+    UREF_DUMP_CLOCK_DUMP(cr, CR, Clock);
+
+    uprobe_log_va(uprobe, NULL, level, "uref %p:", uref);
+UREF_DUMP_CLOCK_DUMP_CLOCK(orig);
+UREF_DUMP_CLOCK_DUMP_CLOCK(prog);
+UREF_DUMP_CLOCK_DUMP_CLOCK(sys);
+
+#undef UREF_DUMP_CLOCK_DUMP_CLOCK
+#undef UREF_DUMP_CLOCK_DUMP
+#undef UREF_DUMP_CLOCK_BD_ARGS
+#undef UREF_DUMP_CLOCK_BD_FMT
+}
+
+/** @hidden */
+#define UREF_DUMP_CLOCK(Name, Level)                                        \
+/** @internal @This dumps the clock of a uref for debug purposes.           \
+ *                                                                          \
+ * @param uref pointer to the uref                                          \
+ * @param uprobe pipe module printing the messages                          \
+ */                                                                         \
+static inline void uref_dump_clock##Name(struct uref *uref,                 \
+                                         struct uprobe *uprobe)             \
+{                                                                           \
+    return uref_dump_clock_lvl(uref, uprobe, UPROBE_LOG_##Level);           \
+}
+UREF_DUMP_CLOCK(, DEBUG);
+UREF_DUMP_CLOCK(_verbose, VERBOSE);
+UREF_DUMP_CLOCK(_dbg, DEBUG);
+UREF_DUMP_CLOCK(_info, INFO);
+UREF_DUMP_CLOCK(_notice, NOTICE);
+UREF_DUMP_CLOCK(_warn, WARNING);
+UREF_DUMP_CLOCK(_err, ERROR);
+#undef UREF_DUMP_CLOCK
 
 #ifdef __cplusplus
 }
