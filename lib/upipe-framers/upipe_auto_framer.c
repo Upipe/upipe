7ad87a7d8a3a26d7a93e9438e1db10ae768fe4a6
James Darnley <jdarnley@obe.tv>
2020-09-08 16:29:32 +0200

    upipe_auto_framer: add lowlatency option to be passed to inner pipe

--- lib/upipe-framers/upipe_auto_framer.c
+++ lib/upipe-framers/upipe_auto_framer.c
@@ -84,6 +84,8 @@ struct upipe_autof_mgr {
 
     /** public upipe_mgr structure */
     struct upipe_mgr mgr;
+
+    bool lowlatency;
 };
 
 UBASE_FROM_TO(upipe_autof_mgr, upipe_mgr, upipe_mgr, mgr)
@@ -287,6 +289,12 @@ static int upipe_autof_set_flow_def(struct upipe *upipe, struct uref *flow_def)
         return UBASE_ERR_UNHANDLED;
     }
 
+    struct upipe_autof_mgr *autof_mgr = upipe_autof_mgr_from_upipe_mgr(upipe->mgr);
+
+    if (autof_mgr->lowlatency)
+        if (unlikely(!ubase_check(upipe_set_option(inner, "lowlatency", "1"))))
+            upipe_warn(upipe, "couldn't set lowlatency option on inner pipe");
+
     upipe_autof_store_bin_input(upipe, upipe_use(inner));
     upipe_autof_store_bin_output(upipe, inner);
     return UBASE_ERR_NONE;
@@ -374,6 +382,11 @@ static int set_option(struct upipe_autof_mgr *autof_mgr,
 {
     if (!option || !value)
         return UBASE_ERR_INVALID;
+
+    if (!strcmp(option, "lowlatency")) {
+        autof_mgr->lowlatency = !!atoi(value);
+        return UBASE_ERR_NONE;
+    }
 
     /* TODO: print error somehow. */
     return UBASE_ERR_INVALID;
@@ -463,5 +476,6 @@ struct upipe_mgr *upipe_autof_mgr_alloc(void)
     autof_mgr->mgr.upipe_input = upipe_autof_bin_input;
     autof_mgr->mgr.upipe_control = upipe_autof_control;
     autof_mgr->mgr.upipe_mgr_control = upipe_autof_mgr_control;
+    autof_mgr->lowlatency = false;
     return upipe_autof_mgr_to_upipe_mgr(autof_mgr);
 }
