0f531d8dfbc6076c23fa0b381a38b3403b227ae1
Clément Vasseur <clement.vasseur@gmail.com>
2020-11-05 15:40:14 +0100

    upipe-modules: remove deprecated upipe_rtp_opus

--- include/upipe-modules/upipe_rtp_opus.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UPIPE_MODULES_UPIPE_RTP_OPUS_H_
-# define _UPIPE_MODULES_UPIPE_RTP_OPUS_H_
-
-#include <upipe-modules/upipe_idem.h>
-
-/** @deprecated */
-#define upipe_rtp_opus_mgr_alloc upipe_idem_mgr_alloc
-
-#endif
