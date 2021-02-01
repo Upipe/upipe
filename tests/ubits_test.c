3860e538500a787261c904b864a88a9480adf6d7
Kieran Kunhya <kierank@obe.tv>
2021-02-01 17:39:12 +0000

    tests/ubits: Add real-world bitstream reading test

--- tests/ubits_test.c
+++ tests/ubits_test.c
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 {
     struct ubits bw;
     size_t buffer_size = 5;
-    uint8_t buffer[buffer_size];
+    uint8_t buffer[8];
     uint8_t *buffer_end = NULL;
 
     ubits_init(&bw, buffer, buffer_size, UBITS_WRITE);
@@ -108,5 +108,23 @@ int main(int argc, char **argv)
     assert(ubits_get(&bw, 1) == 0);
     assert(bw.overflow);
 
+    buffer[0] = 0x02;
+    buffer[1] = 0x8f;
+    buffer[2] = 0x80;
+    buffer[3] = 0x0e;
+    buffer[4] = 0x55;
+    buffer[5] = 0x81;
+    buffer[6] = 0x53;
+    buffer[7] = 0x78;
+
+    buffer_size = 8;
+    ubits_init(&bw, buffer, buffer_size, UBITS_READ);
+    assert(ubits_get(&bw, 6)  ==  0);
+    assert(ubits_get(&bw, 1)  ==  1);
+    assert(ubits_get(&bw, 11) ==  574);
+    assert(ubits_get(&bw, 12) ==  3);
+    assert(ubits_get(&bw, 10) ==  597);
+    assert(ubits_get(&bw, 10) ==  517);
+    assert(ubits_get(&bw, 10) ==  311);
     return 0;
 }
