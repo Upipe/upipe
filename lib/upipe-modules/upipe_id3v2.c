c148f47aaa55170dee5dc5cb3eb1b53f57fb8316
Arnaud de Turckheim <quarium@gmail.com>
2021-08-03 19:02:22 +0200

    upipe_id3v2: support multiple id3v2

--- lib/upipe-modules/upipe_id3v2.c
+++ lib/upipe-modules/upipe_id3v2.c
@@ -29,6 +29,7 @@
 #include <upipe/upipe_helper_void.h>
 #include <upipe/upipe_helper_urefcount.h>
 #include <upipe/upipe_helper_upipe.h>
+#include <upipe/upipe_helper_uref_stream.h>
 
 #include <upipe/upipe.h>
 
@@ -56,26 +57,25 @@ struct upipe_id3v2 {
     /** output requests */
     struct uchain requests;
 
-    /** id3v2 header */
-    uint8_t header[ID3V2_HEADER_SIZE];
-    /** header current size */
-    size_t header_size;
-    /** id3v2 tag */
-    uint8_t *tag;
-    /** id3v2 tag size */
-    uint32_t tag_size;
-    /** id3v2 tag current_size */
-    uint32_t current_size;
     /** timestamp */
     uint64_t timestamp;
     /** timestamp was set */
     bool timestamp_set;
+    /** is parsing done */
+    bool parsed;
+    /** first retained uref for parsing */
+    struct uref *next_uref;
+    /** retained size */
+    size_t next_uref_size;
+    /** list of retained urefs for parsing */
+    struct uchain urefs;
 };
 
 UPIPE_HELPER_UPIPE(upipe_id3v2, upipe, UPIPE_ID3V2_SIGNATURE);
 UPIPE_HELPER_UREFCOUNT(upipe_id3v2, urefcount, upipe_id3v2_no_ref);
 UPIPE_HELPER_VOID(upipe_id3v2);
 UPIPE_HELPER_OUTPUT(upipe_id3v2, output, flow_def, output_state, requests);
+UPIPE_HELPER_UREF_STREAM(upipe_id3v2, next_uref, next_uref_size, urefs, NULL);
 
 /** @internal @This allocates an id3v2 pipe.
  *
@@ -96,12 +96,11 @@ static struct upipe *upipe_id3v2_alloc(struct upipe_mgr *mgr,
 
     upipe_id3v2_init_urefcount(upipe);
     upipe_id3v2_init_output(upipe);
+    upipe_id3v2_init_uref_stream(upipe);
 
     struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_upipe(upipe);
-    upipe_id3v2->header_size = 0;
-    upipe_id3v2->tag = NULL;
-    upipe_id3v2->tag_size = 0;
     upipe_id3v2->timestamp = UINT64_MAX;
+    upipe_id3v2->parsed = false;
 
     upipe_throw_ready(upipe);
 
@@ -114,14 +113,101 @@ static struct upipe *upipe_id3v2_alloc(struct upipe_mgr *mgr,
  */
 static void upipe_id3v2_no_ref(struct upipe *upipe)
 {
-    struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_upipe(upipe);
-
     upipe_throw_dead(upipe);
 
-    free(upipe_id3v2->tag);
+    upipe_id3v2_clean_uref_stream(upipe);
     upipe_id3v2_clean_output(upipe);
     upipe_id3v2_clean_urefcount(upipe);
     upipe_id3v2_free_void(upipe);
+}
+
+/** @internal @This parses an id3v2.
+ *
+ * @param upipe description structure of the pipe
+ * @return true is the parsing is done
+ */
+static bool upipe_id3v2_parse(struct upipe *upipe)
+{
+    struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_upipe(upipe);
+    size_t size;
+    int ret = uref_block_size(upipe_id3v2->next_uref, &size);
+    if (unlikely(!ubase_check(ret))) {
+        upipe_err(upipe, "fail to get block size");
+        upipe_throw_fatal(upipe, ret);
+        return true;
+    }
+
+    if (size < ID3V2_HEADER_SIZE)
+        return false;
+
+    uint8_t header[ID3V2_HEADER_SIZE];
+    ret = uref_block_extract(upipe_id3v2->next_uref,
+                             0, ID3V2_HEADER_SIZE, header);
+    if (unlikely(!ubase_check(ret))) {
+        upipe_err(upipe, "fail to extract from block");
+        upipe_throw_fatal(upipe, ret);
+        return true;
+    }
+
+    if (unlikely(!id3v2_check_tag(header))) {
+        upipe_dbg(upipe, "not an id3v2 tag");
+        return true;
+    }
+    upipe_dbg(upipe, "get id3v2 header");
+
+    size_t tag_size =
+        ID3V2_HEADER_SIZE +
+        id3v2_get_size(header) +
+        id3v2_footer_get_size(header);
+    if (size < tag_size)
+        return false;
+    const uint8_t *tag;
+    struct uref *uref = upipe_id3v2_extract_uref_stream(upipe, tag_size);
+    if (!uref) {
+        upipe_err(upipe, "fail to extract tag from stream");
+        upipe_throw_fatal(upipe, UBASE_ERR_ALLOC);
+        return true;
+    }
+    int block_size = -1;
+    ret = uref_block_read(uref, 0, &block_size, &tag);
+    if (unlikely(!ubase_check(ret) || block_size != tag_size)) {
+        upipe_err(upipe, "fail to read from block");
+        upipe_throw_fatal(upipe, ret);
+        uref_free(uref);
+        return true;
+    }
+
+    upipe_dbg_va(upipe, "get id3v2 tag (%zu)", tag_size);
+
+    struct id3v2_frame frame;
+    memset(&frame, 0, sizeof (frame));
+    while (id3v2_get_frame(tag, &frame)) {
+        struct id3v2_frame_priv frame_priv;
+        if (id3v2_get_frame_priv(&frame, &frame_priv)) {
+            if (!strcmp(frame_priv.owner, FRAME_OWNER)) {
+                if (frame_priv.size == 8) {
+                    uint64_t ts = 0;
+                    for (uint8_t i = 0; i < frame_priv.size; i++)
+                        ts = (ts << 8) + frame_priv.data[i];
+                    upipe_id3v2->timestamp = ts * UCLOCK_FREQ / 90000;
+                    upipe_dbg_va(upipe, "found timestamp %"PRIu64, ts);
+                }
+                else
+                    upipe_err(upipe, "invalid timestamp size");
+            }
+            else
+                upipe_dbg_va(upipe, "ignore priv frame %s",
+                             frame_priv.owner);
+        }
+        else
+            upipe_dbg_va(upipe, "ignore frame %c%c%c%c",
+                         frame.id[0], frame.id[1],
+                         frame.id[2], frame.id[3]);
+    }
+
+    upipe_id3v2->timestamp_set = false;
+    uref_free(uref);
+    return false;
 }
 
 /** @internal @This is called when there is input data.
@@ -135,132 +221,16 @@ static void upipe_id3v2_input(struct upipe *upipe,
                               struct upump **upump_p)
 {
     struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_upipe(upipe);
-    int ret;
 
-    if (unlikely(upipe_id3v2->header_size < ID3V2_HEADER_SIZE)) {
-        size_t size = 0;
-
-        ret = uref_block_size(uref, &size);
-        if (unlikely(!ubase_check(ret))) {
-            uref_free(uref);
-            upipe_throw_fatal(upipe, ret);
+    if (!upipe_id3v2->parsed) {
+        upipe_id3v2_append_uref_stream(upipe, uref);
+        upipe_id3v2->parsed = upipe_id3v2_parse(upipe);
+        if (!upipe_id3v2->parsed)
             return;
-        }
-
-        if (likely(size > ID3V2_HEADER_SIZE - upipe_id3v2->header_size))
-            size = ID3V2_HEADER_SIZE - upipe_id3v2->header_size;
-
-        uint8_t *dst = upipe_id3v2->header + upipe_id3v2->header_size;
-        ret = uref_block_extract(uref, 0, size, dst);
-        if (unlikely(!ubase_check(ret))) {
-            uref_free(uref);
-            upipe_throw_fatal(upipe, ret);
+        if (!upipe_id3v2->next_uref)
             return;
-        }
-        ret = uref_block_resize(uref, size, -1);
-        if (unlikely(!ubase_check(ret))) {
-            uref_free(uref);
-            upipe_throw_fatal(upipe, ret);
-            return;
-        }
-        upipe_id3v2->header_size += size;
-
-        if (unlikely(upipe_id3v2->header_size < ID3V2_HEADER_SIZE)) {
-            uref_free(uref);
-            return;
-        }
-
-        if (unlikely(!id3v2_check_tag(upipe_id3v2->header))) {
-            upipe_err(upipe, "invalid id3v2 tag");
-            uref_free(uref);
-            upipe_id3v2->header_size = 0;
-            return;
-        }
-
-        upipe_dbg(upipe, "get id3v2 header");
-        upipe_id3v2->tag_size =
-            sizeof(upipe_id3v2->header) +
-            id3v2_get_size(upipe_id3v2->header) +
-            id3v2_footer_get_size(upipe_id3v2->header);
-        upipe_id3v2->current_size = sizeof(upipe_id3v2->header);
-        upipe_id3v2->tag = malloc(upipe_id3v2->tag_size);
-        if (unlikely(upipe_id3v2->tag == NULL)) {
-            uref_free(uref);
-            upipe_throw_fatal(upipe, UBASE_ERR_ALLOC);
-            return;
-        }
-        memcpy(upipe_id3v2->tag, upipe_id3v2->header,
-               sizeof (upipe_id3v2->header));
-    }
-
-    if (unlikely(upipe_id3v2->current_size < upipe_id3v2->tag_size)) {
-        if (unlikely(upipe_id3v2->tag == NULL)) {
-            uref_free(uref);
-            upipe_throw_fatal(upipe, UBASE_ERR_ALLOC);
-            return;
-        }
-
-        size_t size = 0;
-        ret = uref_block_size(uref, &size);
-        if (unlikely(!ubase_check(ret))) {
-            uref_free(uref);
-            upipe_throw_fatal(upipe, ret);
-            return;
-        }
-
-        if (likely(size > upipe_id3v2->tag_size - upipe_id3v2->current_size))
-            size = upipe_id3v2->tag_size - upipe_id3v2->current_size;
-
-        uint8_t *dst = upipe_id3v2->tag + upipe_id3v2->current_size;
-        ret = uref_block_extract(uref, 0, size, dst);
-        if (unlikely(!ubase_check(ret))) {
-            uref_free(uref);
-            upipe_throw_fatal(upipe, ret);
-            return;
-        }
-        ret = uref_block_resize(uref, size, -1);
-        if (unlikely(!ubase_check(ret))) {
-            uref_free(uref);
-            upipe_throw_fatal(upipe, ret);
-            return;
-        }
-        upipe_id3v2->current_size += size;
-
-        if (unlikely(upipe_id3v2->current_size < upipe_id3v2->tag_size)) {
-            uref_free(uref);
-            return;
-        }
-
-        upipe_dbg_va(upipe, "get id3v2 tag (%u)", upipe_id3v2->tag_size);
-        upipe_id3v2->timestamp = UINT64_MAX;
-
-        struct id3v2_frame frame;
-        memset(&frame, 0, sizeof (frame));
-        while (id3v2_get_frame(upipe_id3v2->tag, &frame)) {
-            struct id3v2_frame_priv frame_priv;
-            if (id3v2_get_frame_priv(&frame, &frame_priv)) {
-                if (!strcmp(frame_priv.owner, FRAME_OWNER)) {
-                    if (frame_priv.size == 8) {
-                        uint64_t ts = 0;
-                        for (uint8_t i = 0; i < frame_priv.size; i++)
-                            ts = (ts << 8) + frame_priv.data[i];
-                        upipe_id3v2->timestamp = ts * UCLOCK_FREQ / 90000;
-                        upipe_dbg_va(upipe, "found timestamp %"PRIu64, ts);
-                    }
-                    else
-                        upipe_err(upipe, "invalid timestamp size");
-                }
-                else
-                    upipe_dbg_va(upipe, "ignore priv frame %s",
-                                 frame_priv.owner);
-            }
-            else
-                upipe_dbg_va(upipe, "ignore frame %c%c%c%c",
-                             frame.id[0], frame.id[1],
-                             frame.id[2], frame.id[3]);
-        }
-
-        upipe_id3v2->timestamp_set = false;
+        uref = upipe_id3v2->next_uref;
+        upipe_id3v2_init_uref_stream(upipe);
     }
 
     if (likely(upipe_id3v2->timestamp != UINT64_MAX)) {
@@ -275,7 +245,7 @@ static void upipe_id3v2_input(struct upipe *upipe,
         upipe_warn(upipe, "cannot date uref");
 
     if (unlikely(ubase_check(uref_block_get_end(uref))))
-        upipe_id3v2->header_size = 0;
+        upipe_id3v2->parsed = false;
 
     upipe_id3v2_output(upipe, uref, upump_p);
 }
