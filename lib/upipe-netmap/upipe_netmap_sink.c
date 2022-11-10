f291ed4e44de41b5bb832d0b353ff53460a5b074
James Darnley <jdarnley@obe.tv>
2022-11-10 15:54:09 +0100

    upipe_netmap_sink: use new avx512 and avx512icl functions

--- lib/upipe-netmap/upipe_netmap_sink.c
+++ lib/upipe-netmap/upipe_netmap_sink.c
@@ -70,6 +70,7 @@
 #include "../upipe-hbrmt/sdienc.h"
 #include "../upipe-hbrmt/rfc4175_enc.h"
 #include "../upipe-modules/upipe_udp.h"
+#include "x86/avx512.h"
 
 #include <math.h>
 
@@ -670,7 +671,17 @@ static struct upipe *_upipe_netmap_sink_alloc(struct upipe_mgr *mgr,
         upipe_netmap_sink->unpack_v210 = upipe_v210_to_sdi_avx2;
     }
 
-    // FIXME
+    if (has_avx512_support()) {
+        upipe_netmap_sink->pack2_8_planar = upipe_planar_to_sdi_8_2_avx512;
+        upipe_netmap_sink->pack_8_planar = upipe_planar_to_sdi_8_avx512;
+    }
+
+    if (has_avx512icl_support()) {
+        upipe_netmap_sink->pack2_10_planar = upipe_planar_to_sdi_10_2_avx512icl;
+        upipe_netmap_sink->pack2_8_planar = upipe_planar_to_sdi_8_2_avx512icl;
+        upipe_netmap_sink->pack_10_planar = upipe_planar_to_sdi_10_avx512icl;
+        upipe_netmap_sink->pack_8_planar = upipe_planar_to_sdi_8_avx512icl;
+    }
 #endif
 #endif
 
