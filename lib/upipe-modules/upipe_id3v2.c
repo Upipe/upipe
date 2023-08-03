438c2838f63e584466bac4010dbe6057f008bb59
Arnaud de Turckheim <quarium@gmail.com>
2023-08-03 11:03:06 +0200

    upipe_id3v2: use upipe_id3v2d

--- lib/upipe-modules/upipe_id3v2.c
+++ lib/upipe-modules/upipe_id3v2.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 Arnaud de Turckheim <quarium@gmail.com>
+ * Copyright (C) 2023 EasyTools
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -21,17 +22,23 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include "id3v2.h"
-
 #include "upipe-modules/upipe_id3v2.h"
+#include "upipe-modules/upipe_id3v2_decaps.h"
+#include "upipe-modules/upipe_probe_uref.h"
 
-#include "upipe/upipe_helper_output.h"
-#include "upipe/upipe_helper_void.h"
+#include "upipe/upipe_helper_bin_input.h"
+#include "upipe/upipe_helper_bin_output.h"
+#include "upipe/upipe_helper_inner.h"
+#include "upipe/upipe_helper_uprobe.h"
 #include "upipe/upipe_helper_urefcount.h"
+#include "upipe/upipe_helper_urefcount_real.h"
+#include "upipe/upipe_helper_void.h"
 #include "upipe/upipe_helper_upipe.h"
-#include "upipe/upipe_helper_uref_stream.h"
 
 #include "upipe/upipe.h"
+
+#include "upipe/uprobe.h"
+#include "upipe/uprobe_prefix.h"
 
 #include "upipe/uref_block.h"
 #include "upipe/uref_clock.h"
@@ -39,43 +46,101 @@
 #include "upipe/urefcount.h"
 #include "upipe/uclock.h"
 
-#define EXPECTED_FLOW_DEF       "block."
-#define FRAME_OWNER             "com.apple.streaming.transportStreamTimestamp"
+#include <bitstream/id3/id3v2.h>
+#include <bitstream/id3/frame_priv.h>
 
 /** @internal @This is the private context of the pipe. */
 struct upipe_id3v2 {
     /** upipe public structure */
     struct upipe upipe;
-    /** urefcount structure */
+    /** external urefcount structure */
     struct urefcount urefcount;
+    /** internal urefcount structure */
+    struct urefcount urefcount_real;
+    /** proxy probe for inner pipes */
+    struct uprobe proxy_probe;
+    /** decaps inner pipe */
+    struct upipe *decaps;
+    /** probe uref inner pipe */
+    struct upipe *probe_uref;
+    /** input request list */
+    struct uchain input_requests;
     /** output pipe */
     struct upipe *output;
-    /** output flow def */
-    struct uref *flow_def;
-    /** output state */
-    enum upipe_helper_output_state output_state;
-    /** output requests */
-    struct uchain requests;
+    /** output request list */
+    struct uchain output_requests;
 
-    /** timestamp */
+    /** current timestamp */
     uint64_t timestamp;
-    /** timestamp was set */
-    bool timestamp_set;
-    /** is parsing done */
-    bool parsed;
-    /** first retained uref for parsing */
-    struct uref *next_uref;
-    /** retained size */
-    size_t next_uref_size;
-    /** list of retained urefs for parsing */
-    struct uchain urefs;
 };
 
+/** @hidden */
+static int catch_proxy(struct uprobe *, struct upipe *, int, va_list);
+
 UPIPE_HELPER_UPIPE(upipe_id3v2, upipe, UPIPE_ID3V2_SIGNATURE);
-UPIPE_HELPER_UREFCOUNT(upipe_id3v2, urefcount, upipe_id3v2_no_ref);
 UPIPE_HELPER_VOID(upipe_id3v2);
-UPIPE_HELPER_OUTPUT(upipe_id3v2, output, flow_def, output_state, requests);
-UPIPE_HELPER_UREF_STREAM(upipe_id3v2, next_uref, next_uref_size, urefs, NULL);
+UPIPE_HELPER_UREFCOUNT(upipe_id3v2, urefcount, upipe_id3v2_no_ref);
+UPIPE_HELPER_UREFCOUNT_REAL(upipe_id3v2, urefcount_real, upipe_id3v2_free);
+UPIPE_HELPER_UPROBE(upipe_id3v2, urefcount_real, proxy_probe, catch_proxy);
+UPIPE_HELPER_INNER(upipe_id3v2, decaps);
+UPIPE_HELPER_INNER(upipe_id3v2, probe_uref);
+UPIPE_HELPER_BIN_INPUT(upipe_id3v2, decaps, input_requests);
+UPIPE_HELPER_BIN_OUTPUT(upipe_id3v2, probe_uref, output, output_requests);
+
+/** @internal @This catches events from the inner pipes.
+ *
+ * @param uprobe structure used to raise events
+ * @param inner pointer to the inner pipe
+ * @param event event triggered by the inner pipe
+ * @param args optional arguments of the event
+ * @return an error code
+ */
+static int catch_proxy(struct uprobe *uprobe, struct upipe *inner,
+                       int event, va_list args)
+{
+    struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_proxy_probe(uprobe);
+    struct upipe *upipe = upipe_id3v2_to_upipe(upipe_id3v2);
+    struct uref *uref;
+
+    if (uprobe_id3v2d_check_tag(event, args, &uref)) {
+        const uint8_t *id3v2;
+        int block_size = -1;
+        uref_block_read(uref, 0, &block_size, &id3v2);
+
+        id3v2_each_frame(id3v2, frame) {
+            if (id3v2_frame_get_id(frame) == ID3V2_FRAME_ID_PRIV &&
+                id3v2_frame_validate_priv(frame)) {
+                const char *owner = id3v2_frame_priv_get_owner(frame);
+                const uint8_t *priv_data = id3v2_frame_priv_get_data(frame);
+                if (!strcmp(owner, ID3V2_FRAME_PRIV_APPLE_TS_TIMESTAMP)) {
+                    upipe_id3v2->timestamp =
+                        ((uint64_t)priv_data[0] << 56) +
+                        ((uint64_t)priv_data[1] << 48) +
+                        ((uint64_t)priv_data[2] << 40) +
+                        ((uint64_t)priv_data[3] << 32) +
+                        ((uint64_t)priv_data[4] << 24) +
+                        ((uint64_t)priv_data[5] << 16) +
+                        ((uint64_t)priv_data[6] << 8) +
+                        (uint64_t)priv_data[7];
+                }
+            }
+        }
+        uref_block_unmap(uref, 0);
+        return UBASE_ERR_NONE;
+    } else if (uprobe_probe_uref_check(event, args, &uref, NULL, NULL)) {
+        if (upipe_id3v2->timestamp != UINT64_MAX) {
+            uint64_t dts = upipe_id3v2->timestamp * UCLOCK_FREQ / 90000;
+            upipe_dbg_va(upipe, "set timestamp to %" PRIu64, dts);
+            upipe_id3v2->timestamp = UINT64_MAX;
+            uref_clock_set_dts_orig(uref, dts);
+            uref_clock_set_dts_prog(uref, dts);
+            uref_clock_set_dts_pts_delay(uref, 0);
+        }
+        return UBASE_ERR_NONE;
+    }
+
+    return upipe_throw_proxy(upipe, inner, event, args);
+}
 
 /** @internal @This allocates an id3v2 pipe.
  *
@@ -95,16 +160,66 @@ static struct upipe *upipe_id3v2_alloc(struct upipe_mgr *mgr,
         return NULL;
 
     upipe_id3v2_init_urefcount(upipe);
-    upipe_id3v2_init_output(upipe);
-    upipe_id3v2_init_uref_stream(upipe);
-
-    struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_upipe(upipe);
-    upipe_id3v2->timestamp = UINT64_MAX;
-    upipe_id3v2->parsed = false;
+    upipe_id3v2_init_urefcount_real(upipe);
+    upipe_id3v2_init_proxy_probe(upipe);
+    upipe_id3v2_init_bin_input(upipe);
+    upipe_id3v2_init_bin_output(upipe);
 
     upipe_throw_ready(upipe);
 
+    struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_upipe(upipe);
+    upipe_id3v2->timestamp = UINT64_MAX;
+
+    /* allocate ID3v2 decaps */
+    struct upipe_mgr *upipe_id3v2d_mgr = upipe_id3v2d_mgr_alloc();
+    if (unlikely(!upipe_id3v2d_mgr)) {
+        upipe_release(upipe);
+        return NULL;
+    }
+    struct upipe *decaps = upipe_void_alloc(
+        upipe_id3v2d_mgr,
+        uprobe_pfx_alloc(
+            uprobe_use(upipe_id3v2_to_proxy_probe(upipe_id3v2)),
+            UPROBE_LOG_VERBOSE, "decaps"));
+    upipe_mgr_release(upipe_id3v2d_mgr);
+    if (unlikely(!decaps)) {
+        upipe_release(upipe);
+        return NULL;
+    }
+    upipe_id3v2_store_bin_input(upipe, upipe_use(decaps));
+
+    /* allocate probe uref */
+    struct upipe_mgr *upipe_probe_uref_mgr = upipe_probe_uref_mgr_alloc();
+    if (unlikely(!upipe_probe_uref_mgr)) {
+        upipe_release(decaps);
+        upipe_release(upipe);
+        return NULL;
+    }
+    struct upipe *probe_uref = upipe_void_chain_output(
+        decaps, upipe_probe_uref_mgr,
+        uprobe_pfx_alloc(
+            uprobe_use(upipe_id3v2_to_proxy_probe(upipe_id3v2)),
+            UPROBE_LOG_VERBOSE, "probe"));
+    upipe_mgr_release(upipe_probe_uref_mgr);
+    if (unlikely(!probe_uref)) {
+        upipe_release(upipe);
+        return NULL;
+    }
+    upipe_id3v2_store_bin_output(upipe, probe_uref);
+
     return upipe;
+}
+
+/** @internal @This frees the pipe.
+ *
+ * @param upipe description structure of the pipe
+ */
+static void upipe_id3v2_free(struct upipe *upipe)
+{
+    upipe_id3v2_clean_proxy_probe(upipe);
+    upipe_id3v2_clean_urefcount_real(upipe);
+    upipe_id3v2_clean_urefcount(upipe);
+    upipe_id3v2_free_void(upipe);
 }
 
 /** @internal @This is called when there is no external reference to the pipe.
@@ -115,161 +230,9 @@ static void upipe_id3v2_no_ref(struct upipe *upipe)
 {
     upipe_throw_dead(upipe);
 
-    upipe_id3v2_clean_uref_stream(upipe);
-    upipe_id3v2_clean_output(upipe);
-    upipe_id3v2_clean_urefcount(upipe);
-    upipe_id3v2_free_void(upipe);
-}
-
-/** @internal @This parses an id3v2.
- *
- * @param upipe description structure of the pipe
- * @return true is the parsing is done
- */
-static bool upipe_id3v2_parse(struct upipe *upipe)
-{
-    struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_upipe(upipe);
-    size_t size;
-    int ret = uref_block_size(upipe_id3v2->next_uref, &size);
-    if (unlikely(!ubase_check(ret))) {
-        upipe_err(upipe, "fail to get block size");
-        upipe_throw_fatal(upipe, ret);
-        return true;
-    }
-
-    if (size < ID3V2_HEADER_SIZE)
-        return false;
-
-    uint8_t header[ID3V2_HEADER_SIZE];
-    ret = uref_block_extract(upipe_id3v2->next_uref,
-                             0, ID3V2_HEADER_SIZE, header);
-    if (unlikely(!ubase_check(ret))) {
-        upipe_err(upipe, "fail to extract from block");
-        upipe_throw_fatal(upipe, ret);
-        return true;
-    }
-
-    if (unlikely(!id3v2_check_tag(header))) {
-        upipe_dbg(upipe, "not an id3v2 tag");
-        return true;
-    }
-    upipe_dbg(upipe, "get id3v2 header");
-
-    size_t tag_size =
-        ID3V2_HEADER_SIZE +
-        id3v2_get_size(header) +
-        id3v2_footer_get_size(header);
-    if (size < tag_size)
-        return false;
-    struct uref *uref = upipe_id3v2_extract_uref_stream(upipe, tag_size);
-    if (!uref) {
-        upipe_err(upipe, "fail to extract tag from stream");
-        upipe_throw_fatal(upipe, UBASE_ERR_ALLOC);
-        return true;
-    }
-    uint8_t tag[tag_size];
-    ret = uref_block_extract(uref, 0, tag_size, tag);
-    if (unlikely(!ubase_check(ret))) {
-        upipe_err(upipe, "fail to read from block");
-        upipe_throw_fatal(upipe, ret);
-        uref_free(uref);
-        return true;
-    }
-
-    upipe_dbg_va(upipe, "get id3v2 tag (%zu)", tag_size);
-
-    struct id3v2_frame frame;
-    memset(&frame, 0, sizeof (frame));
-    while (id3v2_get_frame(tag, &frame)) {
-        struct id3v2_frame_priv frame_priv;
-        if (id3v2_get_frame_priv(&frame, &frame_priv)) {
-            if (!strcmp(frame_priv.owner, FRAME_OWNER)) {
-                if (frame_priv.size == 8) {
-                    uint64_t ts = 0;
-                    for (uint8_t i = 0; i < frame_priv.size; i++)
-                        ts = (ts << 8) + frame_priv.data[i];
-                    upipe_id3v2->timestamp = ts * UCLOCK_FREQ / 90000;
-                    upipe_dbg_va(upipe, "found timestamp %"PRIu64, ts);
-                }
-                else
-                    upipe_err(upipe, "invalid timestamp size");
-            }
-            else
-                upipe_dbg_va(upipe, "ignore priv frame %s",
-                             frame_priv.owner);
-        }
-        else
-            upipe_dbg_va(upipe, "ignore frame %c%c%c%c",
-                         frame.id[0], frame.id[1],
-                         frame.id[2], frame.id[3]);
-    }
-
-    upipe_id3v2->timestamp_set = false;
-    uref_free(uref);
-    return false;
-}
-
-/** @internal @This is called when there is input data.
- *
- * @param upipe description structure of the pipe
- * @param uref uref carrying the data
- * @param upump_p reference to pump that generated the buffer
- */
-static void upipe_id3v2_input(struct upipe *upipe,
-                              struct uref *uref,
-                              struct upump **upump_p)
-{
-    struct upipe_id3v2 *upipe_id3v2 = upipe_id3v2_from_upipe(upipe);
-
-    if (unlikely(ubase_check(uref_flow_get_def(uref, NULL)))) {
-        upipe_id3v2_store_flow_def(upipe, uref);
-        upipe_id3v2->parsed = false;
-        return;
-    }
-
-    if (!upipe_id3v2->parsed) {
-        upipe_id3v2_append_uref_stream(upipe, uref);
-        upipe_id3v2->parsed = upipe_id3v2_parse(upipe);
-        if (!upipe_id3v2->parsed)
-            return;
-        if (!upipe_id3v2->next_uref)
-            return;
-        uref = upipe_id3v2->next_uref;
-        upipe_id3v2_init_uref_stream(upipe);
-    }
-
-    if (likely(upipe_id3v2->timestamp != UINT64_MAX)) {
-        if (upipe_id3v2->timestamp_set == false) {
-            upipe_dbg_va(upipe, "set timestamp %"PRIu64, upipe_id3v2->timestamp);
-            uref_clock_set_date_orig(uref, upipe_id3v2->timestamp, UREF_DATE_DTS);
-            uref_clock_set_date_prog(uref, upipe_id3v2->timestamp, UREF_DATE_DTS);
-            uref_clock_set_dts_pts_delay(uref, 0);
-        }
-        upipe_id3v2->timestamp_set = true;
-    }
-    else
-        upipe_warn(upipe, "cannot date uref");
-
-    if (unlikely(ubase_check(uref_block_get_end(uref))))
-        upipe_id3v2->parsed = false;
-
-    upipe_id3v2_output(upipe, uref, upump_p);
-}
-
-/** @internal @This sets the flow format.
- *
- * @param upipe description structure of the pipe
- * @param flow_def the flow format to set
- * @return an error code
- */
-static int upipe_id3v2_set_flow_def(struct upipe *upipe,
-                                    struct uref *flow_def)
-{
-    UBASE_RETURN(uref_flow_match_def(flow_def, EXPECTED_FLOW_DEF));
-    struct uref *flow_def_dup = uref_dup(flow_def);
-    UBASE_ALLOC_RETURN(flow_def_dup);
-    upipe_input(upipe, flow_def_dup, NULL);
-    return UBASE_ERR_NONE;
+    upipe_id3v2_clean_bin_input(upipe);
+    upipe_id3v2_clean_bin_output(upipe);
+    upipe_id3v2_release_urefcount_real(upipe);
 }
 
 /** @internal @This dispatches the control commands.
@@ -283,13 +246,8 @@ static int upipe_id3v2_control(struct upipe *upipe,
                                int command,
                                va_list args)
 {
-    UBASE_HANDLED_RETURN(upipe_id3v2_control_output(upipe, command, args));
-    switch (command) {
-    case UPIPE_SET_FLOW_DEF: {
-        struct uref *flow_def = va_arg(args, struct uref *);
-        return upipe_id3v2_set_flow_def(upipe, flow_def);
-    }
-    }
+    UBASE_HANDLED_RETURN(upipe_id3v2_control_bin_input(upipe, command, args));
+    UBASE_HANDLED_RETURN(upipe_id3v2_control_bin_output(upipe, command, args));
     return UBASE_ERR_UNHANDLED;
 }
 
@@ -298,7 +256,7 @@ static struct upipe_mgr upipe_id3v2_mgr = {
     .refcount = NULL,
     .signature = UPIPE_ID3V2_SIGNATURE,
     .upipe_alloc = upipe_id3v2_alloc,
-    .upipe_input = upipe_id3v2_input,
+    .upipe_input = upipe_id3v2_bin_input,
     .upipe_control = upipe_id3v2_control,
 };
 
