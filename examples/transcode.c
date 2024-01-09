a5cdee726a1fbf02d9a02540229bfc85328bda74
Clément Vasseur <clement.vasseur@gmail.com>
2024-01-09 17:52:57 +0100

    transcode: remove unused constants

--- examples/transcode.c
+++ examples/transcode.c
@@ -72,13 +72,8 @@
 #define UDICT_POOL_DEPTH    10
 #define UREF_POOL_DEPTH     10
 #define UBUF_POOL_DEPTH     10
-#define UBUF_PREPEND        0
-#define UBUF_APPEND         0
-#define UBUF_ALIGN          32
-#define UBUF_ALIGN_OFFSET   0
 #define UPUMP_POOL          5
 #define UPUMP_BLOCKER_POOL  5
-#define READ_SIZE           4096
 #define UPROBE_LOG_LEVEL    UPROBE_LOG_INFO
 
 struct es_conf {
