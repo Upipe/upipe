7aedf37648782b41dca846dc450bf66636597206
Rafaël Carré <funman@videolan.org>
2024-06-28 11:26:48 +0200

    srtr: handle now < cr_sys

--- lib/upipe-srt/upipe_srt_receiver.c
+++ lib/upipe-srt/upipe_srt_receiver.c
@@ -584,7 +584,7 @@ static void upipe_srt_receiver_timer(struct upump *upump)
         if (unlikely(!ubase_check(uref_clock_get_cr_sys(uref, &cr_sys))))
             upipe_warn_va(upipe, "Couldn't read cr_sys in %s()", __func__);
 
-        if (now - cr_sys <= upipe_srt_receiver->latency - rtt)
+        if (now <= cr_sys + upipe_srt_receiver->latency - rtt)
             break;
 
         upipe_verbose_va(upipe, "Output seq %"PRIu64" after %"PRIu64" clocks", seqnum, now - cr_sys);
