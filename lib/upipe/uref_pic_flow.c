2b0d3b74efec1ba1e1b89248b11f7cb328929299
Clément Vasseur <clement.vasseur@gmail.com>
2024-10-07 19:00:57 +0200

    upipe_pic_flow: add hdr10 metadata attributes

--- lib/upipe/uref_pic_flow.c
+++ lib/upipe/uref_pic_flow.c
@@ -119,3 +119,126 @@ int uref_pic_flow_get_##Name##_val(struct uref *flow_def, int *value)       \
 UREF_PIC_FLOW_VALUE_CONVERT(colour_primaries)
 UREF_PIC_FLOW_VALUE_CONVERT(transfer_characteristics)
 UREF_PIC_FLOW_VALUE_CONVERT(matrix_coefficients)
+
+/** @This gets the bit depth from flow def attributes.
+ *
+ * @param flow_def flow definition packet
+ * @param p_bit_depth pointer to returned bit depth
+ * @return an error code
+ */
+int uref_pic_flow_get_bit_depth(struct uref *flow_def, int *p_bit_depth)
+{
+    const char *chroma;
+    UBASE_RETURN(uref_pic_flow_get_chroma(flow_def, &chroma, 0))
+
+    char c;
+    unsigned bit_depth;
+    if (sscanf(chroma, "%c%u", &c, &bit_depth) != 2)
+        return UBASE_ERR_INVALID;
+
+    if (p_bit_depth)
+        *p_bit_depth = bit_depth;
+
+    return UBASE_ERR_NONE;
+}
+
+/** @This checks whether the flow definition conforms
+ * to the SDR format.
+ *
+ * @param flow_def flow definition packet
+ * @return an error code
+ */
+int uref_pic_flow_check_sdr(struct uref *flow_def)
+{
+    UBASE_RETURN(uref_flow_match_def(flow_def, UREF_PIC_FLOW_DEF))
+
+    int trc;
+    UBASE_RETURN(uref_pic_flow_get_transfer_characteristics_val(flow_def, &trc))
+
+    if (trc != 1 && trc != 6 && trc != 13 && trc != 14 && trc != 15)
+        return UBASE_ERR_INVALID;
+
+    return UBASE_ERR_NONE;
+}
+
+/** @This checks whether the flow definition conforms
+ * to the HLG format.
+ *
+ * @param flow_def flow definition packet
+ * @return an error code
+ */
+int uref_pic_flow_check_hlg(struct uref *flow_def)
+{
+    const char *val;
+    UBASE_RETURN(uref_flow_match_def(flow_def, UREF_PIC_FLOW_DEF))
+
+    /* ARIB STD-B67 (HLG) transfer function */
+    UBASE_RETURN(uref_pic_flow_get_transfer_characteristics(flow_def, &val))
+    if (strcmp(val, "arib-std-b67"))
+        return UBASE_ERR_INVALID;
+
+    /* BT.2020 color primaries */
+    UBASE_RETURN(uref_pic_flow_get_colour_primaries(flow_def, &val))
+    if (strcmp(val, "bt2020"))
+        return UBASE_ERR_INVALID;
+
+    /* bit depth of 10-bit */
+    int bit_depth;
+    UBASE_RETURN(uref_pic_flow_get_bit_depth(flow_def, &bit_depth))
+    if (bit_depth != 10)
+        return UBASE_ERR_INVALID;
+
+    return UBASE_ERR_NONE;
+}
+
+/** @This checks whether the flow definition conforms
+ * to the PQ10 format.
+ *
+ * @param flow_def flow definition packet
+ * @return an error code
+ */
+int uref_pic_flow_check_pq10(struct uref *flow_def)
+{
+    const char *val;
+    UBASE_RETURN(uref_flow_match_def(flow_def, UREF_PIC_FLOW_DEF))
+
+    /* SMPTE ST 2084 (PQ) transfer function */
+    UBASE_RETURN(uref_pic_flow_get_transfer_characteristics(flow_def, &val))
+    if (strcmp(val, "smpte2084"))
+        return UBASE_ERR_INVALID;
+
+    /* BT.2020 color primaries */
+    UBASE_RETURN(uref_pic_flow_get_colour_primaries(flow_def, &val))
+    if (strcmp(val, "bt2020"))
+        return UBASE_ERR_INVALID;
+
+    /* bit depth of 10-bit */
+    int bit_depth;
+    UBASE_RETURN(uref_pic_flow_get_bit_depth(flow_def, &bit_depth))
+    if (bit_depth != 10)
+        return UBASE_ERR_INVALID;
+
+    return UBASE_ERR_NONE;
+}
+
+/** @This checks whether the flow definition conforms
+ * to the HDR10 Media Profile.
+ *
+ * @param flow_def flow definition packet
+ * @return an error code
+ */
+int uref_pic_flow_check_hdr10(struct uref *flow_def)
+{
+    UBASE_RETURN(uref_flow_match_def(flow_def, UREF_PIC_FLOW_DEF))
+
+    /* SMPTE ST 2086 (mastering display color volume) */
+    UBASE_RETURN(uref_pic_flow_get_mastering_display(flow_def, NULL))
+
+    /* MaxFALL (maximum frame-average light level) */
+    UBASE_RETURN(uref_pic_flow_get_max_fall(flow_def, NULL))
+
+    /* MaxCLL (maximum content light level) */
+    UBASE_RETURN(uref_pic_flow_get_max_cll(flow_def, NULL))
+
+    return uref_pic_flow_check_pq10(flow_def);
+}
