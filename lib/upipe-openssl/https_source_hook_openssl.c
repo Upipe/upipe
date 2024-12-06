d969c3a7c3ed5c50acb6a7838aaa97091e9f3d4a
Arnaud de Turckheim <quarium@gmail.com>
2024-12-06 18:26:36 +0100

    upipe_http_source: improve error messages

--- lib/upipe-openssl/https_source_hook_openssl.c
+++ lib/upipe-openssl/https_source_hook_openssl.c
@@ -99,12 +99,14 @@ static int https_src_hook_flags(struct https_src_hook_openssl *https)
 
 /** @internal @This reads from the socket to the SSL engine.
  *
+ * @param upipe description structure of the pipe
  * @param hook SSL hook structure
  * @param fd socket file descriptor
  * @return 0 or negative value on error, 1 if more data is needed, 2 otherwise
  */
 static int
-https_src_hook_transport_read(struct upipe_http_src_hook *hook, int fd)
+https_src_hook_transport_read(struct upipe *upipe,
+                              struct upipe_http_src_hook *hook, int fd)
 {
     struct https_src_hook_openssl *https =
         https_src_hook_openssl_from_hook(hook);
@@ -126,12 +128,14 @@ https_src_hook_transport_read(struct upipe_http_src_hook *hook, int fd)
 
 /** @internal @This writes from the SSL engine to the socket.
  *
+ * @param upipe description structure of the pipe
  * @param hook SSL hook structure
  * @param fd socket file descriptor
  * @return 0 or negative value on error, 1 if more data is needed, 2 otherwise
  */
 static int
-https_src_hook_transport_write(struct upipe_http_src_hook *hook, int fd)
+https_src_hook_transport_write(struct upipe *upipe,
+                               struct upipe_http_src_hook *hook, int fd)
 {
     struct https_src_hook_openssl *https =
         https_src_hook_openssl_from_hook(hook);
@@ -161,13 +165,15 @@ https_src_hook_transport_write(struct upipe_http_src_hook *hook, int fd)
 
 /** @internal @This reads data from the SSL engine to a buffer.
  *
+ * @param upipe description structure of the pipe
  * @param hook SSL hook structure
  * @param buffer filled with data
  * @param count buffer size
  * @return a negative value on error, 0 if the connection is closed, the number
  * of bytes written to the buffer
  */
-static ssize_t https_src_hook_data_read(struct upipe_http_src_hook *hook,
+static ssize_t https_src_hook_data_read(struct upipe *upipe,
+                                        struct upipe_http_src_hook *hook,
                                         uint8_t *buffer, size_t count)
 {
     struct https_src_hook_openssl *https =
@@ -196,12 +202,14 @@ static ssize_t https_src_hook_data_read(struct upipe_http_src_hook *hook,
 
 /** @internal @This writes data from a buffer to the SSL engine.
  *
+ * @param upipe description structure of the pipe
  * @param hook SSL hook structure
  * @param buffer data to write
  * @param count buffer number of bytes in the buffer
  * @return a negative value on error or the number of bytes read from the buffer
  */
-static ssize_t https_src_hook_data_write(struct upipe_http_src_hook *hook,
+static ssize_t https_src_hook_data_write(struct upipe *upipe,
+                                         struct upipe_http_src_hook *hook,
                                          const uint8_t *buffer, size_t count)
 {
     struct https_src_hook_openssl *https =
