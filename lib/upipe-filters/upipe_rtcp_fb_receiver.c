Upipe/upipe

commit 12b22891831531257737eb139067eea49858382f

rtcpfb: fix seqnum comparison
Assuming:
uref_seqnum == 1    // buffered packet
seq == 0            // the packet we're looking for
pkts == 1           // and no other packet

diff = uref_seqnum - seq;
diff = 1 - 0 = 1

diff > pkts
1 > 1
the comparison fails and we're gonna retransmit seq 1 which we don't want.

(cherry picked from commit ebfe1235c01d0d70416c260f11eae101d3702b59)

lib/upipe-filters/upipe_rtcp_fb_receiver.c
@@ -281,7 +281,7 @@ static void upipe_rtcpfb_lost_sub_n(struct upipe *upipe, uint16_t seq, uint16_t 
         uref_attr_get_priv(uref, &uref_seqnum);
 
         uint16_t diff = uref_seqnum - seq;
-        if (diff > pkts) {
+        if (diff >= pkts) {
             /* packet not in range */
             if (diff < 0x8000) {
                 /* packet after range */

commit 313f5f0abaa49d6b59be881a00185e3ebcfda7f5

rtcp_fb: fix packet loss with APP messages
Regression introduced in 12b2289

lib/upipe-filters/upipe_rtcp_fb_receiver.c
@@ -174,7 +174,7 @@ static void upipe_rtcpfb_app_nack(struct upipe *upipe, const uint8_t *rtp, int s
     for (size_t i = 0; i < s; i += 4) {
         uint16_t start = (range[i+0] << 8) | range[i+1];
         uint16_t pkts =  (range[i+2] << 8) | range[i+3];
-        upipe_rtcpfb_lost_sub_n(upipe, start, pkts);
+        upipe_rtcpfb_lost_sub_n(upipe, start, pkts + 1);
     }
 }
 /** @internal @This handles RTCP NACK messages.