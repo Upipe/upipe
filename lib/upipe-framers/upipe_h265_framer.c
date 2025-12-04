200b3751ac4429b7f4ed35c4d87daea00c0a85fe
Rafaël Carré <funman@videolan.org>
2025-12-04 11:08:25 +0100

    h265_framer: export constraint_idc

--- lib/upipe-framers/upipe_h265_framer.c
+++ lib/upipe-framers/upipe_h265_framer.c
@@ -808,6 +808,8 @@ static bool upipe_h265f_activate_sps(struct upipe *upipe, uint32_t sps_id)
             uref_h265_flow_set_level(flow_def, upipe_h265f->level_idc))
     UBASE_FATAL(upipe,
             uref_h265_flow_set_profile_compatibility(flow_def, upipe_h265f->profile_compatibility))
+    UBASE_FATAL(upipe,
+            uref_h265_flow_set_profile_constraint(flow_def, upipe_h265f->constraint_indicator))
 
     uint64_t max_octetrate, max_bs;
     uint32_t max_luma_ps;
