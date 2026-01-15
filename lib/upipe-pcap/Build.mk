aad6e2dfeca0a53ea937beaf45ea25e5c175134a
Rafaël Carré <funman@videolan.org>
2026-01-15 23:08:48 +0100

    upipe_pcap_src: new pipe to read pcap files

---
+++ lib/upipe-pcap/Build.mk
@@ -0,0 +1,7 @@
+lib-targets = libupipe_pcap
+
+libupipe_pcap-desc = pcap file source
+libupipe_pcap-so-version = 1.0.0
+libupipe_pcap-includes = upipe_pcap_src.h
+libupipe_pcap-src = upipe_pcap_src.c
+libupipe_pcap-libs = libupipe libpcap bitstream
