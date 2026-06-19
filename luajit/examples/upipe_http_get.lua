6e754480fe2e797dc6a14459261cd82c972bed2a
Clément Vasseur <clement.vasseur@gmail.com>
2026-06-19 11:59:15 +0200

    luajit: enable redirections in http example

--- luajit/examples/upipe_http_get.lua
+++ luajit/examples/upipe_http_get.lua
@@ -18,6 +18,7 @@ local url = arg[1]
 -- probes
 local probe, upump_mgr = upipe.default_probe(UPROBE_LOG_LEVEL)
 local probe_https = uprobe.https_openssl()
+local probe_redir = uprobe.http_redir()
 
 local function pfx(tag)
     return uprobe.pfx(UPROBE_LOG_LEVEL, tag)
@@ -27,7 +28,9 @@ end
 local sink = upipe.fsink():new(pfx("sink") .. probe)
 ubase_assert(sink:fsink_set_path("/dev/stdout", 'UPIPE_FSINK_OVERWRITE'))
 
-local src = upipe.http_src():new(pfx("http") .. probe_https .. probe)
+local src = upipe.http_src():new(
+    pfx("http") .. probe_redir .. probe_https .. probe)
+
 src.uri = url
 src.output = sink
 
